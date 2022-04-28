b46497311c6026062b9a238e579a06f5ac4f5335
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-04-29 03:47:10 +0530

    Time: 2 ms (30.08%), Space: 44.1 MB (24.20%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -12,8 +12,25 @@ class Solution {
          
         return dp[n] = ans;
     }
+    
+     private int minCostClimbingStairs_tabu(int[] cost , int[] dp, int N){
+         for(int n=0;n<=N;n++){
+             if(n<=1){
+                dp[n] = cost[n];
+                 continue;
+             }
+
+        int fcall = minCostClimbingStairs(cost,dp,n-1);
+        int scall = minCostClimbingStairs(cost,dp,n-2);   
+        int ans = Math.min(fcall,scall) + (n==cost.length?0:cost[n]);
+         
+         dp[n] = ans;
+         }
+         
+         return dp[N];
+     }
     public int minCostClimbingStairs(int[] cost) {
         int[] dp = new int [cost.length+1];
-       return minCostClimbingStairs(cost,dp,cost.length);
+       return minCostClimbingStairs_tabu(cost,dp,cost.length);
     }
 }
