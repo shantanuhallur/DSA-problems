11c746236dcdde63e51f9f8827ea64458f40c554
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-04-29 03:44:49 +0530

    Time: 2 ms (30.08%), Space: 43.6 MB (55.52%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,22 +1,16 @@
 class Solution {
-    
+    //momoization
     private int minCostClimbingStairs(int[] cost , int[] dp, int n){
-        if(n<=1){
-            return dp[n] = cost[n];
-        }
-        if(dp[n]!=0){
-            return dp[n];
-        }
-            
-            
-          int fcall = minCostClimbingStairs(cost,dp,n-1);
-          int scall = minCostClimbingStairs(cost,dp,n-2);
-        
-         dp[n] = Math.min(fcall,scall) ;
-        if(n<cost.length){
-            dp[n]+=cost[n]; //cost of getting jumped on last stair is 0;
-        }
-        return dp[n];
+       if(n<=1){
+           return dp[n] = cost[n];
+       }
+        if(dp[n]!=0) return dp[n]; 
+
+        int fcall = minCostClimbingStairs(cost,dp,n-1);
+        int scall = minCostClimbingStairs(cost,dp,n-2);   
+        int ans = Math.min(fcall,scall) + (n==cost.length?0:cost[n]);
+         
+        return dp[n] = ans;
     }
     public int minCostClimbingStairs(int[] cost) {
         int[] dp = new int [cost.length+1];
