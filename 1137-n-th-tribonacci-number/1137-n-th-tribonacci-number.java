09d80b0d7fa1dc24b9120fd0cd9609f36f062554
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-04-28 21:19:57 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (93.26%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int tribonacci(int n,int[] dp){
+    private int tribonacci(int n,int[] dp){
         if(n<=2) return dp[n] = n==0?0:1;
         
         if(dp[n]!=0){
@@ -9,10 +9,24 @@ class Solution {
         return dp[n] = tribonacci(n-1,dp) + tribonacci(n-2,dp) + tribonacci(n-3,dp);
         
     }
+    private int tribonacci_tabu(int N,int[] dp){
+        for(int n=0 ; n<=N;n++){
+            if(n<=2){
+               dp[n] = n==0?0:1;
+                continue;
+            } 
+        
+         dp[n] = tribonacci(n-1,dp) + tribonacci(n-2,dp) + tribonacci(n-3,dp);
+        }
+        
+        return dp[N];
+    }
     
     public int tribonacci(int n) {
         int[]dp = new int[n+1];
-        return tribonacci(n,dp);
+        //return tribonacci(n,dp);
+        
+        return tribonacci_tabu(n,dp);
         
         
     }
