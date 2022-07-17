95e49a0e23e17b95d03665ac0d5d495a810b2728
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-17 16:21:50 +0530

    Time: 1 ms (72.10%), Space: 40.5 MB (78.26%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.java
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -1,32 +1,30 @@
 class Solution {
     public int nextGreaterElement(int n) {
-        char[] arr = (n+"").toCharArray();
+      char[] arr = (n+"").toCharArray();
         int i = arr.length-1;
         while(i>0){
-            if(arr[i-1]>=arr[i]){
-                i--;
-            }
-            else{
+            if(arr[i-1]<arr[i]){
                 break;
             }
+            i--;
         }
         
         if(i==0){
             return -1;
         }
-        int idx1=i-1;
+        int idx = i-1;
         int j = arr.length-1;
-        
-        while(j>idx1){
-            if(arr[j]>arr[idx1]){
+        while(j>idx){
+            if(arr[j]>arr[idx]){
                 break;
             }
             j--;
         }
         
-        swap(arr,idx1,j);
-        int right = arr.length-1;
+        swap(arr,idx,j);
         int left = i;
+        int right = arr.length-1;
+        
         while(left<right){
             swap(arr,left,right);
             left++;
@@ -35,8 +33,7 @@ class Solution {
         String res = new String(arr);
         long val = Long.parseLong(res);
         
-        return(val>Integer.MAX_VALUE ? -1 :(int)val);
-        
+        return(val>Integer.MAX_VALUE? -1 : (int)val);
     }
     
     void swap(char[] arr,int s,int e){
