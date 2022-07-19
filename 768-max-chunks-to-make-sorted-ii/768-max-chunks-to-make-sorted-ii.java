cc81e54f92843a18539b1957d3f192b660fbd757
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-19 17:25:43 +0530

    Time: 1 ms (91.33%), Space: 44.6 MB (79.33%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
@@ -6,29 +6,36 @@ class Solution {
         
         int min = minfrmR[arr.length-1];
         
-        // for(int i=arr.length-2;i>=0;i--){
-        //  minfrmR[i] = Math.min(minfrm[i+1],arr[i]);   
-        // }
-        
-        
         for(int i=arr.length-2;i>=0;i--){
-            int val = arr[i];
-            if(val<min){
-                minfrmR[i] = val;
-                min=val;
-            }
-            else{
-                    minfrmR[i] = min;      
-               }
+         minfrmR[i] = Math.min(minfrmR[i+1],arr[i]);   
         }
+        
+        
+        // for(int i=arr.length-2;i>=0;i--){
+        //     int val = arr[i];
+        //     if(val<min){
+        //         minfrmR[i] = val;
+        //         min=val;
+        //     }
+        //     else{
+        //             minfrmR[i] = min;      
+        //        }
+        // }
         
         int res =0;
         int max = Integer.MIN_VALUE;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]>max){
-                max=arr[i];
-            }
+//         for(int i=0;i<arr.length-1;i++){
+//             if(arr[i]>max){
+//                 max=arr[i];
+//             }
             
+//             if(max<=minfrmR[i+1]){
+//                 res++;
+//             }
+//         }
+        
+        for(int i=0;i<arr.length-1;i++){
+            max = Math.max(max,arr[i]);
             if(max<=minfrmR[i+1]){
                 res++;
             }
