f2706b9ce9372a142616461ce29885f1e9b24622
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-21 11:02:08 +0530

    Time: 3 ms (70.40%), Space: 49.5 MB (42.03%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
@@ -3,15 +3,23 @@ class Solution {
         int j = -1, res = 0; int n = seats.length;
         
         for(int i=0;i<n;i++){
-            int ans =0;
             if(seats[i]==1){
-                ans = j<0?i:(i-j)/2;
+                int ans=0;
+                if(j<0){
+                    //j still -1 so distance = i
+                    ans = i;
+                }
+                else{
+                    // j already set to first 1
+                    ans = (i-j)/2;
+                }
+                res = Math.max(res,ans);
                 j=i;
             }
-            res = Math.max(ans,res);
         }
         
-        res = Math.max(res,n-1-j);
+        //Case for distance between last 1 and end of the length:- 10000
+        res = Math.max(res,n-1-j); //j will be set as it is guarenteed atleast one 1 will appear
         
         return res;
     }
