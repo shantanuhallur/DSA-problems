b2da9a04eaf1dc30c5b49009c8059fb2c1ec2fac
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-22 14:31:35 +0530

    Time: 4 ms (70.57%), Space: 126.8 MB (63.94%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,17 +1,19 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
        for(int i=0;i<nums.length;i++){
-           if(nums[i]<=0 || nums[i]>nums.length){
+           if(nums[i]<1 || nums[i]>nums.length){
                nums[i] = nums.length+1;
            }
        }
         
         for(int i=0;i<nums.length;i++){
-           int val = Math.abs(nums[i]);
+          int val = Math.abs(nums[i]);
             if(val<=nums.length){
-                int idx = val-1;
-                if(nums[idx]>0){
-                    nums[idx]*=-1;
+                int index = val-1;
+                if(nums[index]>0){  
+                    //Or else the index is already set *-1 Once (Repeated element in tht array)
+                    nums[index]*=-1;
+                    
                 }
             }
        }
@@ -21,7 +23,6 @@ class Solution {
                 return i+1;
             }
         }
-        
         return nums.length+1;
     }
 }
