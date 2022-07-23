3ad8ddb866f4926e02ba09ef377d36a4bbfdcc6f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-23 13:47:20 +0530

    Time: 5 ms (46.85%), Space: 67.2 MB (55.46%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
@@ -3,19 +3,19 @@ class Solution {
       int e = -1;
         int s = -1;
         int res = 0;
-        
         for(int i=0;i<nums.length;i++){
-            if(nums[i]>=left && nums[i]<=right){
+            if(nums[i]>= left && nums[i]<= right){
                 e=i;
             }
-            else if(nums[i]>= right){
+            else if(nums[i]>=right){
                 e=s=i;
             }
-            else if(nums[i]<left){
+            else if(nums[i]<right){
                 
             }
             res += (e-s);
         }
+       
         return res;
     }
 }
