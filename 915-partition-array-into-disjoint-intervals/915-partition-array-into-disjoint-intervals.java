d5ddbeee0e98d3077439bef452d69a3df6a1d237
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-24 19:23:56 +0530

    Time: 3 ms (92.19%), Space: 86.9 MB (79.05%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -1,14 +1,14 @@
 class Solution {
     public int partitionDisjoint(int[] nums) {
         int leftMax = nums[0];
-        int dummyLeft = nums[0];
+        int maxElementInRightArray = nums[0];
         int ans = 0;
-        
+//Our max value in scanner right array can potentially be the left max for our left array iff we find an element which is smaller than our current leftMax in the RIGHT ARRAY.         
         for(int i=0;i<nums.length;i++){
-            if(nums[i]>leftMax){
-                leftMax=nums[i];
-            }else if(nums[i]<dummyLeft){
-                dummyLeft = leftMax;
+            if(nums[i]>maxElementInRightArray){
+                maxElementInRightArray = nums[i];
+            }else if(nums[i]<leftMax){
+                leftMax = maxElementInRightArray;
                 ans = i;
             }
         }
