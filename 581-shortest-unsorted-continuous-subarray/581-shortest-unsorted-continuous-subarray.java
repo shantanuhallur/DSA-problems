a65e2d5ea6cb6bd478a2f4ce51452530bb8a526f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-24 23:57:42 +0530

    Time: 2 ms (84.59%), Space: 54.2 MB (42.69%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,32 +1,28 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        int end = -1;
+        int e=-1;
         int max = nums[0];
-        
-        for(int i=1;i<nums.length;i++){
-            if(max>nums[i]){
-                end = i;
+        //Check if any element is lesser than max of left element
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<max){
+                e=i;
             }
             else{
-                  max = nums[i];
+                max = nums[i];
             }
-          
-            
         }
-        
-        int st = 0;
-        int min = nums[nums.length-1];
-        
-        
-        
-        for(int i=nums.length-2;i>=0;i--){
-            if(min<nums[i]){
-                st = i;
+        //Check if any element is greater than min of left element
+        int s=0;
+        int min=nums[nums.length-1];
+            for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]>min){
+                s=i;
             }
             else{
-                min =nums[i];   
+                min = nums[i];
             }
         }
-        return (end-st)+1;
+        //return e-s if array sorted ... returns (0-1)+1 = 0; 
+        return (e-s)+1;
     }
 }
