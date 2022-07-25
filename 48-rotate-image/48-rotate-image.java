ed758ed5a983514899c630692f84c5c1413f07cb
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-25 10:54:51 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (46.14%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -20,6 +20,10 @@ class Solution {
                 right--;
             }
         }
+        
+        // for(int[] arr:matrix){
+        //     Collections.reverse(Arrays.asList(arr));
+        // }
        
     }
 }
