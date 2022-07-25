c81e5922954cc503cfdf8e9879f613c3f46ce073
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-25 16:14:12 +0530

    Time: 7 ms (61.21%), Space: 42.1 MB (60.14%) - LeetHub

---
+++ 537-complex-number-multiplication/537-complex-number-multiplication.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String complexNumberMultiply(String num1, String num2) {
+        //num1 = a + bi
+        
+        //num2 = c + di
+        
+        //res = Real :- [(a*c) -(b*d)] + Img :- (a*d + c*b)i
+        
+        int a = Integer.parseInt(num1.substring(0,num1.indexOf('+')));
+        int b = Integer.parseInt(num1.substring(num1.indexOf('+')+1,num1.length()-1));
+        int c = Integer.parseInt(num2.substring(0,num2.indexOf('+')));
+        int d = Integer.parseInt(num2.substring(num2.indexOf('+')+1,num2.length()-1));
+        
+        return "" + (a*c - b*d) + "+" + (a*d + c*b) + "i";
+    }
+}
