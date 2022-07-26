b7daf4e0cfe2a7bd9aa1c6d8a60df65cc02736b1
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-26 22:07:39 +0530

    Time: 3 ms (54.32%), Space: 43.2 MB (41.36%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    private static boolean isMatching(String word,String pattern){
+            HashMap<Character,Character>map = new HashMap<>();
+            HashSet<Character>set = new HashSet<>();
+        for(int i=0;i<pattern.length();i++){
+            char wChar = word.charAt(i);
+            char pChar = pattern.charAt(i);
+            
+            if(map.containsKey(pChar)==true){
+                if(map.get(pChar)!= wChar)return false;
+            }
+            else{
+                if(set.contains(wChar))return false;
+                map.put(pChar,wChar);
+                set.add(wChar);
+            }
+        }
+        return true;
+    }
+    
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        
+        ArrayList<String> ans = new ArrayList<>();
+        
+        for(String word:words){
+            if(isMatching(word,pattern)){
+                ans.add(word);
+            }
+        }
+        return ans;
+    }
+}
