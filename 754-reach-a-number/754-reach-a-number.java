c07b231cd8f2d619177470a2f1afb24a03c9e81f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-26 20:38:37 +0530

    Time: 4 ms (10.62%), Space: 39.2 MB (85.68%) - LeetHub

--- 754-reach-a-number/754-reach-a-number.java
+++ 754-reach-a-number/754-reach-a-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public int reachNumber(int target) {
       target = Math.abs(target);
-        int ans = 0;
         int x = 0;
         while(true){
             int range = x*(x+1)/2;
