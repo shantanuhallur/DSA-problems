11337bf00c2a2f6fa62e74d558b0d8ba0b00e2d1
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-28 11:05:30 +0530

    Time: 4 ms (27.26%), Space: 53.8 MB (81.37%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -4,19 +4,20 @@ class Solution {
         int m = grid[0].length;
         int[] ans = new int[m];
         
-        for(int col=0;col<m;col++){
-            int cpos = col;
-            int npos = -1;
-            for(int row=0;row<n;row++){
-                npos = cpos + grid[row][cpos];
-                if(npos<0 || npos >= m || grid[row][cpos] != grid[row][npos]){
-                    cpos = -1;
-                    break;
-                }
-                cpos = npos;
-            }
-            ans[col] = cpos;
-        }
+       for(int col = 0;col<m;col++){
+           int cpos = col;
+           int npos = -1;
+           
+           for(int row = 0;row<n;row++){
+               npos = cpos +grid[row][cpos];
+               if(npos<0 || npos >=m || grid[row][cpos]!=grid[row][npos]){
+                   cpos = -1;
+                   break;
+               }
+               cpos = npos;
+           }
+           ans[col] = cpos;
+       }
         return ans;
     }
 }
