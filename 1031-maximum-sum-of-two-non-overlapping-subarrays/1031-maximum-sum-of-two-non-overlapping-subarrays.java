b3749f2f343b2e5d400849244821185b2c1345a2
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-29 11:43:36 +0530

    Time: 4 ms (40.64%), Space: 43 MB (36.96%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.java
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.java
@@ -6,18 +6,18 @@ class Solution {
         return max;
     }
     
-    public int maxSubarray(int[] nums, int firstLen, int secondLen){
+    public int maxSubarray(int[] nums, int fl, int sl){
         //left max sum
         int[] left = new int[nums.length];
         int sum=0;
         for(int i=0;i<left.length;i++){
             sum+=nums[i];
             
-            if(i==firstLen-1){
+            if(i==fl-1){
                 left[i]=sum;
             }
-            else if(i>=firstLen){
-                sum -= nums[i-firstLen];
+            else if(i>=fl){
+                sum -= nums[i-fl];
                 left[i] = Math.max(sum,left[i-1]);
             }
         }
@@ -27,17 +27,17 @@ class Solution {
         for(int i=right.length-1;i>=0;i--){
             sum+=nums[i];
             
-            if(i==nums.length-secondLen){
+            if(i==nums.length-sl){
                 right[i]=sum;
             }
-            else if(i<nums.length-secondLen){
-                sum -= nums[i+secondLen];
+            else if(i<nums.length-sl){
+                sum -= nums[i+sl];
                 right[i] = Math.max(sum,right[i+1]);
             }
         }
         
         int max = Integer.MIN_VALUE;
-        for(int i=firstLen-1;i<nums.length-secondLen;i++){
+        for(int i=fl-1;i<nums.length-sl;i++){
             max = Math.max(left[i]+right[i+1],max);
         }
         return max;
