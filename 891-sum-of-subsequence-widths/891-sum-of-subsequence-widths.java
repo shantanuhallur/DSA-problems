10ed5d1c18a32e13c7ede2a47a99ace21824956f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-30 15:39:45 +0530

    Time: 63 ms (27.01%), Space: 72.5 MB (82.94%) - LeetHub

--- 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.java
+++ 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.java
@@ -6,10 +6,12 @@ class Solution {
         long mod = 1000000007;
         long p = 1;
         
+        int j =n-1;
         for(int i=0;i<nums.length;i++){
-            ans = (ans+ p*nums[i] - p*nums[n-1-i])%mod;
+            ans = (ans+ p*nums[i] - p*nums[j])%mod;
             
             p = (p*2)%mod;
+            j--;
         }
         
         return (int)ans;
