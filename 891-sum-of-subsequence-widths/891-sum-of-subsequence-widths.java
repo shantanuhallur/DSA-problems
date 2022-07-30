f3b8e21a4597515e2ea6e634013806318dbc8259
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-30 15:39:15 +0530

    Time: 55 ms (54.98%), Space: 74.9 MB (72.04%) - LeetHub

--- 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.java
+++ 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.java
@@ -2,17 +2,16 @@ class Solution {
     public int sumSubseqWidths(int[] nums) {
      Arrays.sort(nums);
         long ans = 0;
-        long mod = 1000000007;
         int n = nums.length;
-        long[]pow = new long[n];
-        pow[0]=1;
+        long mod = 1000000007;
+        long p = 1;
         
-        for(int i=1;i<n;i++){
-            pow[i]=(pow[i-1]*2)%mod;
+        for(int i=0;i<nums.length;i++){
+            ans = (ans+ p*nums[i] - p*nums[n-1-i])%mod;
+            
+            p = (p*2)%mod;
         }
-        for(int i=0;i<n;i++){
-            ans = (ans+nums[i]*(pow[i]-pow[n-1-i]))%mod;
-        }
+        
         return (int)ans;
     }
 }
