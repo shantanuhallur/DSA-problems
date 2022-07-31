06384240f9b25d21c5209c00e200de634f2383e2
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-07-31 20:14:43 +0530

    Time: 1 ms (92.38%), Space: 44.5 MB (38.57%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.java
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -1,32 +1,25 @@
 class Solution {
     public int countBattleships(char[][] board) {
         int ans=0;
+        int n = board.length;
+        int m = board[0].length;
         
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='X'){
-                     if(i==0 && j==0){
-                        ans++;
-                     }
-                    else if(i==0){
-                        if(board[i][j-1]!='X'){
-                            ans++;
-                        }
-                    }
-                    else if (j==0){
-                        if(board[i-1][j]!='X'){
-                            ans++;
-                        }
-                    }
-                    else{
-                        if(board[i][j-1]!='X' && board[i-1][j]!='X' ){
-                            ans++;
-                        }
-                    }
-                }
-               
-            }
-        }
+      for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+              if(board[i][j]=='X'){
+                  if(i==0 && j==0) ans++;
+                  else if(j==0){
+                      if(board[i-1][j]!='X') ans++;
+                  }
+                  else if(i==0){
+                      if(board[i][j-1]!='X') ans++; 
+                  }
+                  else{
+                      if(board[i-1][j]!='X' && board[i][j-1]!='X') ans++;
+                  }
+              }
+          }
+      }
         return ans;
     }
 }
