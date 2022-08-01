5a46c47f01e1b7cf2a1aecf6e58abb8b6bb91d8c
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-01 19:39:42 +0530

    Time: 19 ms (54.15%), Space: 50.5 MB (50.84%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -9,18 +9,21 @@ class Solution {
         }
         //MIN priority queue on the basis of FREQUENCY of MAP; 
         PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)-> m.get(a)- m.get(b));
+        //keep pq of size K so that we get only elements with greatest freq
         for(int value: m.keySet()){
             pq.add(value);
             if(pq.size()>k){
                 pq.poll();
             }
         }
+        //store answer from pq in res array.
         int[] res = new int[k];
         int ptr=0;
         while(!pq.isEmpty()){
             int val = pq.poll();
             res[ptr++] = val;
         }
+        //return res.
         return res;
     }
 }
