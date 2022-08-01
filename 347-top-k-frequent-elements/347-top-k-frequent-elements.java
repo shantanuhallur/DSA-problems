93cb49bd70a4b81edd7b67660963771a3ad1ac2b
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-01 19:38:23 +0530

    Time: 34 ms (9.96%), Space: 50 MB (75.12%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -3,24 +3,24 @@ class Solution {
         if(nums.length==0){
             return new int[k];
         }
-        HashMap<Integer,Integer> map = new HashMap<>();
+     HashMap<Integer,Integer> m = new HashMap<>();
         for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            m.put(num,m.getOrDefault(num,0)+1);
         }
-        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
-        for(int n:map.keySet()){
-            pq.add(n);
+        //MIN priority queue on the basis of FREQUENCY of MAP; 
+        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b)-> m.get(a)- m.get(b));
+        for(int value: m.keySet()){
+            pq.add(value);
             if(pq.size()>k){
                 pq.poll();
             }
         }
-                                                       
         int[] res = new int[k];
-        int i=0;
+        int ptr=0;
         while(!pq.isEmpty()){
-            int num = pq.poll();
-            res[i++] = num;
+            int val = pq.poll();
+            res[ptr++] = val;
         }
-       return res;
+        return res;
     }
 }
