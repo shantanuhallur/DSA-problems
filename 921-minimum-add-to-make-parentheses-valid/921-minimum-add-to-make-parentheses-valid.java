5360c52d702d45ab54722aacf8eb7f14ab9f7a0e
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-02 21:16:34 +0530

    Time: 4 ms (15.35%), Space: 41.5 MB (81.79%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -2,11 +2,12 @@ class Solution {
     public int minAddToMakeValid(String s) {
         Stack<Character> st = new Stack<>();
         for(int i=0;i<s.length();i++){
+        // ONE AND ONLYY CONDITION WHEN WE WILL POP FROM STACK AND SKIP ITERATION.
            if(st.size()>0 && st.peek()=='(' && s.charAt(i)==')'){
                st.pop();
                continue;
            }
-            
+        //ELSE WE WILL JUST KEEP PUSHING INTO STACK.    
             st.push(s.charAt(i));
         }
         return st.size();
