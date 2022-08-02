b59ee6efedc81d628edf790f8800dcd93d2bda12
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-02 18:47:25 +0530

    Time: 186 ms (54.18%), Space: 87.2 MB (66.09%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -4,15 +4,15 @@ class Solution {
         st.push(-1);
         int maxArea=0;
         //ADDED -1 at start and 0 At end of array to handle all cases
-        for(int i=0;i<=heights.length;i++){
-            int val = i==heights.length?0:heights[i];
-            while(st.peek()!= -1 && val <= heights[st.peek()]){
-                int rm = i;
+        for(int j=0;j<=heights.length;j++){
+            int val = j==heights.length?0:heights[j];
+            while(st.peek()!=-1 && val<=heights[st.peek()]){
+                int rm = j;
                 int height = heights[st.pop()];
                 int lm = st.peek();
                 maxArea = Math.max(maxArea,(rm-lm-1)*height);
             }
-            st.push(i);
+            st.push(j);
         }
      
         
