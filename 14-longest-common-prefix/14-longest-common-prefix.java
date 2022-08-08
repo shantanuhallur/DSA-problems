83e45483c662e9b6fb8a00aa7f46fba053e20d21
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-08 22:18:15 +0530

    Time: 1 ms (89.34%), Space: 42.2 MB (45.62%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0)return "";
+        //get length of first string
+        int length = strs[0].length();
+        //check from the length of first string to each string
+        for(int i=0;i<strs.length;i++){
+            //if next string is small update the current length
+            if(strs[i].length()<length){
+                length = strs[i].length();
+            }
+            //form 0 to that length start checking characters one by one of 2 strings
+            for(int j=0;j<length;j++){
+               char ch1 = strs[0].charAt(j);
+               char ch2 = strs[i].charAt(j);
+                // if character is found not equal , update that length and move forward with that                        length by breaking
+                if(ch1!=ch2){
+                    length = j;
+                    break;
+                }
+            }
+            
+            //keep continuing the checking till last string.
+            
+        }
+        return strs[0].substring(0,length);
+    }
+}
