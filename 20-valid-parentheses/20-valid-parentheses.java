9d72523e4d81ef01ab560745797c0e295d775816
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-09 21:18:54 +0530

    Time: 1 ms (99.61%), Space: 40.1 MB (97.75%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,41 +1,29 @@
 class Solution {
+    private static final String OPENING_BRACKETS = "({[";
+       private static final String CLOSING_BRACKETS = ")}]";
+    private boolean isOpeningBracket(char ch){
+        return OPENING_BRACKETS.indexOf(ch)!= -1;
+    }
+    private boolean isMatching(char pop , char ch){ 
+        return OPENING_BRACKETS.indexOf(pop)==CLOSING_BRACKETS.indexOf(ch);
+    }
     public boolean isValid(String s) {
         Stack<Character>st = new Stack<>();
         
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             
-            if(ch == ')'){
-                if(st.size()>0 && st.peek() == '('){
-                    st.pop();
-                    continue;
-                }
-                else{
+           if(isOpeningBracket(ch)){
+               st.push(ch);
+           }
+            else{
+                if(st.isEmpty() || !isMatching(st.pop(),ch)){
                     return false;
                 }
             }
-            else if(ch == ']'){
-                if(st.size()>0 && st.peek() == '['){
-                    st.pop();
-                    continue;
-                }
-                else{
-                    return false;
-                }
-            }
-            else if(ch == '}'){
-                if(st.size()>0 && st.peek() == '{'){
-                    st.pop();
-                    continue;
-                }
-                else{
-                    return false;
-                }
-            }
-            
-            st.push(ch);
         }
         
-        return (st.size()==0);
+        return (st.isEmpty());
     }
+    
 }
