39311b9eba1ece202c706fd19cb8af9e9e228f82
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-14 15:46:45 +0530

    Time: 5 ms (56.06%), Space: 56.8 MB (69.51%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,26 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int count =0;
-        
+        int[][] dir ={{0,1},{-1,0},{0,-1},{1,0}};
+        // int[][] vis = new int[grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
                     count+=1;
-                    checkBFS(grid,i,j);
+                    checkBFS(grid,i,j,dir);
                 }
             }
         }
         return count;
     }
     
-    private void checkBFS(char[][]grid, int i,int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0')return;
+    private void checkBFS(char[][]grid, int i,int j,int[][] dir){
+            grid[i][j]='0';
         
-        grid[i][j]='0';
-        
-        checkBFS(grid,i+1,j);
-        checkBFS(grid,i-1,j);
-        checkBFS(grid,i,j+1);
-        checkBFS(grid,i,j-1);
-    }
+                for(int d=0;d<dir.length;d++){
+                        int r = i + dir[d][0];
+                        int c = j + dir[d][1];
+                        if(r>=0 && c>=0 && r<grid.length && c<grid[0].length && grid[r][c]=='1'){
+                            checkBFS(grid,r,c,dir);    
+                        }
+                    }
+}
+    
 }
