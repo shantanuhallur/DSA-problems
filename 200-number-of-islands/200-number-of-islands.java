a0a2b80dffac0dc4641990d1c75071fc6e51bff5
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-14 15:00:54 +0530

    Time: 5 ms (56.06%), Space: 57.8 MB (32.56%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count =0;
+        
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    count+=1;
+                    checkBFS(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void checkBFS(char[][]grid, int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0')return;
+        
+        grid[i][j]='0';
+        
+        checkBFS(grid,i+1,j);
+        checkBFS(grid,i-1,j);
+        checkBFS(grid,i,j+1);
+        checkBFS(grid,i,j-1);
+    }
+}
