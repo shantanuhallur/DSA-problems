0b1d6b9ebdba2b6e740cec03b43ae627bea2fdc8
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-15 12:12:48 +0530

    Time: 4 ms (27.43%), Space: 46.3 MB (39.53%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -19,18 +19,18 @@ class Node {
 
 class Solution {
     public List<Integer> preorder(Node root) {
-        LinkedList<Node>q = new LinkedList<>();
+        LinkedList<Node> st = new LinkedList<>();
         LinkedList<Integer> output_arr =  new LinkedList<>();
         if(root==null){
             return output_arr;
         }
-        q.add(root);
+        st.add(root);
         
-        while(!q.isEmpty()){
-            Node node = q.pollLast();
+        while(!st.isEmpty()){
+            Node node = st.pollLast();
             
             for(int child = node.children.size()-1;child>=0;child--){
-                q.addLast(node.children.get(child));
+                st.addLast(node.children.get(child));
             }
             output_arr.add(node.val);
         }
