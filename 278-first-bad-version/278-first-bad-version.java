b241b47871db54f37ebfb9b0b00ce15174e9b9b8
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-16 21:42:26 +0530

    Time: 24 ms (45.47%), Space: 41.1 MB (27.31%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -8,13 +8,8 @@ public class Solution extends VersionControl {
         
         while(s<e){
             int mid = s+(e-s)/2;
-            
-            if(isBadVersion(mid)){
-                e = mid;
-            }
-            else{
-                s = mid+1;
-            }
+            if(isBadVersion(mid)) e = mid;
+            else s = mid+1; 
         }
         return e;
     }
