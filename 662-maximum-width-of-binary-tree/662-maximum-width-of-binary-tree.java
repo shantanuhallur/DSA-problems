8446d275dc3c13affd5f2ed1bc880fae0d68d419
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-20 17:48:51 +0530

    Time: 2 ms (87.74%), Space: 44.4 MB (9.69%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -15,40 +15,41 @@
  */
 class Solution {
     class Pair{
-        TreeNode node;
         int num;
-        
-        Pair(TreeNode _node , int _num){
-            this.node = _node;
-            this.num = _num;
+        TreeNode node;
+            Pair( TreeNode _node,int _num){
+                this.num = _num;
+                this.node = _node;
         }
     }
     public int widthOfBinaryTree(TreeNode root) {
         if(root==null)return 0;
-        int ans = 0;
+       int ans = 0;
         Queue<Pair>q = new LinkedList<>();
         q.offer(new Pair(root,0));
         
         while(!q.isEmpty()){
             int size = q.size();
-            int mmin = q.peek().num;
-            int first=0,last=0;
-            
+            int mmin = q.peek().num; //min is the first Pair's num at that level;
+            int first=0,last=0; //first and last for that level;
             for(int i=0;i<size;i++){
-                int curr_id = q.peek().num-mmin;
-                TreeNode node = q.peek().node;
-                q.poll();
-                if(i==0) first = curr_id;
-                if(i==size-1) last = curr_id;
-                
+                int curr_id = q.peek().num-mmin; // curr_id needs to be set for each                                             node in that level ranging from 1 to node n;
+                TreeNode node = q.peek().node; //get the node
+                q.poll(); //remove the node;
+                if(i==0)first = curr_id; //first will be initialized at first node at                                              that level
+                if(i==size-1) last = curr_id; //as obviously last node at that leevel                                                   will be the last curr_id at that level.
+                //for left node first curr_id*2+1
                 if(node.left!=null){
                     q.offer(new Pair(node.left,curr_id*2+1));
                 }
+                //for right node second curr_id*2+2
                 if(node.right!=null){
                     q.offer(new Pair(node.right,curr_id*2+2));
                 }
             }
+            //Maximize answer at that level;
             ans = Math.max(ans,last-first+1);
+            
         }
         return ans;
     }
