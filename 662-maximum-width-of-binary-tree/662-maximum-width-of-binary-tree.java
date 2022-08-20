3d9d64a9e7f85373b5f30e74ec395e132b5308c6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-20 17:12:51 +0530

    Time: 3 ms (57.80%), Space: 44.3 MB (16.15%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Pair{
+        TreeNode node;
+        int num;
+        
+        Pair(TreeNode _node , int _num){
+            this.node = _node;
+            this.num = _num;
+        }
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root==null)return 0;
+        int ans = 0;
+        Queue<Pair>q = new LinkedList<>();
+        q.offer(new Pair(root,0));
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            int mmin = q.peek().num;
+            int first=0,last=0;
+            
+            for(int i=0;i<size;i++){
+                int curr_id = q.peek().num-mmin;
+                TreeNode node = q.peek().node;
+                q.poll();
+                if(i==0) first = curr_id;
+                if(i==size-1) last = curr_id;
+                
+                if(node.left!=null){
+                    q.offer(new Pair(node.left,curr_id*2+1));
+                }
+                if(node.right!=null){
+                    q.offer(new Pair(node.right,curr_id*2+2));
+                }
+            }
+            ans = Math.max(ans,last-first+1);
+        }
+        return ans;
+    }
+}
