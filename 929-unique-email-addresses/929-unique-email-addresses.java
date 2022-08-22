9e8f61f82e3224bbbfbfc2f033820d2b6e930056
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-22 12:00:07 +0530

    Time: 49 ms (23.94%), Space: 54.9 MB (9.84%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.java
+++ 929-unique-email-addresses/929-unique-email-addresses.java
@@ -1,22 +1,21 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        HashSet<String> hs = new HashSet<>();
+       HashSet<String>unique_emails = new HashSet<>();
         
         for(String email:emails){
             int seperation = email.indexOf("@");
-            String local_name = email.substring(0,seperation);
+            String local_name = email.substring(0,seperation); //starting inclusive [)
             String domain_name = email.substring(seperation);
-            
+        
             if(local_name.contains("+")){
                 int seperation2 = local_name.indexOf("+");
-                 local_name = local_name.substring(0,seperation2);
+                local_name = local_name.substring(0,seperation2);
             }
             
             local_name = local_name.replaceAll("\\.","");
-            String full_id = local_name + domain_name;
-            hs.add(full_id);
+            String email_id = local_name + domain_name;
+            unique_emails.add(email_id);
         }
-        
-        return hs.size();
+        return unique_emails.size();
     }
 }
