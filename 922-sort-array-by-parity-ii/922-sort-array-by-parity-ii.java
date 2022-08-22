be1dbd8d5e7eb2325756bd0c9015adb7e928624f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-22 18:07:19 +0530

    Time: 4 ms (62.03%), Space: 54.5 MB (81.04%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int e = 0;
+        int o = 1;
+        int n = nums.length-1;
+        while(e<=n && o <= n){
+            while(e<=n && nums[e]%2==0){
+                e+=2;
+            }
+            
+            while(o<=n && nums[o]%2!=0){
+                o+=2;
+            }
+            if(e>n || o>n)break;
+            swap(e,o,nums);
+        }
+        return nums;
+    }
+    public void swap(int idx1,int idx2,int[] nums){
+        int temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
+    }
+}
