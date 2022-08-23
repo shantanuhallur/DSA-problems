2c03881c80aa9153d5be6e434936e47312537a68
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-23 13:32:25 +0530

    Time: 27 ms (81.93%), Space: 89.8 MB (42.14%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    public int[] getWord2Freq(String word2){
+        int[] word2_freq = new int[26];
+        for(char ch:word2.toCharArray()){
+            word2_freq[ch-'a']++;
+        }
+        return word2_freq;
+    }
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        List<String> ans = new ArrayList<>();
+        int[] word2Max_freq = new int[26];
+        for(String word2:words2){
+            int[] word2_freq = getWord2Freq(word2);
+            
+            for(int i=0;i<26;i++){
+                word2Max_freq[i] = Math.max(word2Max_freq[i],word2_freq[i]);               
+            }
+        }
+        
+        for(String word1:words1){
+            boolean isSubset = true;
+            int[] word1_freq = new int[26];
+            for(char ch:word1.toCharArray()){
+                word1_freq[ch-'a']++;
+            }
+            
+            for(int i=0;i<26;i++){
+                if(word2Max_freq[i]>0){
+                    if(word2Max_freq[i]>word1_freq[i]){
+                        isSubset = false;
+                        break;
+                    }
+                }
+            }
+            if(isSubset)ans.add(word1);
+        }
+        
+        
+        
+        return ans;
+    }
+}
