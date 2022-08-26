cafa18a769546289a56139f55c5eafec748e94f0
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-26 12:43:08 +0530

    Time: 2 ms (32.05%), Space: 43.9 MB (74.48%) - LeetHub

--- 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.java
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.java
@@ -1,30 +1,21 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int[] max_height_row = new int[row];
-        int[] max_height_col = new int[col];
-        int max_row=0;
-        int max_col=0;
+        int n = grid.length;
+        
+        int[] max_height_row = new int[n];
+        int[] max_height_col = new int[n];
         int answer=0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                max_row = Math.max(max_row,grid[i][j]);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                 max_height_row[i] = Math.max(max_height_row[i],grid[i][j]);
+                 max_height_col[j] = Math.max(max_height_col[j],grid[i][j]);
             }
-            max_height_row[i] = max_row; 
-            max_row=0;
+           
         }
         
-        for(int i=0;i<col;i++){
-            for(int j=0;j<row;j++){
-                max_col = Math.max(max_col,grid[j][i]);
-            }
-            max_height_col[i] = max_col; 
-            max_col = 0;
-        }
-        
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
                 answer += Math.min(max_height_row[i], max_height_col[j]) -grid[i][j];   
             }
         }
