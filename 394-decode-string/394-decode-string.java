808221aa4c402867b68b335b437945e433359daf
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-28 11:13:19 +0530

    Time: 10 ms (11.83%), Space: 42.4 MB (35.87%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -6,30 +6,29 @@ class Solution {
         int idx = 0;
         while(idx<s.length()){
             if(Character.isDigit(s.charAt(idx))){
-                int count=0;
+                int count = 0;
                 while(Character.isDigit(s.charAt(idx))){
-                    count = 10 * count + (s.charAt(idx) -'0');
+                    count = count * 10 + (s.charAt(idx)-'0');
                     idx++;
                 }
-                      counts.push(count);
-            }
-            else if(s.charAt(idx)==']'){
-                   StringBuilder temp = new StringBuilder(result.pop());
-                int noOfTimes = counts.pop();
-                for(int i=0;i<noOfTimes;i++){
-                    temp.append(res);
-                }
-                res= temp.toString();
-                idx++;
+                counts.push(count);
             }
             else if(s.charAt(idx)=='['){
                 result.push(res);
                 res="";
                 idx++;
             }
+            else if(s.charAt(idx)==']'){
+               StringBuilder temp = new StringBuilder(result.pop());
+                int noOfTimes = counts.pop();
+                for(int i=0;i<noOfTimes;i++){
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            }
             else{
-             
-                res+= s.charAt(idx);
+                res +=   s.charAt(idx);
                 idx++;
             }
         }
