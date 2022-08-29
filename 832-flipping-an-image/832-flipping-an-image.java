fb07186f0c3959ffce3b2a6de14ddf17318c4d63
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-08-29 16:03:35 +0530

    Time: 1 ms (80.64%), Space: 45 MB (41.24%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.java
+++ 832-flipping-an-image/832-flipping-an-image.java
@@ -6,8 +6,8 @@ class Solution {
             int high=image[0].length-1;
             while(low<=high){
                 if(image[i][low]==image[i][high]){
-                    image[i][low] = 1 - image[i][low]; //trick to Flip 1s to 0s and 
-                    image[i][high] = image[i][low]; //0s to 1s.
+                    image[i][high] = 1 - image[i][high]; //trick to Flip 1s to 0s and 
+                    image[i][low] = image[i][high]; //0s to 1s.
                 }
                 low++;
                 high--;
