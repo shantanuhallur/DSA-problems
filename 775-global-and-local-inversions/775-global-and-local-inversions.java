429c72022201ab3ceb7019e2673a9f4eb9d894ed
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-09-07 21:37:11 +0530

    Time: 2 ms (86.97%), Space: 79.8 MB (31.65%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.java
+++ 775-global-and-local-inversions/775-global-and-local-inversions.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isIdealPermutation(int[] nums) {
         int max = -1;
+//As local inversions are subset of global inversions, just find one global inversion which is not a local inversion which will prove that global inver> local inver.
         for(int i=0;i<nums.length-2;i++){
+            //max has the highest likelyhood of being bigger than that element of                   index+2 || not index + 1 as else it will bee a local inversion as well.
             max = Math.max(max,nums[i]);
             if(max>nums[i+2])return false;
         }
