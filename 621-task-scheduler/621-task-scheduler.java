e82052656a3975c451a552650297be575c2121cf
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-09-09 20:33:40 +0530

    Time: 2 ms (98.91%), Space: 43.2 MB (96.99%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        
+        int[] char_map = new int[26];
+        for(char ch:tasks){
+            char_map[ch-'A']++;
+        }
+        
+        Arrays.sort(char_map);
+        int max_val = char_map[25]-1; //-1 because we dont care if they are at end we dont                              have to fill them as we have already finished all tasks.
+        int idle_slot = max_val*n;
+        for(int i=24;i>=0;i--){
+            idle_slot -= Math.min(char_map[i],max_val);
+        }
+        
+        return idle_slot >0 ? idle_slot + tasks.length : tasks.length;
+    }
+}
