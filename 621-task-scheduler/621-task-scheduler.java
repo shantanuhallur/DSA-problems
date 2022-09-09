8b6ce3de734e1c3e246e5d0eadcf56d89a5d0812
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-09-09 20:40:29 +0530

    Time: 11 ms (55.30%), Space: 58.3 MB (84.37%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -10,7 +10,7 @@ class Solution {
         int max_val = char_map[25]-1; //-1 because we dont care if they are at end we dont                              have to fill them as we have already finished all tasks.
         int idle_slot = max_val*n;
         for(int i=24;i>=0;i--){
-            idle_slot -= Math.min(char_map[i],max_val);
+            idle_slot -= Math.min(char_map[i],max_val); //filling up all tasks evenly in                                            idle spots greedyly.
         }
         
         return idle_slot >0 ? idle_slot + tasks.length : tasks.length;
