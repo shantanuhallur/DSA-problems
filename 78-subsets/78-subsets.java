3505e9f73391b5924dbf0630c8dff87cef9b523f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-09-13 21:53:34 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (97.65%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> curr = new ArrayList<>();
         if(nums.length==0)return res;
-        getSubsets(nums,0,new ArrayList<Integer>(),res);
+        getSubsets(nums,0,curr,res);
         return res;
     }
     
-    void getSubsets(int[] nums,int idx, ArrayList<Integer> output,List<List<Integer>> res){
+    void getSubsets(int[] nums,int idx, ArrayList<Integer> curr,List<List<Integer>> res){
         if(idx==nums.length){
-            res.add(output);
+            res.add(new ArrayList<>(curr));
             return;
         }
        
         
         //include
-        output.add(nums[idx]);
-        getSubsets(nums,idx+1,new ArrayList<>(output),res);
-        output.remove(output.size()-1);
+        curr.add(nums[idx]);
+        getSubsets(nums,idx+1,curr,res);
+        curr.remove(curr.size()-1);
          //exclude
-        getSubsets(nums,idx+1,new ArrayList<>(output),res);
+        getSubsets(nums,idx+1,curr,res);
         
         //can do in both ways but if you include first always backtrack to remove the element, if you dont include first you are good to go.
     }
