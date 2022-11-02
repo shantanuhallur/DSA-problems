0fcab3a3b9d253d170d220b82cdd39e92005ddd3
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-11-02 09:21:54 +0530

    Time: 3 ms (90.51%), Space: 45.1 MB (46.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -16,8 +16,6 @@ class Solution {
     }
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        //JUST SORT ARRAY FOR THIS UNIQUE TWIST.
-        Arrays.sort(candidates);
          List<List<Integer>> ans = new ArrayList<>();
          List<Integer> psf = new ArrayList<>();
          combinationSum(candidates,target,0,psf,ans);
