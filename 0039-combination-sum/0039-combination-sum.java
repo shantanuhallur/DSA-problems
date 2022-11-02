3f9c08f817ca429830c29f0aa769c2f563e1cf05
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-11-02 08:45:16 +0530

    Time: 3 ms (90.51%), Space: 45.1 MB (46.25%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void combinationSum(int[] candidates, int target,int idx,List<Integer> psf ,List<List<Integer>> ans) {
+        if(target==0){
+            List<Integer> base = new ArrayList<>(psf);
+            ans.add(base);
+            return;
+        }
+        
+        for(int i = idx; i<candidates.length;i++){
+            if(target - candidates[i]>=0){
+                psf.add(candidates[i]);
+                combinationSum(candidates,target-candidates[i],i,psf,ans);
+                psf.remove(psf.size()-1);
+            }
+        }
+    }
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+         List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> psf = new ArrayList<>();
+         combinationSum(candidates,target,0,psf,ans);
+        return ans;
+    }
+}
