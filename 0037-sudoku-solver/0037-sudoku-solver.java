998adca1fe8a64adc19c3287a855e5e31566b235
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-11-12 16:41:30 +0530

    Time: 9 ms (85.04%), Space: 41.4 MB (61.91%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,22 +1,25 @@
 class Solution {
-    public boolean isValid(int r, int c , char[][] board,int num) {
+    int[] row = new int[10];
+    int[] col = new int[10];
+    int[][] mat = new int[3][3];
+    
+    public boolean isValid(int r, int c ,int num) {
+        int mask = 1<<num;
         //Row    
-        for(int j=0;j<9;j++){
-                if(board[r][j]-'0' == num) return false;                
-            }
+        if((row[r] & mask) != 0)return false;
         //Col
-        for(int i=0; i<9; i++){
-                if(board[i][c]-'0' == num) return false;                
-            }
+       if((col[c] & mask) != 0)return false;
         //3X3
-        r = (r/3)*3;
-        c = (c/3)*3;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(board[r+i][c+j]-'0' == num)return false;
-            }
-        }
-            return true;
+       if((mat[r/3][c/3] & mask) != 0)return false;
+        
+        return true;
+    }
+    
+    public void toggle(int r,int c,int num) {
+        int mask = 1<<num;
+        row[r] ^= mask;
+        col[c] ^= mask;
+        mat[r/3][c/3] ^= mask;
     }
     
     public boolean SudokuSolver(int idx, ArrayList<Integer> loc, char[][] board) {
@@ -29,11 +32,13 @@ class Solution {
         boolean res = false;
 
         for(int number=1;number<=9;number++) {
-            if(isValid(r,c,board,number)) {
+            if(isValid(r,c,number)) {
                 board[r][c] = (char)('0' + number);
+                toggle(r,c,number);
                 res = res || SudokuSolver(idx+1,loc,board);
                 if(res) return true;
                 board[r][c] = '.';
+                toggle(r,c,number);
             }
         }
         return res;
@@ -43,7 +48,10 @@ class Solution {
         ArrayList<Integer> loc = new ArrayList<>();
         for(int i=0;i<board.length;i++) {
             for(int j=0; j<board[0].length;j++) {
-                if(board[i][j]=='.')loc.add((i*9) + j);
+                if(board[i][j]=='.')
+                    loc.add((i*9) + j);
+                else
+                    toggle(i,j,board[i][j]-'0');
             }
         }
        SudokuSolver(0,loc,board);
