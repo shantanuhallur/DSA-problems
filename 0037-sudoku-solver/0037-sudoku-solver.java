46021469736047e19b23fe14e9938eb7b3c37401
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-11-14 09:04:18 +0530

    Time: 7 ms (90.81%), Space: 41.9 MB (28.67%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,48 +1,45 @@
 class Solution {
+    
     int[] row = new int[10];
     int[] col = new int[10];
     int[][] mat = new int[3][3];
     
-    public void toggle(int r,int c,int num) {
+    public void toggle(int r , int c , int num) {
         int mask = 1<<num;
         row[r] ^= mask;
         col[c] ^= mask;
         mat[r/3][c/3] ^= mask;
     }
     
-    public boolean SudokuSolver(int idx, ArrayList<Integer> loc, char[][] board) {
-        if(idx==loc.size()) {
+    public boolean sudokuSolver(ArrayList<Integer> list,int idx,char[][] board) {
+        if(idx == list.size()) {
             return true;
         }
-        
-        int r = loc.get(idx)/9;
-        int c = loc.get(idx)%9;
+        int r = list.get(idx)/9;
+        int c = list.get(idx)%9;
         boolean res = false;
-
-        for(int number=1;number<=9;number++) {
+        for(int number = 1; number<=9;number++) {
             int mask = 1<<number;
-            if((row[r] & mask) == 0 && (col[c] & mask) == 0 && (mat[r/3][c/3] & mask) == 0) {
-                board[r][c] = (char)('0' + number);
+            if((row[r]&mask)== 0 && (col[c]&mask)==0 && (mat[r/3][c/3]&mask)==0) {
+                board[r][c] = (char)(number+'0');
                 toggle(r,c,number);
-                res = res || SudokuSolver(idx+1,loc,board);
-                if(res) return true;
+                res = res | sudokuSolver(list,idx+1,board);
+                    if(res)return true;
                 board[r][c] = '.';
                 toggle(r,c,number);
             }
         }
         return res;
     }
-   
+    
     public void solveSudoku(char[][] board) {
-        ArrayList<Integer> loc = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i=0;i<board.length;i++) {
-            for(int j=0; j<board[0].length;j++) {
-                if(board[i][j]=='.')
-                    loc.add((i*9) + j);
-                else
-                    toggle(i,j,board[i][j]-'0'); //set the number true in row col and matrix.
+            for(int j=0;j<board[0].length;j++) {
+                if(board[i][j]=='.')list.add(i*9 + j);
+                else toggle(i,j,board[i][j]-'0');
             }
         }
-       SudokuSolver(0,loc,board);
+        sudokuSolver(list,0,board);
     }
 }
