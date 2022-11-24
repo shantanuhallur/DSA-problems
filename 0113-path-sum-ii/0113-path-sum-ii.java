9b54496550db30a2d4bda0292adb5ec769861ebb
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-11-24 07:03:56 +0530

    Time: 2 ms (85.38%), Space: 44.4 MB (66.84%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public void pathSum(TreeNode root, int targetSum, List<List<Integer>> ans,List<Integer> curr) {
+        if(root == null) return;
+        
+        if(root.left == null && root.right == null && targetSum-root.val==0) {
+            // if( targetSum-root.val==0){
+              ArrayList<Integer> base = new ArrayList<>(curr);
+                base.add(root.val);
+                ans.add(base);   
+            // }
+            return; //we have to stop whenever we leach a leaf or else next calls fail at val.
+        }
+        curr.add(root.val);
+        
+        pathSum(root.left,targetSum-root.val,ans,curr);
+        pathSum(root.right,targetSum-root.val,ans,curr);
+        
+        curr.remove(curr.size()-1);
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        pathSum(root,targetSum,ans,new ArrayList<>());
+        return ans;
+    }
+}
