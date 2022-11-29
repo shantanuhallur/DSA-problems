c0d620cc1a4ec40f5a4d542b9bcdb1bfccf00aac
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-11-29 07:45:43 +0530

    Time: 2 ms (17.00%), Space: 44.9 MB (81.80%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,23 +14,43 @@
  * }
  */
 class Solution {
-    int maxSumNTN=-(int)1e9;
-    public int maxPathSum_01(TreeNode root) {
-        if(root==null) return 0;
+    public static class maxNodeToNodePair {
+        int NTN_sum = -(int)1e9; // Node To Node Sum.
+        int NTR_sum = 0;   // Root To Node Sum.
+
+        maxNodeToNodePair(int NTN_sum , int NTR_sum) {
+            this.NTN_sum = NTN_sum;
+            this.NTR_sum = NTR_sum;
+        }
+
+        maxNodeToNodePair() {}
+    }
+
+    public static int maxCompare(int... arr) {
+        int max = arr[0];
+        for(int ele : arr) {
+            max = Math.max(ele,max);
+        }
+        return max;
+    }
+    
+    public maxNodeToNodePair maxPathSumHard_02(TreeNode root) {
+        if(root==null) return new maxNodeToNodePair();
         
-        int leftMaxPathSum =  maxPathSum_01(root.left);
-        int rightMaxPathSum =  maxPathSum_01(root.right);
+        maxNodeToNodePair lans =  maxPathSumHard_02(root.left);
+        maxNodeToNodePair rans =  maxPathSumHard_02(root.right);
+
+        maxNodeToNodePair myAns = new maxNodeToNodePair();
+        myAns.NTN_sum = maxCompare(lans.NTN_sum,rans.NTN_sum,root.val,lans.NTR_sum + root.val,
+                                   rans.NTR_sum + root.val,lans.NTR_sum + rans.NTR_sum + root.val);
+        myAns.NTR_sum = maxCompare(root.val, lans.NTR_sum + root.val, rans.NTR_sum + root.val);
         
-       int MaxTillRoot = Math.max(leftMaxPathSum,rightMaxPathSum) + root.val;
-       maxSumNTN =  Math.max(Math.max(maxSumNTN,MaxTillRoot),Math.max(root.val,leftMaxPathSum+rightMaxPathSum+root.val));
-        //maxCalculation.
-        
-        return Math.max(MaxTillRoot,root.val); //faith
+       return myAns;
     }
     
     public int maxPathSum(TreeNode root) {
         if(root==null)return 0;
-        maxPathSum_01(root);
-        return maxSumNTN;
+        
+        return maxPathSumHard_02(root).NTN_sum;
     }
 }
