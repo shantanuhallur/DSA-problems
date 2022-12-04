4004b3fe094fe3e80a281e5c5f77db45fc9b453d
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-04 19:30:10 +0530

    Time: 0 ms (100.00%), Space: 49.6 MB (55.92%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static int maxEle(TreeNode root) {
+        TreeNode curr = root;
+
+        while (curr.right != null) {
+            curr = curr.right;
+        }
+
+        return curr.val;
+    }
+    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key)
+            root.right = deleteNode(root.right, key);
+        else {
+            if (root.left == null || root.right == null) {
+                return root.left == null ? root.right : root.left;
+            }
+
+            int maxValue = maxEle(root.left); // get max from left or min from right the node to delete will                                                    always
+            root.val = maxValue; // be a leaf or one with 1 subtree only never a node with 2 subtrees
+                                 // as it will then not be the max or min node of that subtree.
+            root.left = deleteNode(root.left, maxValue);
+        }
+
+        return root;
+    }
+}
