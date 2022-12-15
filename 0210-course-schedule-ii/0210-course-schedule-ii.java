2eb0f91ce2ba7a07d77e1412d4ebf3486b9c12b4
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-15 19:49:43 +0530

    Time: 5 ms (92.62%), Space: 43.1 MB (95.95%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -20,7 +20,7 @@ class Solution {
             int size = que.size();
             while(size-- >0){
                 int rvtx = que.removeFirst();
-                ans.addFirst(rvtx);
+                ans.addLast(rvtx);
 
                 for(int e : graph.get(rvtx)) {
                     // inDegree[e.v]--;
@@ -50,7 +50,7 @@ class Solution {
         if(ans.size() != numCourses) return new int[0];
         int[] Rans = new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            Rans[i] = ans.removeFirst();
+            Rans[i] = ans.removeLast();
     }
         
         return Rans;
