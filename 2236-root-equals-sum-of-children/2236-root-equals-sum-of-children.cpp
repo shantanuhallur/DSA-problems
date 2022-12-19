2e7bc7157f0aa3555656f639ed29884793f4328e
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-19 19:53:36 +0530

    Time: 9 ms (16.86%), Space: 12.6 MB (15.81%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -12,6 +12,6 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        return root->left->val + root->right->val == root->val ? true : false;
+       return root->val == root->left->val + root->right->val ? true : false;
     }
 };
