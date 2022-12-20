8284bc0f68164263ccbae93b99157cf01c5b73db
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-20 17:24:48 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (81.77%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -14,32 +14,42 @@
  * }
  */
 class Solution {
-    public void getleafS(TreeNode node, ArrayList<Integer> ans) {
-        if(node == null) return;
+    public void fillList(TreeNode node,ArrayList<Integer> list) {
+        //Base case
         if(node.left == null && node.right == null) {
-            ans.add(node.val);
+            list.add(node.val);
             return;
         }
         
-            getleafS(node.left,ans);            
-
-            getleafS(node.right,ans);            
+        //Left Recursive Call
+        if(node.left!=null) {
+            fillList(node.left,list);
+        }
+        
+        //Right Recursive Call
+        if(node.right !=null) {
+            fillList(node.right,list);
+        }
+        
     }
     
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        ArrayList<Integer> ans1 = new ArrayList<>(); 
-        ArrayList<Integer> ans2 = new ArrayList<>();
-        getleafS(root1,ans1);
-        getleafS(root2,ans2);
-        int i = ans1.size();
-        int j = ans2.size();
-        
-        if(i != j) return false;
-        
-        while(--i>=0){
-            if(ans1.get(i) != ans2.get(i)) return false;
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        //filled both lists
+        fillList(root1,list1);
+        fillList(root2,list2);
+        //checked if both lists are of same size
+        int list1Size = list1.size();
+        int list2Size = list2.size();
+        //if not all the leaves are not same
+        if(list1Size != list2Size) return false;
+        //checked if all leaves are same
+        while(--list1Size >=0) {
+            //if no we return false
+            if(list1.get(list1Size) != list2.get(list1Size)) return false;
         }
-        
+        //if yes we return true
         return true;
     }
 }
