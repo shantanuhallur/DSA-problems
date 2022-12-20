e1a9961ccf87ca70efde57a571b8a676275f0aac
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-20 06:08:18 +0530

    Time: 1 ms (69.76%), Space: 42.1 MB (46.75%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void getleafS(TreeNode node, ArrayList<Integer> ans) {
+        if(node == null) return;
+        if(node.left == null && node.right == null) {
+            ans.add(node.val);
+            return;
+        }
+        
+        if(node.left != null) {
+            getleafS(node.left,ans);            
+        }
+        
+        if(node.right != null) {
+            getleafS(node.right,ans);            
+        }
+    }
+    
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> ans1 = new ArrayList<>(); 
+        ArrayList<Integer> ans2 = new ArrayList<>();
+        getleafS(root1,ans1);
+        getleafS(root2,ans2);
+        int i = ans1.size();
+        int j = ans2.size();
+        
+        if(i != j) return false;
+        
+        while(--i>=0){
+            if(ans1.get(i) != ans2.get(i)) return false;
+        }
+        
+        return true;
+    }
+}
