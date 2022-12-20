e4788e35b8e3c0706c890f19d4651482e9436473
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-20 15:16:32 +0530

    Time: 18 ms (89.63%), Space: 20.6 MB (22.41%) - LeetHub

--- 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.cpp
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.cpp
@@ -11,22 +11,26 @@
  */
 class Solution {
 public:
-    bool getLonelyNodes_01(TreeNode* node, vector<int>& ans) {
+    bool getLonelyNodes_01(TreeNode* node,vector<int>& ans) {
+    //Base case
         if(node == nullptr) return false;
         
-        bool leftChild = getLonelyNodes_01(node->left,ans);
-        bool rightChild = getLonelyNodes_01(node->right,ans);
-        
-        if(leftChild && !rightChild) {
+        //Left Recursive Call
+        bool hasLeftChild = getLonelyNodes_01(node->left,ans);
+        //Right Recursive Call
+        bool hasRightChild = getLonelyNodes_01(node->right,ans);
+        //if has left Child but not Right Child
+        if(hasLeftChild && !hasRightChild) {
             ans.push_back(node->left->val);
         }
-        
-        if(rightChild && !leftChild) {
+        //if has right Child but not left Child
+        if(hasRightChild && !hasLeftChild) {
             ans.push_back(node->right->val);
         }
         
         return true;
-    } 
+    }
+    
     vector<int> getLonelyNodes(TreeNode* root) {
         vector<int> ans;
         getLonelyNodes_01(root,ans);
