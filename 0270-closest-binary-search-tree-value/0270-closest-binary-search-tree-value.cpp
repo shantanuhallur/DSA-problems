ee1c76cb8087f0d0e814a85641e5ac073d8114af
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-21 07:48:13 +0530

    Time: 27 ms (26.69%), Space: 21 MB (6.93%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
@@ -11,27 +11,15 @@
  */
 class Solution {
 public:
-    int closestVal = -1;
-    double minDiff = 1e10;
-    
-    void closestValue_01(TreeNode* node, double target) {
-        if(abs(node->val - target) < minDiff) {
-            minDiff = abs(node->val - target);
-            closestVal = node->val;
-        }
-        
-        if(node->left != nullptr) {
-            closestValue_01(node->left,target);
-        }
-        
-        if(node->right != nullptr) {
-            closestValue_01(node->right,target);
-        }
-        
-    }
-    
     int closestValue(TreeNode* root, double target) {
-        closestValue_01(root,target);
-        return closestVal;
+        int val , closest = root->val;
+        while(root != nullptr) {
+            val = root->val;
+            closest = abs(val - target) < abs(closest - target) ? val : closest;
+            
+            root = target < root->val ? root->left : root-> right; 
+        }
+        
+        return closest;
     }
 };
