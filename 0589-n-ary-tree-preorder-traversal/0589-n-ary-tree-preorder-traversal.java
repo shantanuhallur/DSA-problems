fe5b12acd8eecc1e44abd5c7bcb4d0726ccb836a
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-22 17:08:21 +0530

    Time: 2 ms (65.13%), Space: 43.1 MB (81.60%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -20,19 +20,20 @@ class Node {
 class Solution {
     public List<Integer> preorder(Node root) {
         ArrayList<Integer> ans = new ArrayList<>();
-        if(root==null) return ans;
+        if(root == null) return ans;
+        //Stack
         LinkedList<Node> st = new LinkedList<>();
         
         st.addLast(root);
         
         while(st.size()!=0) {
-            Node rn = st.removeLast();
+            Node removeN = st.removeLast();
             
-            for(int i = rn.children.size()-1;i>=0;i--){
-                st.addLast(rn.children.get(i));
+            for(int i=removeN.children.size()-1 ; i >=0 ; i--) {
+                st.addLast(removeN.children.get(i));
             }
             
-            ans.add(rn.val);
+            ans.add(removeN.val);
         }
         
         return ans;
