0f3fdff2249ec183d858d88360438d8359a5b585
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-22 15:05:46 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (95.11%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.java
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.java
@@ -15,45 +15,48 @@
  */
 class Solution {
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        //Base Case
         if(depth == 1) {
             TreeNode base = new TreeNode(val);
             base.left = root;
             return base;
         }
-        
+        //Create a level for while loop's break condition
         int level = 1;
-        
         LinkedList<TreeNode> que = new LinkedList<>();
-        
         que.addLast(root);
         
-        while(level < depth-1) {
+        while(level < depth -1){
             int size = que.size();
-            while(size-- >0) {
-                TreeNode rn = que.removeFirst();
-                
-                if(rn.left != null) {
-                    que.addLast(rn.left);
+            while(size-->0) {
+                //Remove the Node from the que
+                TreeNode removeN = que.removeFirst();
+                //Add the node's children if exist.
+                if(removeN.left != null) {
+                    que.addLast(removeN.left);
                 }
-                
-                if(rn.right != null) {
-                    que.addLast(rn.right);
+                if(removeN.right != null) {
+                    que.addLast(removeN.right);
                 }
             }
             level++;
         }
         
-        while(que.size() != 0) {
-            TreeNode rn = que.removeFirst();
-            
-                TreeNode temp1 = rn.left;
-                TreeNode temp2 = rn.right;
-                rn.left = new TreeNode(val);
-                rn.right = new TreeNode(val);
-                rn.left.left = temp1;
-                rn.right.right = temp2;
-            }
-            
+        while(que.size()!=0) {
+            TreeNode removeN = que.removeFirst();
+            // Create temp nodes for not loosing our subtrees in left or right.
+            TreeNode temp1 = removeN.left;
+            TreeNode temp2 = removeN.right;
+            //create nodes we want to add.
+            TreeNode leftN = new TreeNode(val);
+            TreeNode rightN = new TreeNode(val);
+            //attach these nodes
+            removeN.left = leftN;
+            removeN.right = rightN;
+            leftN.left = temp1;
+            rightN.right = temp2;
+        }
+        
         return root;
     }
 }
