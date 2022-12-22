cef8006a5bb3dfd047dbc4075b11c0d3d289ac8e
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-22 17:11:48 +0530

    Time: 26 ms (84.35%), Space: 11 MB (98.45%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -22,19 +22,22 @@ class Solution {
 public:
     vector<int> preorder(Node* root) {
         vector<int> ans;
+        // Base Condition
         if(root == nullptr) return ans;
-        
+        //Addition of Root in new stack
         stack<Node*> st;
         st.push(root);
-        
+        // to iterate over all the children of my root and their own children in inorder.
         while(st.size()!=0) {
-              Node* rn = st.top(); st.pop();
+            Node* removeN = st.top(); st.pop();
             
-            for(int i= rn->children.size()-1;i>=0;i--) {
-                st.push(rn->children[i]);
+            for(int i=removeN->children.size()-1 ; i>=0;i--) {
+                st.push(removeN->children[i]);
             }
-            ans.push_back(rn->val);
+            //add the current removeN in answer.
+            ans.push_back(removeN->val);
         }
+        
         return ans;
     }
 };
