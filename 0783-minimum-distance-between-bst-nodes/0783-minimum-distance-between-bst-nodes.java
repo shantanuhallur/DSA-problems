0eb3dace72893d77ebebd13ad1dc550027eada77
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-23 16:08:47 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (37.88%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
@@ -14,39 +14,23 @@
  * }
  */
 class Solution {
-
-    public int getMin(int... arr) {
-        int min = arr[0];
-        for(int ele : arr) {
-            min = Math.min(ele,min);
+    int minDiff = Integer.MAX_VALUE;
+    TreeNode prev = null;
+    public void minDiffInBST_01(TreeNode node) {
+        if(node==null) return;
+        minDiffInBST_01(node.left);
+        
+        if(prev!=null){
+            int diff = node.val-prev.val;
+            minDiff = Math.min(minDiff,diff);
         }
-        return min;
-    }
-    
-    int ans = (int)1e8;
-    public int[] getMinMax(TreeNode node) {
-        if(node == null) return new int[]{(int)1e8,-(int)1e8};
-            
-        int[] leftMinMax =  getMinMax(node.left);
-           
-        int[] rightMinMax = getMinMax(node.right);
         
-        //ans
-        ans = getMin(ans,Math.abs(leftMinMax[0]-node.val),Math.abs(leftMinMax[1]-node.val),Math.abs(rightMinMax[0]-node.val),
-            Math.abs(rightMinMax[1]-node.val));
-        
-        int[] myNodeMinMax = new int[2];
-        
-        //min
-        myNodeMinMax[0] = Math.min(leftMinMax[0],Math.min(node.val,rightMinMax[0]));
-        //max
-        myNodeMinMax[1] = Math.max(leftMinMax[1],Math.max(node.val,rightMinMax[1]));
-        
-        return myNodeMinMax; 
+        prev=node;
+        minDiffInBST_01(node.right);
     }
     
     public int minDiffInBST(TreeNode root) {
-        int[] dummy = getMinMax(root);
-        return ans;
+        minDiffInBST_01(root);
+        return minDiff;
     }
 }
