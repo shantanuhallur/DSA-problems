d8fc96a495979c7f633ba3a965afe7d858aa3ce6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-23 09:30:46 +0530

    Time: 40 ms (24.10%), Space: 47.3 MB (38.15%) - LeetHub

---
+++ 0919-complete-binary-tree-inserter/0919-complete-binary-tree-inserter.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class CBTInserter {
+    TreeNode h = null;
+    TreeNode currPtr = null;
+    Queue<TreeNode> que = new LinkedList<>();
+    public CBTInserter(TreeNode root) {
+        h = root;
+        que.add(root);
+        
+        while(true) {
+            TreeNode rn = que.peek();
+            currPtr = que.peek();
+            if(rn.left !=null) {
+                que.add(rn.left);
+            }
+            else break;
+            
+            if(rn.right !=null) {
+                que.add(rn.right);
+            }
+            else break;
+            
+            que.poll();
+        }
+        
+        
+    }
+    
+    public int insert(int val) {
+        int rnVal = 0;
+        if(currPtr.left == null){
+            TreeNode node = new TreeNode(val);
+            currPtr.left = node;
+            que.add(node);
+            rnVal = currPtr.val;
+        }
+        else{
+            TreeNode node = new TreeNode(val);
+            currPtr.right = node;
+            que.add(node);
+            rnVal = currPtr.val;
+            que.poll();
+            currPtr = que.peek();
+            
+        }
+        return rnVal;
+    }
+    
+    public TreeNode get_root() {
+        return h;
+    }
+}
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter obj = new CBTInserter(root);
+ * int param_1 = obj.insert(val);
+ * TreeNode param_2 = obj.get_root();
+ */
