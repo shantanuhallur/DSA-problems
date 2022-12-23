5a853da2cb71e22fe5d196f04b5bdcdd0e51208a
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-23 19:33:27 +0530

    Time: 300 ms (45.97%), Space: 64.8 MB (21.77%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -11,23 +11,19 @@
  */
 class Solution {
 public:
-    
     int rangeSum = 0;
-    void rangeSumBST_01(TreeNode* root, int low, int high) {
-        if(root == nullptr) return;
+    void rangeSumBST_01(TreeNode* node, int low, int high) {
+        //Base condition
+        if(node == nullptr) return;
+        // If current value is in range
+        if(low <= node->val && node->val <= high) rangeSum += node->val;
+        //Check if we want to recurse to left or right
+        if(low<= node->val) rangeSumBST_01(node->left,low,high);
         
-        if(root->val >= low && root->val<= high) {
-            rangeSum += root->val;
-        }
+        if(node->val <= high) rangeSumBST_01(node->right,low,high);
         
-        if(root->val >= low) {
-            rangeSumBST_01(root->left,low,high);
-        }
-        
-        if(root->val <= high) {
-            rangeSumBST_01(root->right,low,high);
-        }
     }
+    
     int rangeSumBST(TreeNode* root, int low, int high) {
         rangeSumBST_01(root,low,high);
         return rangeSum;
