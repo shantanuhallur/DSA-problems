4a3fa7fab106b5ba6bf64484e7f1545bbd1f1006
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-23 18:24:47 +0530

    Time: 125 ms (74.36%), Space: 61.9 MB (27.42%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -12,24 +12,25 @@
 class Solution {
 public:
     int deepestLeavesSum(TreeNode* root) {
+        //Base condn
         if(root == nullptr) return 0;
-        
+        // Create Que
         queue<TreeNode*> que;
         que.push(root);
         int sum = 0;
-        
         while(que.size()!=0) {
             int size = que.size();
             sum = 0;
-            while(size -->0) {
-                TreeNode* removeN = que.front(); que.pop();
+            //maintain sum as 0 for each new level
+            while(size-- > 0) {
                 
+                TreeNode* removeN = que.front(); que.pop();
+                //if left and right child exists add in que.
                 if(removeN->left != nullptr) que.push(removeN->left);
                 
                 if(removeN->right != nullptr) que.push(removeN->right);
-                
+                // sum of last level will get updated in latest sum.
                 sum += removeN->val;
-                cout<<removeN->val;
             }
         }
         
