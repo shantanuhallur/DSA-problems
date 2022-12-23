6fb4af2357411503670ce5f8a4bee3ef525f8f30
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-23 15:29:48 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (82.86%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -14,18 +14,22 @@
  * }
  */
 class Solution {
-    public int sumRootToLeaf_01(TreeNode root, int sum) {
-        if(root == null )return 0;
-        
-        sum = (2*sum) + root.val;
-        if(root.left == null && root.right == null) return sum;
-        int leftSum = sumRootToLeaf_01(root.left,sum);
-        int rightSum = sumRootToLeaf_01(root.right,sum);
-        
-        int totalSum = leftSum+rightSum;
+    public int sumRootToLeaf_01(TreeNode node,int sum) {
+        //Case when our node itself is null so tree doesnt exist.
+        if(node == null) return 0;
+        //calculate Pre sum.
+        sum = (2*sum) + node.val;
+        //Condition for recognizing leaf
+        if(node.left ==null && node.right ==null) return sum;
+        //left Whole left & right subtree's decimal sum
+        int leftSum = sumRootToLeaf_01(node.left,sum);
+        int rightSum = sumRootToLeaf_01(node.right,sum);
+        //return this total Sum.
+        int totalSum = leftSum + rightSum;
         
         return totalSum;
     }
+    
     public int sumRootToLeaf(TreeNode root) {
         return sumRootToLeaf_01(root,0);
     }
