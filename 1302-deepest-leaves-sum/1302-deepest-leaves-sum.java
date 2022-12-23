b4e6087edaeddeff3336461dd4a0e678fda5f273
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-23 18:21:06 +0530

    Time: 12 ms (40.96%), Space: 59.8 MB (21.91%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -15,26 +15,25 @@
  */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
-        if(root==null) return 0;
-        
+        //If tree Doesnt exist
+        if(root == null) return 0;
+        int sum = 0;
         LinkedList<TreeNode> que = new LinkedList<>();
         que.addLast(root);
-        int sum = 0;       
-        while(que.size()!= 0) {
+        //Start Level Order Traversal
+        while(que.size()!=0) {
             int size = que.size();
+            //initialize sum = 0 for each new levl
             sum = 0;
             while(size-- >0) {
+                
                 TreeNode removeN = que.removeFirst();
+                //If left and right exists add in que
+                if(removeN.left != null) que.addLast(removeN.left);
                 
-                if(removeN.left != null) {
-                    que.addLast(removeN.left);
-                }
-                
-                if(removeN.right != null) {
-                    que.addLast(removeN.right);
-                }
-                
-                sum += removeN.val; 
+                if(removeN.right != null) que.addLast(removeN.right);
+                //add all the nodes sum of same level in sum.
+                sum += removeN.val;
             }
         }
         return sum;
