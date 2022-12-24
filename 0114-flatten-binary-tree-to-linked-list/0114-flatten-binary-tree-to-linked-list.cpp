30abc3ac11799e02bfc30e57e11b5a31823c2ce6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-24 16:33:56 +0530

    Time: 9 ms (66.80%), Space: 12.8 MB (39.69%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,23 +11,24 @@
  */
 class Solution {
 public:
-    
     TreeNode* flatten_01(TreeNode* node) {
+        //Base Case to handle Leafs
         if(!node || (!node->left && !node->right)) return node;
-        
+        //Left and Right recursive Call;
         TreeNode* leftTail = flatten_01(node->left);
         TreeNode* rightTail = flatten_01(node->right);
         
-        if(leftTail!=nullptr){
+        //Do the attachments
+        if(leftTail) {
             leftTail->right = node->right;
             node->right = node->left;
             node->left = nullptr;
         }
         
-        return rightTail != nullptr? rightTail : leftTail;
+        return rightTail != nullptr ? rightTail : leftTail;
     }
-        
-    void flatten(TreeNode* node) {
-        flatten_01(node);
+    
+    void flatten(TreeNode* root) {
+        flatten_01(root);
     }
 };
