7e6204cae515d018636a3258efdd95453bc8c56d
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-24 21:09:52 +0530

    Time: 8 ms (47.10%), Space: 12.9 MB (79.07%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -11,22 +11,24 @@
  */
 class Solution {
 public:
-    void binaryTreePaths_01(TreeNode* node,vector<string>& ans,string s) {
+    void binaryTreePaths_01(TreeNode* node,string currPath, vector<string>& ans) {
+        //Base Case 
         if(!node->left && !node->right) {
-            s+= to_string(node->val);
-            ans.push_back(s);
+            currPath += to_string(node->val);
+            ans.push_back(currPath);
             return;
         }
         
-        s += to_string(node->val) + "->";
-       if(node->left) binaryTreePaths_01(node->left,ans,s);
-       if(node->right) binaryTreePaths_01(node->right,ans,s);
+        //Updation of CurrPath
+        currPath += to_string(node->val) + "->";
         
+        //Left & Right Recursive Calls
+       if(node->left) binaryTreePaths_01(node->left,currPath,ans);
+       if(node->right) binaryTreePaths_01(node->right,currPath,ans);
     }
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> ans;
-        string s = "";
-        binaryTreePaths_01(root,ans,s);
+        binaryTreePaths_01(root,"",ans);
         return ans;
     }
 };
