4a8f23de6abf11a8b71ddaf5e7f384a708fd524f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-24 13:24:36 +0530

    Time: 1 ms (100.00%), Space: 43.1 MB (73.32%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -14,39 +14,35 @@
  * }
  */
 class Solution {
-    public TreeNode delNodes_01(TreeNode node,HashSet<Integer> map,ArrayList<TreeNode> ans) {
+    public TreeNode delNodes_01(TreeNode node,  HashSet<Integer> set,ArrayList<TreeNode> ans) {
         if(node == null) return null;
         
-        node.left = delNodes_01(node.left,map,ans);
-        node.right = delNodes_01(node.right,map,ans);
+        node.left = delNodes_01(node.left,set,ans);
+        node.right = delNodes_01(node.right,set,ans);
         
-        if(map.contains(node.val)) {
+        if(set.contains(node.val)) {
             if(node.left != null) {
-                ans.add(node.left); 
+                ans.add(node.left);
             }
             
             if(node.right != null) {
-                ans.add(node.right); 
+                ans.add(node.right);
             }
-            
             return null;
         }
-        
         return node;
     }
-    
+            
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        HashSet<Integer> map = new HashSet<>();
+        ArrayList<TreeNode> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int ele : to_delete) {
-            map.add(ele);
+            set.add(ele);
         }
-        ArrayList<TreeNode> ans = new ArrayList<>();  
-        if(!map.contains(root.val)) {
+        if(!set.contains(root.val)){
             ans.add(root);
         }
-        delNodes_01(root,map,ans);
+        delNodes_01(root,set,ans);
         return ans;
-        
-       
     }
 }
