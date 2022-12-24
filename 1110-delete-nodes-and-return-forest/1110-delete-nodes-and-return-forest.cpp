6c79b5ff8bbf1d9815537d84649e07289465a2a8
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-24 13:12:52 +0530

    Time: 16 ms (98.16%), Space: 25.2 MB (89.94%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-    TreeNode* delNodes_01(TreeNode* node,vector<TreeNode*>& ans, vector<int>& to_delete) {
-        if(node == nullptr) return nullptr;
+    TreeNode* delNodes_01(TreeNode* node, vector<int>& to_delete,vector<TreeNode*>& ans) {
+        if(!node) return nullptr;
         
-        node->left = delNodes_01(node->left,ans,to_delete);
-        node->right = delNodes_01(node->right,ans,to_delete);
+        node->left = delNodes_01(node->left,to_delete,ans);
+        node->right =  delNodes_01(node->right,to_delete,ans);
         
-        if(find(to_delete.begin(),to_delete.end(),node->val) != to_delete.end()){
-            if(node->left != nullptr){
+        if(find(to_delete.begin(),to_delete.end(),node->val) != to_delete.end()) {
+            if(node->left) {
                 ans.push_back(node->left);
-            } 
+            }
             
-            if(node->right != nullptr){
+            if(node->right) {
                 ans.push_back(node->right);
             }
             
@@ -33,13 +33,9 @@ public:
     }
     
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        
         vector<TreeNode*> ans;
-        //if we dont have root value to delete add this value to our ans;
-        // auto pos = mySet.find(root->val);
-        // if(pos == mySet.end()) ans.push_back(root); 
         
-        if(delNodes_01(root,ans,to_delete)) ans.push_back(root);
+        if(delNodes_01(root,to_delete,ans)) ans.push_back(root);
         return ans;
     }
 };
