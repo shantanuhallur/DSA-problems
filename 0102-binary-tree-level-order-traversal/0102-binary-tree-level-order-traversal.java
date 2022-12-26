7dbefca5745bffd25e9695efa681eddeb3f4dddf
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-26 18:44:36 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (72.85%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,124 +14,31 @@
  * }
  */
 class Solution {
-    
-    public void levelOrder_M1(TreeNode root , List<List<Integer>> ans) {
-        LinkedList<TreeNode> que = new LinkedList<>();
-        LinkedList<TreeNode> childQue = new LinkedList<>();
-        que.addLast(root);
-        int level = 0;
-        List<Integer> smallAns = new ArrayList<>();
-        //System.out.println("Level :" + level + " : ");
-        while (que.size() != 0) {
-            TreeNode rn = que.removeFirst(); // remove Node
-            smallAns.add(rn.val);
-            //System.out.print(rn.val + " ");
-
-            if (rn.left != null)
-                childQue.addLast(rn.left);
-            if (rn.right != null)
-                childQue.addLast(rn.right);
-
-            if(que.size() == 0 ) {
-                //System.out.println();
-                //System.out.print("Level :" + (++level) + " : ");
-                List<Integer> base = new ArrayList<>(smallAns);
-                ans.add(base);
-                smallAns.clear();
-                LinkedList<TreeNode> temp = new LinkedList<>();
-                temp = que;
-                que = childQue;
-                childQue = temp;
-            }
-        }
-    }
-    
-    public static void levelOrder_M2(TreeNode root, List<List<Integer>> ans) {
-        LinkedList<TreeNode> que = new LinkedList<>();
-        que.addLast(root);
-        que.addLast(null);
-
-        //int level = 0;
-        List<Integer> smallAns = new ArrayList<>();
-        //System.out.println("Level :" + level + " : ");
-        while (que.size() != 1) {
-            TreeNode rn = que.removeFirst(); // remove Node
-            //System.out.print(rn.val + " ");
-            smallAns.add(rn.val);
-            
-            if (rn.left != null)
-                que.addLast(rn.left);
-            if (rn.right != null)
-                que.addLast(rn.right);
-
-            if (que.getFirst() == null) {
-                //System.out.println();
-                //System.out.print("Level :" + (++level) + " : ");
-                List<Integer> base = new ArrayList<>(smallAns);
-                ans.add(base);
-                smallAns.clear();
-                
-                que.addLast(que.removeFirst());
-
-            }
-        }
-    }
-    
-    public static void levelOrder_M3(TreeNode root, List<List<Integer>> ans) {
-        LinkedList<TreeNode> que = new LinkedList<>();
-        que.addLast(root);
-
-        int level = 0;
-        while (que.size() != 0) {
-            //System.out.println("Level :" + level + " : ");
-            List<Integer> smallAns = new ArrayList<>();
-            int size = que.size();
-            while (size-- > 0) {
-                TreeNode rn = que.removeFirst(); // remove Node
-                //System.out.print(rn.val + " ");
-                smallAns.add(rn.val);
-
-                if (rn.left != null)
-                    que.addLast(rn.left);
-                if (rn.right != null)
-                    que.addLast(rn.right);
-
-            }
-            List<Integer> base = new ArrayList<>(smallAns);
-            ans.add(base);
-            smallAns.clear();
-        }
-    }
-    
-    public static void levelOrder_M4(TreeNode root, List<List<Integer>> ans) {
-        LinkedList<TreeNode> que = new LinkedList<>();
-        que.addLast(root);
-
-        int level = 0;
-        while (que.size() != 0) {
-            List<Integer> smallAns = new ArrayList<>();
-            int size = que.size();
-            while (size-- > 0) {
-                TreeNode rn = que.removeFirst(); // remove Node
-                if(level == ans.size()){
-                    ans.add(new ArrayList<>());
-                }
-                ans.get(level).add(rn.val);
-
-                if (rn.left != null)
-                    que.addLast(rn.left);
-                if (rn.right != null)
-                    que.addLast(rn.right);
-
-            }
-            level++;
-        }
-    }
-    
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root==null) return new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        levelOrder_M4(root,ans);
+        if (root==null) return new ArrayList<>();
+        List<List<Integer>> ans = new LinkedList<>();
+        
+        LinkedList<TreeNode> que = new LinkedList<>();
+        que.addLast(root);
+        while(que.size()!=0) {
+            int size = que.size();
+            List<Integer> levelAns = new LinkedList<>();
+            while(size-->0) {
+                TreeNode rn = que.removeFirst();
+                
+                if(rn.left != null) {
+                    que.addLast(rn.left);
+                }
+                
+                if(rn.right != null) {
+                    que.addLast(rn.right);
+                }
+                
+                levelAns.add(rn.val);
+            }
+            ans.add(levelAns);
+        }
+        
         return ans;
     }
 }
