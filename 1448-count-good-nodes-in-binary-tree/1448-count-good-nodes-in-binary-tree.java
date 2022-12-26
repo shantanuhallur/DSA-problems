45cd377d53a376754c9cf45e872faef2fc08f01c
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-26 17:02:34 +0530

    Time: 2 ms (100.00%), Space: 50.5 MB (86.68%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -15,21 +15,23 @@
  */
 class Solution {
     int totalGoodNodes = 0;
-    
-    public void goodNodes_01(TreeNode root,int maxVal) {
-        if(root==null) return;
-        
-        if(root.val >= maxVal) {
+    public void goodNodes_01(TreeNode node,int maxVal) {
+        //Base Case
+        if(node==null)return;
+        // Checking if current node is a good node or not
+        if(node.val >= maxVal) {
             totalGoodNodes++;
-            maxVal = root.val;
+            maxVal = node.val;
         }
         
-        goodNodes_01(root.left,maxVal);
-        goodNodes_01(root.right,maxVal);
-    }  
+        //Left and right recursive call for traversing the tree.
+        goodNodes_01(node.left,maxVal);
+        goodNodes_01(node.right,maxVal);
+        
+    }
     
     public int goodNodes(TreeNode root) {
-        goodNodes_01(root,-(int)1e8);
+        goodNodes_01(root,-(int)1e5);
         return totalGoodNodes;
     }
 }
