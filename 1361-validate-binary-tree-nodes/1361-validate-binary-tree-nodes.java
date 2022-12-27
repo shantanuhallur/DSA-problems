e5a75c3fea4186e7d8e040d1cac01cae992716a3
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-27 08:41:00 +0530

    Time: 7 ms (80.81%), Space: 43.4 MB (88.89%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
@@ -15,9 +15,11 @@ class Solution {
         for(int i=0;i<n;i++) {
             if(inDegree[i]==0) {
                 if(root==-1) root = i;
+                //MULTIPLE ROOTS FOUND SO NOT UNIQUE TREE
                 else return false;
             }
         }
+        //NO ROOT FOUND SO CAN BE A CYCLE OF 2 nodes
         if(root == -1) return false;
         //BFS
         HashSet<Integer> vis = new HashSet<>();
@@ -25,8 +27,12 @@ class Solution {
         que.addLast(root);
         while(que.size()!=0) {
             Integer rn = que.removeFirst();
+            //ALREADY VISITED NODE so CYCLE FOUND /2 PARENTS FOUND
+/*4
+[1,-1,3,-1]
+[2,3,-1,-1]
+*/
             if(!vis.add(rn)) return false;
-            
             if(leftChild[rn]!=-1) que.addLast(leftChild[rn]);
             
             if(rightChild[rn]!=-1) que.addLast(rightChild[rn]);
