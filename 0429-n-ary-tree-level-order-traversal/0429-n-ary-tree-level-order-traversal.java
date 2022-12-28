dca97e23caf4c8715c4c699c1b03fd28b750329c
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-28 14:03:23 +0530

    Time: 2 ms (95.05%), Space: 43.1 MB (90.85%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
@@ -19,20 +19,24 @@ class Node {
 
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
+        //Base Case
         if(root == null) return new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
+        
         LinkedList<Node> que = new LinkedList<>();
         que.addLast(root);
-        
+        //LEVEL ORDER TRAVERSAL
         while(que.size()!=0) {
+            //size & levelAns -> list 
             int size = que.size();
             List<Integer> levelAns = new ArrayList<>();
             while(size-->0) {
                 Node removeN = que.removeFirst();
-                for(Node child: removeN.children) {
+                
+                for(Node child : removeN.children) {
                     que.addLast(child);
                 }
-                levelAns.add(removeN.val);    
+                levelAns.add(removeN.val);
             }
             ans.add(levelAns);
         }
