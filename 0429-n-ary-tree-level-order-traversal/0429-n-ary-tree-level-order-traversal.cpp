d61808c61bf65a0a5f37e011beca2c9c2cb42648
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-28 14:07:03 +0530

    Time: 21 ms (93.45%), Space: 11.9 MB (34.48%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.cpp
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.cpp
@@ -21,19 +21,24 @@ public:
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
+        //Edge Case
         if(!root) return {};
+        
         vector<vector<int>> ans;
         queue<Node*> que;
         que.push(root);
-        
+        //level order traversal
         while(que.size()!=0) {
+            //size levelAns
             int size = que.size();
             vector<int> levelAns;
-            while(size-->0){
-                Node* removeN = que.front(); que.pop();    
-                for(Node* child: removeN->children) {
+            while(size-->0) {
+                Node* removeN = que.front(); que.pop();
+                //add all the children of this removeN
+                for(Node* child : removeN->children) {
                     que.push(child);
                 }
+                
                 levelAns.push_back(removeN->val);
             }
             ans.push_back(levelAns);
