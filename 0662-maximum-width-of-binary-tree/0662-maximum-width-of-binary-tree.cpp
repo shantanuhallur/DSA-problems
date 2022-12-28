9b466b83a4a1d12543e6b1e299e465baf1d19672
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-28 19:00:45 +0530

    Time: 3 ms (99.45%), Space: 17.4 MB (62.92%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -11,29 +11,26 @@
  */
 class Solution {
 public:
-    
     int widthOfBinaryTree(TreeNode* root) {
-        int width = 0;
         queue<pair<TreeNode*,int>> que;
         que.push({root,0});
-
+        int ans=0;
         while(que.size()!=0) {
+            int startId = 0;
+            int endId = 0;
             int size = que.size();
-            int start = 0;
-            int end = 0;
             for(int i=0;i<size;i++) {
                 pair removeP = que.front(); que.pop();
                 
-                if(removeP.first->left) que.push({removeP.first->left,(long long)removeP.second*2+1});
+                if(removeP.first->left) que.push({removeP.first->left,(long long)removeP.second*2 +1});
                 
-                if(removeP.first->right) que.push({removeP.first->right,(long long)removeP.second*2+2});
-                
-                //starting & ending 
-                if(i == 0) start = removeP.second;
-                if(i == size-1) end = removeP.second;
+                if(removeP.first->right) que.push({removeP.first->right,(long long)removeP.second*2 +2});
+                //start
+                if(i==0) startId = removeP.second;
+                if(i==size-1) endId = removeP.second;
             }
-            width = max(width,end-start+1);
+            ans = max(ans,endId-startId+1);
         }
-        return width;
+        return ans;
     }
 };
