1dd57410e5f12717c415b3cb0ba0680180039517
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-29 06:32:34 +0530

    Time: 2 ms (78.10%), Space: 44.6 MB (74.42%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumEvenGrandparent_01(TreeNode node) {
+        if(node == null) return 0;
+         
+        int leftSum = 0;
+        int rightSum = 0;
+        int mySum = 0;
+        
+        if(node.left != null) leftSum += sumEvenGrandparent_01(node.left);
+        
+        if(node.right != null) rightSum += sumEvenGrandparent_01(node.right);
+        
+        if(node.val%2 == 0) {
+            if(node.left!=null && node.left.left != null) mySum += node.left.left.val;
+        
+            if(node.left!=null && node.left.right != null) mySum += node.left.right.val;
+        
+            if(node.right!=null && node.right.left != null) mySum += node.right.left.val;
+        
+            if(node.right !=null && node.right.right != null) mySum += node.right.right.val;
+        }
+        
+        return leftSum+rightSum+mySum;
+    }
+    
+    public int sumEvenGrandparent(TreeNode root) {
+        return sumEvenGrandparent_01(root);
+    }
+}
