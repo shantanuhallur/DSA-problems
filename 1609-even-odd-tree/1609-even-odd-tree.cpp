14fdc479a765aab9616754f2c4f4126b5dd09554
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-29 15:27:27 +0530

    Time: 263 ms (99.67%), Space: 150.8 MB (93.27%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -15,26 +15,31 @@ public:
         queue<TreeNode*> que;
         que.push(root);
         int level = 0;
+        //BFS
         while(que.size()!=0) {
             int size = que.size();
-            int min = -1;
-            int max = 1e8;
+            int increasing = -1;
+            int decreasing = 1e8;
             while(size-->0) {
                 TreeNode* removeN = que.front(); que.pop();
-                //even index
+                
+                //IF EVEN LEVEL
                 if(level%2==0) {
-                    if(removeN->val % 2 == 0 || removeN->val <= min) return false;
-                    min = removeN->val;
+                    if(removeN->val%2==0 || increasing >= removeN->val) return false;
+                    increasing = removeN->val;
                 }
+                //ODD LEVEL
                 else{
-                    if(removeN->val % 2 != 0 || removeN->val >= max) return false;
-                    max = removeN->val;
+                    if(removeN->val %2==1 || decreasing <=removeN->val) return false;
+                    decreasing = removeN->val;
                 }
                 
+                //setup the next level in the tree
                 if(removeN->left) que.push(removeN->left);
                 
                 if(removeN->right) que.push(removeN->right);
             }
+            //increasing the level
             level++;
         }
         return true;
