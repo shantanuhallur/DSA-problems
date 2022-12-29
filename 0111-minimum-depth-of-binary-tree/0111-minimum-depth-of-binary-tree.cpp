ab0a2c96fd417edcae150bde2c8689d5139bbd88
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-29 20:04:44 +0530

    Time: 302 ms (73.09%), Space: 146.8 MB (6.60%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,21 +12,23 @@
 class Solution {
 public:
     int minDepth_01(TreeNode* node) {
+        //Leafs min height Base Case 
         if(!node->left && !node->right) return 1;
         
         int leftMinH = 1e6;
         int rightMinH = 1e6;
-        
+        //Left Recursive Call of FAITH
         if(node->left) leftMinH = minDepth_01(node->left);
         
+        //right Recursive Call of FAITH
         if(node->right) rightMinH = minDepth_01(node->right);
-        
+        //return this current node's Min val
         return min(leftMinH,rightMinH) + 1;
+        
     }
     
     int minDepth(TreeNode* root) {
         if(!root) return 0;
-        int height = minDepth_01(root);
-        return height;
+        return minDepth_01(root);
     }
 };
