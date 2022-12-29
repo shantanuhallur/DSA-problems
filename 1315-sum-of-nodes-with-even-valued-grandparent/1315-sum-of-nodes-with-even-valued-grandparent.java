9bd3b7c2fa2e6d1d83a7c53a1fed373175d37340
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-29 18:51:49 +0530

    Time: 2 ms (78.10%), Space: 44.6 MB (78.10%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -15,25 +15,28 @@
  */
 class Solution {
     public int sumEvenGrandparent_01(TreeNode node) {
-        if(node == null) return 0;
+        //Base Case
+        if(node==null) return 0;
+        
         int mySum = 0;
-        int leftSum=0;
-        int rightSum=0;
+        int leftChildrenSum = 0;
+        int rightChildrenSum = 0;
+        //Recursive left and right calls asking sum of their grandchilren if exist
+        leftChildrenSum = sumEvenGrandparent_01(node.left);
+        rightChildrenSum = sumEvenGrandparent_01(node.right);
         
-        leftSum = sumEvenGrandparent_01(node.left);
-        rightSum = sumEvenGrandparent_01(node.right);
-        
-        if(node.val % 2 == 0) {
-            //leftGranchChilds
-            if(node.left!=null && node.left.left!=null) mySum += node.left.left.val;
-            if(node.left!=null && node.left.right!=null) mySum += node.left.right.val;
+        //if my node is even
+        if(node.val%2==0) {
+            //add LEFT grandchildren
+            if(node.left!=null && node.left.left !=null) mySum += node.left.left.val;
+            if(node.left!=null && node.left.right !=null) mySum += node.left.right.val;
             
-            //Right GrandChilds
+            //add RIGHT grandchildren
             if(node.right!=null && node.right.left!=null) mySum += node.right.left.val;
             if(node.right!=null && node.right.right!=null) mySum += node.right.right.val;
         }
         
-        return leftSum+rightSum+mySum;
+        return leftChildrenSum+rightChildrenSum+mySum;
     }
     
     public int sumEvenGrandparent(TreeNode root) {
