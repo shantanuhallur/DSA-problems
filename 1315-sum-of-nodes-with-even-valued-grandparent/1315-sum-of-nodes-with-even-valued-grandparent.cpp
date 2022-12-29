282e1b73eeb3ba8e9670584971c0e7e098418893
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-29 18:55:49 +0530

    Time: 59 ms (71.03%), Space: 41.4 MB (43.63%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -12,25 +12,30 @@
 class Solution {
 public:
     int sumEvenGrandparent_01(TreeNode* node) {
-        int leftSum = 0;
-        int rightSum = 0;
+        //Base Case
+        if(!node) return 0;
+        //variables
         int mySum = 0;
+        int leftChildrenSum = 0;
+        int rightChildrenSum = 0;
         
-        if(node->left) leftSum +=  sumEvenGrandparent_01(node->left);
+        //recursive left and right calls
+        leftChildrenSum = sumEvenGrandparent_01(node->left);
+        rightChildrenSum = sumEvenGrandparent_01(node->right);
         
-        if(node->right) rightSum +=  sumEvenGrandparent_01(node->right);
-        
-        if(node->val % 2 == 0) {
+        //IF MY NODE iS EVEN
+        if(node->val%2==0) {
+            //add left children
             if(node->left && node->left->left) mySum += node->left->left->val;
-            
             if(node->left && node->left->right) mySum += node->left->right->val;
             
+            //add right children
             if(node->right && node->right->left) mySum += node->right->left->val;
-            
             if(node->right && node->right->right) mySum += node->right->right->val;
+            
         }
-        
-        return leftSum+rightSum+mySum;
+        //return total sumation of all the nodes grandchildren.
+        return mySum + leftChildrenSum + rightChildrenSum;
     }
     
     int sumEvenGrandparent(TreeNode* root) {
