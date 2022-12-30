e91bcd91c21d8b3815a97b06899b0c5c533caeb7
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-30 16:27:10 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (91.93%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,21 +14,21 @@
  * }
  */
 class Solution {
-    // int idx = 0;
-    public TreeNode bstFromPreorder_(int[] preorder,int lrange,int rrange,int[] arr) {
-        if(arr[0] >= preorder.length || preorder[arr[0]] > rrange || preorder[arr[0]] < lrange) 
+     int idx = 0;
+    public TreeNode bstFromPreorder_(int[] preorder,int lrange,int rrange) {
+        if(idx >= preorder.length || preorder[idx] > rrange || preorder[idx] < lrange) 
             return null;
         
-        TreeNode root = new TreeNode(preorder[arr[0]++]);
+        TreeNode root = new TreeNode(preorder[idx++]);
         
-        root.left = bstFromPreorder_(preorder,lrange,root.val,arr);
-        root.right = bstFromPreorder_(preorder,root.val,rrange,arr);
+        root.left = bstFromPreorder_(preorder,lrange,root.val);
+        root.right = bstFromPreorder_(preorder,root.val,rrange);
         
         return root;
     }
     
     public TreeNode bstFromPreorder(int[] preorder) {
-        int[] arr = {0};
-        return bstFromPreorder_(preorder,-(int)1e8,(int)1e8,arr);
+        //int[] arr = {0};
+        return bstFromPreorder_(preorder,-(int)1e8,(int)1e8);
     }
 }
