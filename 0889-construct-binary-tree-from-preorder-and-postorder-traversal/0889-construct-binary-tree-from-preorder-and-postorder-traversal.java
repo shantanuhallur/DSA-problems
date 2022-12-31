03de285d5a450aad6c28c4d6c55b167ef8641108
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-31 15:57:33 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (97.76%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -14,28 +14,22 @@
  * }
  */
 class Solution {
-    
-    public TreeNode construct(int[] post,int ppsi,int ppei,int[] pre,int psi,int pei) 
-    {
-        if(psi>pei) return null;
-        
-        TreeNode node = new TreeNode(pre[psi]);
-        
-        if(psi==pei) return node; //saves from overflow for the while loop below pre[psi + 1]
-        
-        int idx = ppsi;
-        
-        while(post[idx] != pre[psi + 1])idx++;
-        
-        int tel = idx - ppsi + 1;
-        
-        node.left = construct(post,ppsi,idx,pre,psi+1,psi+tel);
-        node.right = construct(post,idx+1,ppei-1,pre,psi+tel+1,pei);
-        
-        return node;
-    }
-    
+    int preIdx = 0;
+    int posIdx = 0;
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
-        return construct(postorder,0,postorder.length-1,preorder,0,preorder.length-1);
+        
+        TreeNode root = new TreeNode(preorder[preIdx++]);
+        
+        if(root.val != postorder[posIdx]) {
+            root.left = constructFromPrePost(preorder,postorder);
+        }
+        
+        if(root.val != postorder[posIdx]) {
+            root.right = constructFromPrePost(preorder,postorder);
+        }
+        
+        posIdx ++;
+        
+        return root;
     }
 }
