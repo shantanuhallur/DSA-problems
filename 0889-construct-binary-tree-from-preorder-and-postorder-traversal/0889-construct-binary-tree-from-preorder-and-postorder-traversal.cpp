630d732d3cefdd5312b235a6ebecc19854357ee4
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-31 17:09:14 +0530

    Time: 14 ms (82.94%), Space: 25.5 MB (52.90%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -11,23 +11,22 @@
  */
 class Solution {
 public:
-    int preIdx=0;
-    int postIdx =0;
-    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        
-        TreeNode* root = new TreeNode(preorder[preIdx++]);
-        
-        if(root->val != postorder[postIdx]) {
-            root->left = constructFromPrePost(preorder,postorder);
+    int preIdx = 0;
+    int postIdx = 0;
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        //Root Creation
+        TreeNode* root = new TreeNode(pre[preIdx++]);
+        //check if tree is completed if not call recursively R & L
+        if(root->val != post[postIdx]) {
+            root->left = constructFromPrePost(pre,post);
         }
         
-        if(root->val != postorder[postIdx]) {
-            root->right = constructFromPrePost(preorder,postorder);
+        if(root->val != post[postIdx]) {
+            root->right = constructFromPrePost(pre,post);
         }
         
+        //if both ifs are false then tree is already constructed return root.
         postIdx++;
-    
         return root;
     }
-    
 };
