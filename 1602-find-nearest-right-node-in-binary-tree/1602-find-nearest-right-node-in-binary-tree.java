f603f626fe43a014d55f492e8a8b710b0da2f355
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2022-12-31 18:05:35 +0530

    Time: 11 ms (65.87%), Space: 55 MB (76.98%) - LeetHub

--- 1602-find-nearest-right-node-in-binary-tree/1602-find-nearest-right-node-in-binary-tree.java
+++ 1602-find-nearest-right-node-in-binary-tree/1602-find-nearest-right-node-in-binary-tree.java
@@ -18,17 +18,27 @@ class Solution {
         LinkedList<TreeNode> que = new LinkedList<>();
         que.addLast(root);
         TreeNode ans = null;
+        //Level Order Traversal
         while(que.size()!=0) {
             int size = que.size();
+            //Set up the next level in que
             for(int i=0;i<size;i++) {
                 TreeNode removeN = que.removeFirst();
                 
-                if(removeN.left!=null) que.addLast(removeN.left);
-                if(removeN.right!=null) que.addLast(removeN.right);
+                //add left and right nodes if exist
+                if(removeN.left != null) {
+                    que.addLast(removeN.left);
+                }
                 
-                if((removeN.val == u.val) && i != size-1) ans = que.peekFirst();
+                if(removeN.right !=null) {
+                    que.addLast(removeN.right);
+                }
+                //Check if current removeN is our U node
+                if(removeN.val == u.val && i!=size-1) {
+                    ans = que.peekFirst();
+                }
             }
-        }
+        } 
         return ans;
     }
 }
