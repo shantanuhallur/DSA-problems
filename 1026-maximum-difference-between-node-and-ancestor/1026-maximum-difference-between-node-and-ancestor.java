0bb15159a7d653c029f3234691f8edc3cd5ba43c
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-01 09:08:11 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (91.65%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -15,54 +15,19 @@
  */
 class Solution {
     int ancestor = -1;
-    public class pair{
-        int min;
-        int max;
-        pair(int min,int max){
-            this.min = min;
-            this.max = max;
-        }
-    }
-    int getMin(int ...arr) {
-        int min = arr[0];
-        for(int ele : arr) {
-            if(min>ele) {
-                min = ele;
-            }
-        }
-        return min;
-    }
-    
-    int getMax(int ...arr) {
-        int min = arr[0];
-        for(int ele : arr) {
-            if(min<ele) {
-                min = ele;
-            }
-        }
-        return min;
-    }
-    public pair maxi(TreeNode root) {
-        if(root.left==null && root.right== null) return new pair(root.val,root.val);
+    public void diff(TreeNode node,int max, int min) {
+        if(node == null) return;
         
-        pair leftP = new pair(root.val,root.val);
-        pair rightP = new pair(root.val,root.val);
-        if(root.left != null) leftP = maxi(root.left);
-        if(root.right != null) rightP = maxi(root.right);
+        max = Math.max(max, node.val);
+        min = Math.min(min,node.val);
         
-        if(ancestor< Math.abs(leftP.min-root.val)) ancestor = Math.abs(leftP.min-root.val);
-        if(ancestor< Math.abs(leftP.max-root.val)) ancestor = Math.abs(leftP.max-root.val);
+        ancestor = Math.max(ancestor,Math.abs(max-min));
+        diff(node.left,max,min);
+        diff(node.right,max,min);
         
-        
-        if(ancestor< Math.abs(rightP.min-root.val)) ancestor = Math.abs(rightP.min-root.val);
-        if(ancestor< Math.abs(rightP.max-root.val)) ancestor = Math.abs(rightP.max-root.val);
-        
-        pair myP = new pair(getMin(leftP.min,rightP.min,root.val),getMax(leftP.max,rightP.max,root.val));
-        
-        return myP;
     }
     public int maxAncestorDiff(TreeNode root) {
-        maxi(root);
+        diff(root,root.val,root.val);
         return ancestor;
     }
 }
