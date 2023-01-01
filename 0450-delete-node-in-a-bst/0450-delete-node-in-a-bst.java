f0fe5ca63e8f083364208a46029a4f9be170f407
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-01 20:14:14 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (80.77%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,27 +14,35 @@
  * }
  */
 class Solution {
-    public int findMin(TreeNode node) {
-        
-        while(node.left != null) node = node.left;
+    public int findMinEleInRightSubtree(TreeNode node) {
+        while(node.left!=null) node = node.left;
         return node.val;
     }
     public TreeNode deleteNode(TreeNode root, int key) {
+        //Base Case
         if(root == null) return null;
         
+        //binary search
         if(root.val < key) {
+            //recurse to right side
             root.right = deleteNode(root.right,key);
         }
         else if(root.val > key) {
             root.left = deleteNode(root.left,key);
         }
-        else {
+        else{
+            // Condition to handle both child null , right child null & left child null
             if(root.left == null || root.right == null) {
                 return root.left == null ? root.right : root.left;
             }
-            //if both children nodes exist
-            int minEle = findMin(root.right);
+            
+            //if both children are present of node to be deleted
+            //get min element in right subtree
+            int minEle = findMinEleInRightSubtree(root.right);
+            //as this element is min in our right subtree there is no chance that it will have 2 children or even 1 left child
+            //substitute the val of min element in our current root.
             root.val = minEle;
+            //delete the min element
             root.right = deleteNode(root.right,minEle);
         }
         return root;
