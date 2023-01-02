35b24e60a461e6d5ca0815ef524add43d732bf66
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-02 07:10:33 +0530

    Time: 138 ms (93.48%), Space: 68.3 MB (66.30%) - LeetHub

---
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* correctBinaryTree(TreeNode* root) {
+        unordered_set<int> set;
+        queue<TreeNode*> que;
+        que.push(root);
+        
+        while(que.size()!=0) {
+            int size = que.size();
+            while(size-->0) {
+                TreeNode* removeN = que.front(); que.pop();
+                
+                if(removeN->right) {
+                    if(removeN->right->right && set.find(removeN->right->right->val)!=set.end()){
+                        removeN->right = nullptr;
+                    }
+                    else {
+                        set.insert(removeN->right->val);
+                        que.push(removeN->right);
+                    }
+                }
+                
+                if(removeN->left) {
+                    if(removeN->left->right && set.find(removeN->left->right->val)!=set.end()) {
+                        removeN->left = nullptr;
+                    }
+                    else {
+                        set.insert(removeN->left->val);
+                        que.push(removeN->left);
+                    }
+                }
+            }
+        }
+        return root;
+    }
+};
