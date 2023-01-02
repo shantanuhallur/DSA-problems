99c457e593aa169839e358f9416fcb5ce1ae9bd7
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-02 09:51:30 +0530

    Time: 177 ms (87.50%), Space: 92 MB (45.83%) - LeetHub

--- 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.cpp
+++ 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.cpp
@@ -9,7 +9,7 @@ public:
 
         for (int i = 2; i <= n; i++) {
             nodes[i] ^= nodes[i / 2];
-            result += nodes[i];
+            result += nodes[i]; // CHECK XOR TABLE
         }
 
         return result;
