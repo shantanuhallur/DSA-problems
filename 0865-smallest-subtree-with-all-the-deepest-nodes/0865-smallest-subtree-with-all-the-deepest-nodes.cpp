2c09ba7f8587f211b2a5906015f44d5b40952dcc
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-02 20:25:08 +0530

    Time: 6 ms (71.94%), Space: 14.4 MB (78.51%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,25 +11,26 @@
  */
 class Solution {
 public:
-        int height(TreeNode* node) {
+    int height(TreeNode* node) {
+        //Base Case
         if(!node) return 0;
         
-        int leftHeight = height(node->left);
-        int rightHeight = height(node->right);
-        
-        return max(leftHeight,rightHeight) + 1;
+        //recursive faith of call of left and right height
+        int leftH = height(node->left);
+        int rightH = height(node->right);
+        //Calculate meri height
+        return max(leftH,rightH) +1;
     }
-    
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-    if(!root) return nullptr;
+        //Base case
+        if(!root) return nullptr;
+        //get max depth of left and right subtrees
+        int leftSubTreeH = height(root->left);
+        int rightSubTreeH = height(root->right);
+        //check our cases conditions to conclude where we want to search for our subtree
+        if(leftSubTreeH==rightSubTreeH) return root;
+        else if(leftSubTreeH<rightSubTreeH) return subtreeWithAllDeepest(root->right);
+        else return subtreeWithAllDeepest(root->left);
         
-        int leftSubtreeHeight = height(root->left);
-        int rightSubtreeHeight = height(root->right);
-        
-        if(leftSubtreeHeight == rightSubtreeHeight) return root;
-        
-        else if(leftSubtreeHeight > rightSubtreeHeight)
-            return subtreeWithAllDeepest (root->left);
-        else return subtreeWithAllDeepest(root->right);
     }
 };
