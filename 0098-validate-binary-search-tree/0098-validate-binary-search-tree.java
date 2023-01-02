fc51e140f7ac69dd1fafb5793101c892ccb858e5
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-02 14:08:21 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (88.86%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,40 +14,18 @@
  * }
  */
 class Solution {
-    public class BSTpair {
-        boolean isBST = true;
-        long min = (long)1e13;
-        long max = -(long)1e13;
+    boolean isValidTree = true;
+    TreeNode prev = null;
+    public void isValidBST_01(TreeNode root) {
+        if(root == null) return;
         
-        BSTpair(long max,long min, boolean isBST) {
-            this.max = max;
-            this.min = min;
-            this.isBST = isBST;
-        }
-        
-        BSTpair() {
-            
-        }
+        isValidBST(root.left);
+        if(prev!=null && prev.val >= root.val)isValidTree = false;
+        prev = root;
+        isValidBST(root.right);
     }
-    
-    public BSTpair isValidBST_01(TreeNode root) {
-        if(root == null) return new BSTpair();
-        
-        BSTpair lans = isValidBST_01(root.left);
-        BSTpair rans = isValidBST_01(root.right);
-        
-        BSTpair myPair = new BSTpair();
-        myPair.isBST = lans.isBST && rans.isBST && lans.max < root.val && root.val < rans.min;
-        if(!myPair.isBST) return myPair;
-        
-        myPair.min = Math.min(lans.min,root.val);
-        myPair.max = Math.max(rans.max,root.val);
-        
-        return myPair;
-    }
-    
     public boolean isValidBST(TreeNode root) {
-        BSTpair ans = isValidBST_01(root);
-        return ans.isBST;
+        isValidBST_01(root);
+        return isValidTree;
     }
 }
