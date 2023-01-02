2f8f1b043f1626a9b0597e563984122f6e0905c2
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-02 07:04:25 +0530

    Time: 9 ms (59.60%), Space: 45.3 MB (71.52%) - LeetHub

---
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode correctBinaryTree(TreeNode root) {
+        HashSet<Integer> set = new HashSet<>();
+        LinkedList<TreeNode> que = new LinkedList<>();
+        que.addLast(root);
+        set.add(root.val);
+        
+        while(que.size()!=0) {
+            int size = que.size();
+            while(size-->0) {
+                TreeNode removeN = que.removeFirst();
+                //rightNode
+                if(removeN.right!=null) {
+                    if(removeN.right.right!=null && set.contains(removeN.right.right.val)){
+                        removeN.right=null;
+                    }
+                    else{
+                        set.add(removeN.right.val);
+                        que.addLast(removeN.right);
+                    }
+                }
+                //leftNode
+                if(removeN.left!=null) {
+                    if(removeN.left.right!=null && set.contains(removeN.left.right.val)) {
+                        removeN.left = null;
+                    }
+                    else{
+                        set.add(removeN.left.val);
+                        que.addLast(removeN.left);
+                    }
+                }
+            }
+        }
+        return root;
+    }
+}
