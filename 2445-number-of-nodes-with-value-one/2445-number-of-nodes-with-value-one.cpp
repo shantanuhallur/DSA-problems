651166c79602bc2185e20b537045d7a9bbbdc6e7
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-03 15:49:35 +0530

    Time: 193 ms (83.10%), Space: 92 MB (61.97%) - LeetHub

--- 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.cpp
+++ 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int numberOfNodes(int n, vector<int>& queries) {
-        vector<int> nodes(n+1,0);
+        vector<int> dp(n+1,0);
+        int on =0;
+        for(int query : queries) dp[query] ^=1;
         
-        for(int &query : queries) nodes[query] ^=1;
-        
-        int result = nodes[1];
-
-        for (int i = 2; i <= n; i++) {
-            nodes[i] ^= nodes[i / 2];
-            result += nodes[i]; // XOR USED TO TOGGLE 0 to 1 and 1 to 0;
+        for(int i=1;i<n+1;i++) {
+            if(i!=1) {
+                dp[i] ^= dp[i/2];
+            }
+            if(dp[i]%2==1) on++;
         }
-
-        return result;
+        return on;
     }
 };
