ff67809a981433ac6a12a60bb1e77e683b8ee396
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-03 15:40:30 +0530

    Time: 26 ms (43.62%), Space: 97 MB (29.79%) - LeetHub

--- 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.java
+++ 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.java
@@ -1,16 +1,19 @@
 class Solution {
     public int numberOfNodes(int n, int[] queries) {
+        int result=0;
         int[] dp = new int[n+1];
-        
-        for(int query : queries) dp[query] ^=1;
-        
-        int result = dp[1];
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] ^= dp[i / 2];
-            result += dp[i]; // XOR USED TO TOGGLE 0 to 1 and 1 to 0;
+        //on and off the bulb these many times.
+        for(int query : queries) {
+            dp[query]++;
         }
-
+        
+        for(int i=1;i<n+1;i++) {
+            if(i!=1) { //as 1 doesnt have any parents
+                dp[i] += dp[i/2];
+            }
+            //check if bulb is on or not after all the queries done.   
+            if(dp[i]%2 == 1) result++;
+        }
         return result;
     }
 }
