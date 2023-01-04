abe3c6534d822831188c3fa42a27d540f4e67c67
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-04 15:43:07 +0530

    Time: 1 ms (9.64%), Space: 42 MB (31.27%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -15,16 +15,19 @@
  */
 class Solution {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if(root1==null && root2==null) return true;
-        if(root1==null || root2==null) return false;
+        if(root1==null && root2==null)return true; //both roots are null so tree is equal
+        if(root1 == null || root2==null)return false; //only one of the trwo nodes is null return false
         if(root1.val != root2.val) return false;
         
-        if(flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right)){
+        //check if the tree is equal normally
+        if(flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right)) {
             return true;
         }
-        else if(flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left)){
+        //check if the tree is flip equal
+        else if(flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left)) {
             return true;
         }
+        
         return false;
     }
 }
