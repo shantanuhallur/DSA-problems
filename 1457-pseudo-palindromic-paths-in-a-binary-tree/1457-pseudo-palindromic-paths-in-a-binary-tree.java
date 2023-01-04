df6331337ec5cd76280e281682c2f3b4484f6cf0
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-04 17:40:27 +0530

    Time: 22 ms (63.20%), Space: 68.8 MB (75.09%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -14,24 +14,25 @@
  * }
  */
 class Solution {
-    int paths=0;
-    public void dfs(TreeNode node, int[] arr) {
+    int paths = 0;
+    public void dfs(TreeNode node,int[] arr) {
+        //LEAF
         if(node.left == null && node.right == null) {
             arr[node.val]++;
-            int odd=0;
+            int oddFreqCount=0;
+            //check if potential palindrome
             for(int i=1;i<=9;i++) {
-                if(arr[i]%2==1) odd++;
+                if(arr[i]%2 == 1) oddFreqCount++;
             }
-            if(odd<=1) paths++;
+            //check if the path is pseudo palindromic
+            if(oddFreqCount <=1) paths++;
             arr[node.val]--;
-            return;
         }
-        
         arr[node.val]++;
-        if(node.left!=null) dfs(node.left,arr);
-        if(node.right!=null)dfs(node.right,arr);
+        //recurse left and right
+        if(node.left !=null) dfs(node.left,arr);
+        if(node.right !=null) dfs(node.right,arr);
         arr[node.val]--;
-        
     }
     public int pseudoPalindromicPaths (TreeNode root) {
         int[] arr = new int[10];
