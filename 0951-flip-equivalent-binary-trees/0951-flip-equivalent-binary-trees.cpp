41fd7cc8299e7f6b2d3274bc20ecb9e39fe2b9c7
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-04 15:47:01 +0530

    Time: 5 ms (56.34%), Space: 12.1 MB (97.18%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -12,17 +12,17 @@
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if(!root1 && !root2) return true;
-        if(!root1 || !root2) return false;
-        if(root1->val != root2->val) return false;
-    
+        if(!root1 && !root2) return true; //both roots are null return true
+        if(!root1 || !root2) return false; //only one of them is null return false
+        if(root1->val != root2->val) return false; //if values are not equal return false;
+        //check normal trees left and right child nodes recursively
         if(flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right)) {
             return true;
         }
+        //check if flipped trees are euqal
         else if(flipEquiv(root1->left,root2->right) && flipEquiv(root1->right,root2->left)) {
             return true;
         }
-        
-        return false;
+        return false;   
     }
 };
