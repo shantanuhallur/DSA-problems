3387961e9a005c45661284d81be06a6ab15ec78a
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-05 14:51:52 +0530

    Time: 7 ms (72.44%), Space: 13.8 MB (83.69%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -13,14 +13,16 @@ class Solution {
 public:
     int moves = 0;
     int distribute(TreeNode* node) {
+        //Base Case if node is null we dont have any excessive or deficit coins
         if(!node) return 0;
         
+        //get left & right node/subtree excessive deficit coins by recursive faith of call
         int leftExDef = distribute(node->left);
         int rightExDef = distribute(node->right);
-        
+        //count our total min moves
         moves += abs(leftExDef) + abs(rightExDef);
-    
-        return leftExDef + rightExDef + node->val -1;
+        //return our net ex def coins to parent
+        return leftExDef + rightExDef + node->val - 1;
     }
     int distributeCoins(TreeNode* root) {
         distribute(root);
