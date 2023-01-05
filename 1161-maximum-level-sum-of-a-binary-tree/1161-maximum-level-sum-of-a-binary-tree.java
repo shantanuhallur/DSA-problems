07171c955984b3c3de6be36f4eaeb4a086b48e44
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-05 16:12:18 +0530

    Time: 24 ms (33.76%), Space: 65.9 MB (55.75%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -15,27 +15,32 @@
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        LinkedList<TreeNode> que = new LinkedList<>();
-        que.addLast(root);
-        int maxSumLevel = 1;
+        int AnsLevel = 1;
         int maxSum = -(int)1e6;
         int level = 1;
+        LinkedList<TreeNode> que = new LinkedList<>();
+        que.addLast(root);
+        //Level order traversal.
         while(que.size()!=0) {
-            int sum = 0;
             int size = que.size();
+            int levelSum=0;
             while(size-->0) {
+                //remove node
                 TreeNode removeN = que.removeFirst();
-                
-                sum += removeN.val;
-                if(removeN.left!=null) que.addLast(removeN.left);
-                if(removeN.right!=null) que.addLast(removeN.right);
+                //add each nodes value of this level in level sum
+                levelSum += removeN.val;
+                //set up next level of the que
+                if(removeN.left !=null)que.addLast(removeN.left);
+                if(removeN.right !=null)que.addLast(removeN.right);
             }
-            if(sum > maxSum) {
-                maxSum = sum;
-                maxSumLevel = level;
+            //updation of maxlevel if required
+            if(maxSum<levelSum) {
+                maxSum = levelSum;
+                AnsLevel = level;
             }
+            //go to next level
             level++;
         }
-        return maxSumLevel;
+        return AnsLevel;
     }
 }
