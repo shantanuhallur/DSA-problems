45ec92022e4f159d058f6274f99076f9b7194556
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-06 08:51:34 +0530

    Time: 28 ms (47.68%), Space: 57.7 MB (22.94%) - LeetHub

--- 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.java
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.java
@@ -23,18 +23,17 @@ class Node {
 
 class Solution {
     public Node findRoot(List<Node> tree) {
-        HashMap<Integer,Integer> map = new HashMap();
+        HashSet<Node> set = new HashSet<>();
         
-        for(Node node:tree) {
-            for(Node child:node.children) {
-                map.put(child.val,map.getOrDefault(child.val,0)+1);
+        for(Node node :  tree) {
+            for(Node child : node.children) {
+                set.add(child);
             }
         }
         Node root = null;
         for(Node node:tree) {
-            if(!map.containsKey(node.val)) root = node;
+            if(!set.contains(node)) root=node;
         }
-        
         return root;
     }
 }
