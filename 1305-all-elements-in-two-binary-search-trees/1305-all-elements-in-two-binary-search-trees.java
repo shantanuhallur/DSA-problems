ea448d7f75c1732bad42ac40bdee52f4149b22f9
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-06 17:33:29 +0530

    Time: 52 ms (30.60%), Space: 68.8 MB (48.01%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,26 +14,38 @@
  * }
  */
 class Solution {
-    public void inOrder(TreeNode node,List<Integer> list) {
-        if(node == null) return;
-    
-        inOrder(node.left,list);
+    public void inorder(TreeNode node,List<Integer> list) {
+        if(node==null) return;
+        
+        //left and right recursive calls
+        inorder(node.left,list);
+        //INORDER AREA ---->>>
         list.add(node.val);
-        inOrder(node.right,list);
+        //INORDER AREA ---->>>
+        inorder(node.right,list);
     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
-        inOrder(root1,list1);
-        inOrder(root2,list2);
+        System.out.print(list1 + " " + list2);
+        inorder(root1,list1);
+        inorder(root2,list2);
+        
+        //merge these 2 sorted lists
         List<Integer> ans = new ArrayList<>();
-        int i=0,j=0;
+        int i=0; int j=0;
         while(i<list1.size() && j<list2.size()) {
-            if(list1.get(i)<list2.get(j)) ans.add(list1.get(i++));
-            else ans.add(list2.get(j++));
+            if(list1.get(i) < list2.get(j)) {
+                ans.add(list1.get(i++));
+            }
+            else{ //element in lest 2 is smaller
+                ans.add(list2.get(j++));
+            }
         }
-        while(i<list1.size())ans.add(list1.get(i++));
-        while(j<list2.size())ans.add(list2.get(j++));
+        
+        while(i<list1.size()) ans.add(list1.get(i++));
+        while(j<list2.size()) ans.add(list2.get(j++));
+        //return our ans arraylist which is now merged.
         return ans;
     }
 }
