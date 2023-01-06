e68fed0a8b008f34d1a2ec970eb565c3fc9bf0a7
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-06 07:09:51 +0530

    Time: 7 ms (35.27%), Space: 62.7 MB (13.42%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -14,31 +14,27 @@
  * }
  */
 class Solution {
-    List<Integer> sortedList = new ArrayList<>();
-    public void inorder(TreeNode node) {
+    public void inorder(ArrayList<Integer> sortedList,TreeNode node) {
         if(node == null) return;
         
-        inorder(node.left);
+        inorder(sortedList,node.left);
         sortedList.add(node.val);
-        inorder(node.right);
-        
+        inorder(sortedList,node.right);
     }
-    
-    public TreeNode balance(int st,int end) {
+    public TreeNode balance(ArrayList<Integer> sortedList,int st,int end) {
         if(st>end) return null;
+        
         int mid = (st+end)/2;
         TreeNode root = new TreeNode(sortedList.get(mid));
-        
-        root.left = balance(st,mid-1);
-        root.right = balance(mid+1,end);
-        
+        root.left = balance(sortedList,st,mid-1);
+        root.right = balance(sortedList,mid+1,end);
         
         return root;
     }
     public TreeNode balanceBST(TreeNode root) {
-        if(root==null) return null;
-        inorder(root);
-        root = balance(0,sortedList.size()-1);
+        ArrayList<Integer> sortedList = new ArrayList<>();
+        inorder(sortedList,root);
+        root = balance(sortedList,0,sortedList.size()-1);
         return root;
     }
 }
