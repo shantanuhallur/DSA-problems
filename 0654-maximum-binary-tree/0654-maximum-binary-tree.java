79543a541ac02caef0987862afaad264348981f1
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-06 06:54:31 +0530

    Time: 3 ms (72.32%), Space: 54 MB (37.80%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -16,24 +16,22 @@
 class Solution {
     public TreeNode makeBST(int[] nums,int st,int end) {
         if(st>end) return null;
+        int maxNo = -1;
+        int midIdx = -1;
         
-        int midIdx=-1;
-        int max = -1; 
         for(int i=st;i<=end;i++) {
-            if(nums[i]>max) {
+            if(nums[i]>maxNo) {
+                maxNo = nums[i];
                 midIdx = i;
-                max = nums[i];
             }
         }
         
         TreeNode root = new TreeNode(nums[midIdx]);
-        
         root.left = makeBST(nums,st,midIdx-1);
         root.right = makeBST(nums,midIdx+1,end);
         
         return root;
     }
-    
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         return makeBST(nums,0,nums.length-1);
     }
