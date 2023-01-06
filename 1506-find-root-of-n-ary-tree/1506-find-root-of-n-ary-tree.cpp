208c1385537172a86b5fdf1a2e71e6764d8a5834
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-06 14:31:33 +0530

    Time: 120 ms (92.50%), Space: 255.1 MB (47.50%) - LeetHub

--- 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.cpp
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.cpp
@@ -21,16 +21,19 @@ public:
 class Solution {
 public:
     Node* findRoot(vector<Node*> tree) {
-        unordered_set<Node*> set;
+        unordered_set<int> childrenSet;
+        //loop over trees to add all the children of each node.
         for(auto* node : tree) {
-            for(auto* node : node->children) {
-                set.insert(node);
+            for(auto* child : node->children) {
+                childrenSet.insert(child->val);
             }
         }
+        //check which node in tree vector is not present in our childrenSet
         Node* root = nullptr;
-        for(auto* node:tree) {
-            if(set.find(node)==set.end()) root = node;
+        for(auto* node : tree) {
+            if(childrenSet.find(node->val) == childrenSet.end()) root = node;
         }
+        
         return root;
     }
 };
