ea7a6394d9819dd34b8a8748f8f2b2c1bf5a924a
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-06 09:44:28 +0530

    Time: 8 ms (64.29%), Space: 45.5 MB (70.13%) - LeetHub

--- 1660-correct-a-binary-tree/1660-correct-a-binary-tree.java
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.java
@@ -24,28 +24,28 @@ class Solution {
             int size = que.size();
             while(size-->0) {
                 TreeNode removeN = que.removeFirst();
-                //rightNode
+                
                 if(removeN.right!=null) {
                     if(removeN.right.right!=null && set.contains(removeN.right.right.val)){
-                        removeN.right=null;
-                    }
-                    else{
-                        set.add(removeN.right.val);
-                        que.addLast(removeN.right);
-                    }
+                            removeN.right = null;
+                        }
+                        else{
+                            set.add(removeN.right.val);
+                            que.addLast(removeN.right);
+                        }
                 }
-                //leftNode
+                    
                 if(removeN.left!=null) {
-                    if(removeN.left.right!=null && set.contains(removeN.left.right.val)) {
-                        removeN.left = null;
-                    }
-                    else{
-                        set.add(removeN.left.val);
-                        que.addLast(removeN.left);
+                        if(removeN.left.right!=null && set.contains(removeN.left.right.val)) {
+                                removeN.left = null;
+                            }
+                            else{
+                                set.add(removeN.left.val);
+                                que.addLast(removeN.left); 
+                            }
+                        }
                     }
                 }
-            }
-        }
         return root;
     }
 }
