bac6268590228539e459f3adfa4e1a56a4f5e3ad
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-08 10:33:28 +0530

    Time: 5 ms (82.80%), Space: 42.3 MB (97.13%) - LeetHub

---
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int getfreq(TreeNode node,HashMap<Integer,Integer> map) {
+        if(node == null) return 0;
+        
+        int leftSum = getfreq(node.left,map);
+        int rightSum = getfreq(node.right,map);
+        
+        int totalSum = leftSum+rightSum+node.val;
+        map.put(totalSum,map.getOrDefault(totalSum,0)+1);
+        
+        return totalSum;
+    }
+    public int[] findFrequentTreeSum(TreeNode root) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        getfreq(root,map);
+        
+        int maxFreq = -1;
+        int count=0;
+        for(int value:map.values()) {
+            if(maxFreq == value) count++;
+            if(maxFreq < value) {
+                maxFreq = value;
+                count=1;
+            }
+        }
+        System.out.print(count);
+        int[] ans = new int[count];
+        int i=0;
+        for(int key:map.keySet()) {
+            if(map.get(key) == maxFreq) ans[i++] = key;
+        }
+        return ans;
+    }
+}
