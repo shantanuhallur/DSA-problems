3068c39c123356f0951e52772e51f356ffe5cfe4
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-10 20:10:02 +0530

    Time: 28 ms (46.76%), Space: 24 MB (45.84%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
@@ -16,8 +16,8 @@ public:
         if(root->val < low) return trimBST(root->right,low,high);
         if(root->val > high) return trimBST(root->left,low,high);
         
-        root->left = trimBST(root->left,low,high);
         root->right = trimBST(root->right,low,high);
+        root->left = trimBST(root->left,low,high);
         
         return root;
     }
