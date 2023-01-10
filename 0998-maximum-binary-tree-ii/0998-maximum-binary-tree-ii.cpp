418ff1a649ce9a372d234ea2b2fe51b737d2e1d6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-10 19:21:57 +0530

    Time: 0 ms (100.00%), Space: 13.4 MB (63.67%) - LeetHub

--- 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.cpp
+++ 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.cpp
@@ -12,12 +12,14 @@
 class Solution {
 public:
     TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        //Base Case
         if(!root) return new TreeNode(val);
         
-        if(root->val < val) return new TreeNode(val,root,nullptr);
-        
+        //if val is greater than the current root.val then it should be attached to right of this new root.
+        if(root->val<val) return new TreeNode(val,root,nullptr);
+        //recursive call to find the real position of val if the about if condition does not apply.
         root->right = insertIntoMaxTree(root->right,val);
-        
+        //just return our newly modified trees root.
         return root;
     }
 };
