b562d81178fbc915c49a66fa3ecd2c5894d08b0f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-10 07:13:47 +0530

    Time: 12 ms (19.92%), Space: 13.5 MB (63.67%) - LeetHub

--- 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.java
+++ 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.java
@@ -1,26 +1,23 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
-        if(root==null) return new TreeNode(val);
+public:
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        if(!root) return new TreeNode(val);
         
-        if(root.val<val) return new TreeNode(val,root,null);
+        if(root->val < val) return new TreeNode(val,root,nullptr);
         
-        root.right = insertIntoMaxTree(root.right,val);
+        root->right = insertIntoMaxTree(root->right,val);
         
         return root;
     }
-}
+};
