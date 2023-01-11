d2ef95af09200a21ceb0057b59d92d8bfc25eb1b
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-11 08:10:45 +0530

    Time: 2 ms (84.67%), Space: 9.8 MB (22.00%) - LeetHub

---
+++ 0156-binary-tree-upside-down/0156-binary-tree-upside-down.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if(!root || !root->left) return root;
+        //save new root reference to return as new root for that subtree
+        TreeNode* newRoot = upsideDownBinaryTree(root->left);
+        
+        root->left->left = root->right;
+        root->left->right = root;
+        root->left = nullptr;
+        root->right = nullptr;
+        
+        return newRoot;
+    }
+};
