aace2a7e7f80b9cd2e0252e152b77554ea428b2b
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-12 15:14:13 +0530

    Time: 123 ms (100.00%), Space: 61.7 MB (62.50%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -9,25 +9,33 @@
  */
 class Solution {
 public:
-    
-    int found=0;
-    TreeNode* LCA(TreeNode* node, int p, int q) {
-        if(node == NULL) return node;
-        
+    //to track if both p and q are found in tree
+    int totalFound = 0;
+    TreeNode* LCA(TreeNode* node,int p ,int q) {
+         //Base case
+        if(!node) return NULL;
+        //left and right recursive call of faith asking if p or q is present in subtree
         TreeNode* leftFound = LCA(node->left,p,q);
         TreeNode* rightFound = LCA(node->right,p,q);
-        
+        //if current node is equal to p or q
         if(node->val == p || node->val == q) {
-            found++;
+            totalFound++;
             return node;
         }
-        
-        if(leftFound && rightFound) return node;
-        
+        // if p q are found in left and right then i am the LCA ill return myself
+        if(leftFound != NULL && rightFound != NULL) {
+            return node;
+        }
+         //return statement covering all the 3 cases of leftF,!rightF | rightF,!leftF | !leftF,!rightF
         return leftFound != NULL ? leftFound : rightFound;
     }
+   
+    
+    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         TreeNode* ans = LCA(root,p->val,q->val);
-        return found == 2 ? ans : NULL;
+        //onlyy if both p and q are found in my overall tree then ill return lca or else the
+        //returned lca is a fake one.
+        return totalFound == 2 ? ans : NULL;
     }
 };
