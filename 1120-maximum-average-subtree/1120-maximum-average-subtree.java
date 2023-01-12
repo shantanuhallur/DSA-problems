9637debb8c3ac0db27b039d07418d0889acdddbe
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-12 18:49:30 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (100.00%) - LeetHub

--- 1120-maximum-average-subtree/1120-maximum-average-subtree.java
+++ 1120-maximum-average-subtree/1120-maximum-average-subtree.java
@@ -14,32 +14,36 @@
  * }
  */
 class Solution {
-    double maxAvg = 0;
-    public class pair{
+    public class pair {
         double sum;
         int totalNodes;
         
-        pair(double sum,int totalNodes) {
+        pair(double sum, int totalNodes) {
             this.sum = sum;
             this.totalNodes = totalNodes;
         }
     }
-    public pair getMax(TreeNode node) {
-        if(node==null) return new pair(0,0);
-        
-        pair leftAns = getMax(node.left);
-        pair rightAns = getMax(node.right);
-        
+    double maxAvg = 0;
+    public pair maxAvg(TreeNode node) { 
+        //base case
+        if(node == null) return new pair(0,0);
+        //left and right right recursive call
+        pair leftAns = maxAvg(node.left);
+        pair rightAns = maxAvg(node.right);
+        //postOrderArea ---->>
         pair myAns = new pair(0,0);
+        //total sum of my subtree
         myAns.sum = leftAns.sum + rightAns.sum + node.val;
+        //total nodes in my whole subtree
         myAns.totalNodes = leftAns.totalNodes + rightAns.totalNodes + 1;
+        //maximuze Avg
         if(myAns.sum/myAns.totalNodes > maxAvg) maxAvg = myAns.sum/myAns.totalNodes;
         
         return myAns;
-        
     }
+    
     public double maximumAverageSubtree(TreeNode root) {
-        getMax(root);
+        maxAvg(root);
         return maxAvg;
     }
 }
