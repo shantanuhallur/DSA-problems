87a2cebacd7329c0c1598d7be5834e28f4f49df5
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-12 17:20:09 +0530

    Time: 43 ms (80.73%), Space: 34.6 MB (32.11%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.cpp
+++ 1214-two-sum-bsts/1214-two-sum-bsts.cpp
@@ -11,28 +11,29 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* node,unordered_set<int>& set,int target)  {
+    void traverse(TreeNode* node,unordered_set<int>& set,int target) {
+        //Base Case
         if(!node) return;
-        
-        set.insert(target - node->val);
-        
+        // Fill the set
+        set.insert(target-node->val);
+        //left and right recursive call
         traverse(node->left,set,target);
         traverse(node->right,set,target);
+        
     }
-    
-    bool traverse1(TreeNode* node,unordered_set<int> & set) {
+    bool traverse1(TreeNode* node,unordered_set<int>& set) {
+        //Base Case
         if(!node) return false;
-        
+        //check if set contains Y -> (target-x)
         if(set.find(node->val) != set.end()) return true;
-        
+        //else run left and righ recursive calls
         bool leftFound = traverse1(node->left,set);
         bool rightFound = traverse1(node->right,set);
         
-        return leftFound||rightFound;
+        return leftFound || rightFound;
     }
     bool twoSumBSTs(TreeNode* root1, TreeNode* root2, int target) {
         unordered_set<int> set;
-        
         traverse(root1,set,target);
         return traverse1(root2,set);
     }
