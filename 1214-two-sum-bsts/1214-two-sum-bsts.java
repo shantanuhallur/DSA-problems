0327aa5292ec01c0e188fe4f07c3533c5f8c5bc2
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-12 17:14:26 +0530

    Time: 3 ms (87.82%), Space: 43 MB (64.97%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.java
+++ 1214-two-sum-bsts/1214-two-sum-bsts.java
@@ -15,22 +15,27 @@
  */
 class Solution {
     public void traverse(TreeNode node,int target,HashSet<Integer> set) {
+        //Base Case
         if(node == null) return;
-        
+        // Fill the set
         set.add(target - node.val);
+        //left and right recursive call
         traverse(node.left,target,set);
         traverse(node.right,target,set);
     }
     
     public boolean traverse1(TreeNode node,HashSet<Integer> set) {
-        if(node==null) return false;
+        //Base Case
+        if(node == null) return false;
+        //check if set contains Y -> (target-x)
+        if(set.contains(node.val))return true;
+        //else run left and righ recursive calls
+        boolean leftFound = traverse1(node.left,set);
+        boolean rightFound = traverse1(node.right,set);
         
-        if(set.contains(node.val)) return true;
-        boolean left = traverse1(node.left,set);
-        boolean right = traverse1(node.right,set);
-        
-        return (left||right);
+        return leftFound || rightFound;
     }
+    
     public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
         HashSet<Integer> set = new HashSet<>();
         traverse(root1,target,set);
