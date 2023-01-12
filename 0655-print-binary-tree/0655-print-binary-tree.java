38ce67a10705e580e8711c4f15e6a48c934a8c67
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-12 09:34:46 +0530

    Time: 2 ms (54.68%), Space: 42.8 MB (62.23%) - LeetHub

--- 0655-print-binary-tree/0655-print-binary-tree.java
+++ 0655-print-binary-tree/0655-print-binary-tree.java
@@ -32,6 +32,15 @@ class Solution {
         fillAns(ans,mid+1,right,level+1,node.right);
     }
     
+    public class pair{
+        TreeNode node;
+        int c;
+        pair(TreeNode node,int c) {
+            this.node = node;
+            this.c = c;
+        }
+    }
+    
     public List<List<String>> printTree(TreeNode root) {
         List<List<String>> ans = new ArrayList<>();
         int height = getHeight(root);
@@ -46,12 +55,28 @@ class Solution {
             ans.add(list);
         }
         
-        int left =0;
-        int right = col-1;
-        int level = 0;
+//         int left =0;
+//         int right = col-1;
+//         int level = 0;
         
-        fillAns(ans,left,right,level,root);
+//         fillAns(ans,left,right,level,root);
         
+        LinkedList<pair> que = new LinkedList<>();
+        que.addLast(new pair(root,(col-1)/2));
+        int r = 0;
+        while(que.size()!=0) {
+            int size = que.size();
+            while(size-->0) {
+                pair removeP = que.removeFirst();
+                TreeNode removeN = removeP.node;
+                int nodeC = removeP.c;
+                ans.get(r).set(nodeC,""+removeN.val);
+                
+                if(removeN.left!=null) que.addLast(new pair(removeN.left,nodeC-(int)Math.pow(2,height-1-r)));
+                if(removeN.right!=null) que.addLast(new pair(removeN.right,nodeC+(int)Math.pow(2,height-1-r)));
+            }
+            r++;
+        }
         return ans;
     }
 }
