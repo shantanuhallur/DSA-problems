646ed1d01d96add33c55cdaba0fcc90496431538
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-13 14:26:46 +0530

    Time: 5 ms (81.97%), Space: 42.2 MB (91.80%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
@@ -14,33 +14,31 @@
  * }
  */
 class Solution {
-    int count=0;
-    public int[] getPair(TreeNode node,int d) {
-        if(node == null) return new int[]{};
-        if(node.left == null && node.right == null) {
-            return new int[]{1};
-        }
+    int count = 0;
+    //faith each node will return all the leaves nodes distances under that node
+    public int[] getPairs(TreeNode node,int d) {
+        if(node==null) return new int[]{};
+        if(node.left == null && node.right == null) return new int[]{1};
         
-        int[] lAns = getPair(node.left,d);
-        int[] rAns = getPair(node.right,d);
+        int[] lAns = getPairs(node.left,d);
+        int[] rAns = getPairs(node.right,d);
         
-        int n = lAns.length; int m = rAns.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++) {
-                if(lAns[i] + rAns[j] <= d) count++;
+        for(int val1 : lAns) {
+            for(int val2 : rAns) {
+                if(val1+val2 <= d) count++;
             }
         }
+        int n = lAns.length , m = rAns.length;
+        int[] myAns = new int[n+m];
+        int i = 0;
+        for(int val1:lAns) myAns[i++] = val1+1;
+        for(int val2:rAns) myAns[i++] = val2+1;
         
-        int i=0;
-        int[] res = new int[n+m];
-        for(int val:lAns) res[i++] = val+1;
-        for(int val:rAns) res[i++] = val+1;
-        
-        return res;
+        return myAns;
     }
     public int countPairs(TreeNode root, int distance) {
-        if(distance <=1 || root==null) return 0;
-        getPair(root,distance);
+        if(root == null || distance <= 1) return 0;
+        getPairs(root,distance);
         return count;
     }
 }
