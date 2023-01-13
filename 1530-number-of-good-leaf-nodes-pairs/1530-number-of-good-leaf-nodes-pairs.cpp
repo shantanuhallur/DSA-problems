bcbdf467e1317bcc6b85369e21adebd5c74b72ab
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-13 15:43:18 +0530

    Time: 72 ms (84.87%), Space: 33.5 MB (92.58%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -11,31 +11,34 @@
  */
 class Solution {
 public:
-    int count = 0;
-    
+    int pairC = 0;
     vector<int> getPairs(TreeNode* node, int d) {
+        //Base Case
         if(!node) return {};
+        //if leaf create and return distance 1
         if(!node->left && !node->right) return {1};
-        
+        //left and right recursive call for information
         vector<int> lAns = getPairs(node->left,d);
         vector<int> rAns = getPairs(node->right,d);
-        
+        //try to make pairs with all leaves if left and right
         for(int val1 : lAns) {
             for(int val2 : rAns) {
-                if(val1 + val2 <= d) count++;
+                //check if our pair is valid
+                if(val1 + val2 <= d) pairC ++;
             }
         }
-        int n = lAns.size() , m = rAns.size();
-        
+        int n = lAns.size(); int m = rAns.size();
         vector<int> myAns(n+m,0);
+         //distance upate for leaves to return to parent node;
         int i = 0;
         for(int val : lAns) myAns[i++] = val+1;
         for(int val : rAns) myAns[i++] = val+1;
-        
+        //return this updated myAns to parent
         return myAns;
     }
+        
     int countPairs(TreeNode* root, int distance) {
         getPairs(root,distance);
-        return count;
+        return pairC;
     }
 };
