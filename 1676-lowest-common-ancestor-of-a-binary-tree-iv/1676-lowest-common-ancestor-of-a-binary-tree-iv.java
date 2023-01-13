e2cc216f75eafc488fb5f41b586165f83a453025
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-13 19:39:39 +0530

    Time: 6 ms (61.00%), Space: 44.7 MB (54.00%) - LeetHub

--- 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.java
+++ 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.java
@@ -10,17 +10,22 @@
 class Solution {
     TreeNode LCA = null;
     
-    public int getLCA (TreeNode node,HashSet<Integer> set) {
+    public int getLCA(TreeNode node,HashSet<Integer> set) {
+        //Base Case
         if(node == null) return 0;
-        
+        //left and right recursive call
         int leftFound = getLCA(node.left,set);
         int rightFound = getLCA(node.right,set);
+        //calculate totalFOund
         int totalFound = leftFound + rightFound;
+        //check if current node is to be searched for
         if(set.contains(node.val)) totalFound++;
-        if(LCA == null && totalFound == set.size()) LCA = node;
         
+        if(LCA == null && totalFound == set.size()) LCA = node;
+        //return totalfound to parent
         return totalFound;
     }
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
         HashSet<Integer> set = new HashSet<>();
         for(TreeNode node : nodes) set.add(node.val);
