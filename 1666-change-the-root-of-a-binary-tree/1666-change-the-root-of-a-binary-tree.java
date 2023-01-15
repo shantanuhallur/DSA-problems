99c441e4467130630a8b5ea41e08b2aedbab186b
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-15 06:43:25 +0530

    Time: 13 ms (100.00%), Space: 41.4 MB (92.00%) - LeetHub

---
+++ 1666-change-the-root-of-a-binary-tree/1666-change-the-root-of-a-binary-tree.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+Node original_root;
+    
+public Node flipBinaryTree(Node root, Node leaf) {
+	original_root = root;
+	return helper(leaf, null); // the new parent of the leaf node must be null
+}
+
+public Node helper(Node node, Node new_parent) {
+	Node old_parent = node.parent;
+	node.parent = new_parent;
+
+	if (node.left == new_parent) node.left = null;
+	if (node.right == new_parent) node.right = null;
+
+	if (node == original_root) return node;
+
+	if (node.left != null) node.right = node.left;
+	node.left = helper(old_parent, node);
+
+	return node;
+}
+}
