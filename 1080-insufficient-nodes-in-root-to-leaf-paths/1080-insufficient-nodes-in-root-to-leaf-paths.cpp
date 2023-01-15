5e2f4aa038eb863e63233f1aea59d5ebe5e46cc2
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-15 08:15:41 +0530

    Time: 50 ms (71.17%), Space: 33.1 MB (13.96%) - LeetHub

---
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* getTree(TreeNode* node,int limit) {
+        if(!node) return NULL;
+        if(!node->left && !node->right) return node->val < limit ? NULL : node;
+        
+        node->left = getTree(node->left,limit-node->val);
+        node->right = getTree(node->right,limit-node->val);
+        
+        return node->left == node->right ? NULL : node;
+    }
+    TreeNode* sufficientSubset(TreeNode* root, int limit) {
+        return getTree(root,limit);
+    }
+};
