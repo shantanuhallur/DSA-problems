6e289ecbff188f56ec763c732942373b64160b60
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-15 18:10:12 +0530

    Time: 19 ms (90.13%), Space: 11.6 MB (48.03%) - LeetHub

--- 0510-inorder-successor-in-bst-ii/0510-inorder-successor-in-bst-ii.cpp
+++ 0510-inorder-successor-in-bst-ii/0510-inorder-successor-in-bst-ii.cpp
@@ -10,25 +10,25 @@ public:
 */
 
 class Solution {
-public:
-    Node* getLeftMostChild(Node* node) {
+public:    
+    Node* findLeftMostNode(Node* node) {
         while(node->left) node = node->left;
-        
         return node;
     }
     
-    Node* getParSucc(Node* node) {
-        if(node->parent == NULL) return NULL;
-        
-        if(node == node->parent->right) return getParSucc(node->parent);
-        
+    Node* findSuccInParent(Node* node) {
+        //no parent means no node is greater or i am the root node hence return null
+        if(!node->parent) return NULL;
+        //check if current node is a right child of a parent if yes we need to recurse on our parent
+        if(node->parent->right == node) return findSuccInParent(node->parent);
+        //if not then our node is a left child of a parent node means our parent is the successor
         return node->parent;
     }
     Node* inorderSuccessor(Node* node) {
         if(!node) return NULL;
-        
-        if(node->right) return getLeftMostChild(node->right);
-        
-        return getParSucc(node);
+        //if potential answer to my nodes right exist
+        if(node->right) return findLeftMostNode(node->right);
+        //else it means that no node exists in my nodes right child hence answer is in my parents
+        return findSuccInParent(node);
     }
 };
