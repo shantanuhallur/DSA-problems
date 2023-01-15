6fdedc6b2f3b1854871b5c7b213cad5d3a364448
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-15 17:31:43 +0530

    Time: 34 ms (68.93%), Space: 44.1 MB (58.58%) - LeetHub

--- 0510-inorder-successor-in-bst-ii/0510-inorder-successor-in-bst-ii.java
+++ 0510-inorder-successor-in-bst-ii/0510-inorder-successor-in-bst-ii.java
@@ -9,22 +9,20 @@ class Node {
 */
 
 class Solution {
-    public Node getParentSucc(Node node) {
-        if(node.parent == null) return null;
-        
-        if(node == node.parent.right) return getParentSucc(node.parent);
-        
+    public Node getParentSuccessor(Node node) {
+        if(node == null || node.parent == null) return null;
+        if(node.parent.right == node) return getParentSuccessor(node.parent);
         return node.parent;
     }
     
-    public Node getLeftMostChild(Node node) {
-        while(node.left != null) node = node.left;
+    public Node getLeftMost(Node node) {
+        while(node.left!=null) node = node.left;
         return node;
     }
     public Node inorderSuccessor(Node node) {
         if(node == null) return null;
         
-        if(node.right!=null) return getLeftMostChild(node.right);
-        else return getParentSucc(node);
+        if(node.right != null) return getLeftMost(node.right);
+        return getParentSuccessor(node);
     }
 }
