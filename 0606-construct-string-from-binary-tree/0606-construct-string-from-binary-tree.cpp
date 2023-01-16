6dc97ac84849c8ec977d27ceed12cfe574116623
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-16 18:56:25 +0530

    Time: 31 ms (60.64%), Space: 66.3 MB (13.06%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
@@ -12,12 +12,14 @@
 class Solution {
 public:
     string tree2str(TreeNode* root) {
-        string str = "";
-        
-        if(root->left) str+= "(" + tree2str(root->left) + ")";
-        if(!root->left && root->right) str += "()";
-        if(root->right) str+= "(" + tree2str(root->right) + ")";
-        
-        return to_string(root->val) + str;
+        string ans = "";
+        //left recursive call
+        if(root->left) ans+= "(" + tree2str(root->left) + ")";
+        //if left doesnt exist but right child does exist
+        if(!root->left && root->right)  ans+= "()";
+        //right recursive call
+        if(root->right) ans+= "(" + tree2str(root->right) + ")";
+        //append ans to my rootnode's val
+        return to_string(root->val) + ans; 
     }
 };
