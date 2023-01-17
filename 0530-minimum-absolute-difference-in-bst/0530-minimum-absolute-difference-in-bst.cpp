2f0cce8057e0a8feaab847038a71443c2bf4e53e
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-17 20:14:16 +0530

    Time: 15 ms (93.47%), Space: 25.2 MB (81.06%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -11,41 +11,20 @@
  */
 class Solution {
 public:
-            int getMin(vector<int>& arr) {
-        int min_ = arr[0];
-        for(int ele : arr) {
-            min_ = min(ele,min_);
+    TreeNode* prev = NULL;
+    int minN = 1e8;
+    void getAns(TreeNode* node) {
+        if(!node) return;
+        
+        getAns(node->left);
+        if(prev!= NULL) {
+            if(node->val - prev->val < minN) minN = node->val - prev->val; 
         }
-        return min_;
+        prev = node;
+        getAns(node->right);
     }
-    
-    int ans = (int)1e8;
-     vector<int> getMinMax(TreeNode* node) {
-        if(node == nullptr) {
-            vector<int> base = {(int)1e8,-(int)1e8};
-            return base;
-        }
-            
-        vector<int> leftMinMax =  getMinMax(node->left);
-           
-        vector<int> rightMinMax = getMinMax(node->right);
-        
-        //ans
-         vector<int> arr = {ans,abs(leftMinMax[0]-node->val),abs(leftMinMax[1]-node->val),abs(rightMinMax[0]-                                      node->val),abs(rightMinMax[1]-node->val)};
-        ans = getMin(arr);
-        
-        vector<int> myNodeMinMax = {(int)1e8,-(int)1e8};
-        
-        //min
-        myNodeMinMax[0] = min(leftMinMax[0],min(node->val,rightMinMax[0]));
-        //max
-        myNodeMinMax[1] = max(leftMinMax[1],max(node->val,rightMinMax[1]));
-        
-        return myNodeMinMax; 
-    }
-    
     int getMinimumDifference(TreeNode* root) {
-        vector<int> dummy = getMinMax(root);
-        return ans;
+        getAns(root);
+        return minN;
     }
 };
