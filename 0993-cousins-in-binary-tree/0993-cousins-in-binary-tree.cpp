a80dba15b3a1bd2f9a5d2a9b53477e6cd5781ff8
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-18 17:45:13 +0530

    Time: 6 ms (39.57%), Space: 10.7 MB (98.30%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -11,30 +11,37 @@
  */
 class Solution {
 public:
-    int par = -1;
+    int parent = -1;
     int getDepth(TreeNode* node,int depth,int target) {
+        //Base Case
         if(node->val == target) return depth;
-        
+        //left and right DFS/Recursive calls
         if(node->left) {
-            if(node->left->val == target) par = node->val;
+            //check if curr node is a parent to my target node
+            if(node->left->val == target) parent = node->val;
+            //get height form child
             int lAns = getDepth(node->left,depth+1,target);
             if(lAns != 0) return lAns;
         }
-        
         if(node->right) {
-            if(node->right->val == target) par = node->val;
+            //check if curr node is a parent to my target node
+            if(node->right->val == target) parent = node->val;
+            //get height form child
             int rAns = getDepth(node->right,depth+1,target);
-            if(rAns !=0) return rAns;
+            if(rAns != 0) return rAns;
         }
         return 0;
     }
+    
     bool isCousins(TreeNode* root, int x, int y) {
-        int depth1 = getDepth(root,0,x);
-        int par1 = par;
-        par = -1;
-        int depth2 = getDepth(root,0,y);
-        int par2 = par;
-        
-        return depth1 == depth2 && par1 != par2 ? true : false; 
+        //get dep & par for X
+        int dep1 = getDepth(root,0,x);
+        int par1 = parent;
+        parent = -1;
+        //get dep & par for Y
+        int dep2 = getDepth(root,0,y);
+        int par2 = parent;
+        //Check if both are Cousin or not.
+        return dep1==dep2 && par1 != par2 ? true : false;
     }
 };
