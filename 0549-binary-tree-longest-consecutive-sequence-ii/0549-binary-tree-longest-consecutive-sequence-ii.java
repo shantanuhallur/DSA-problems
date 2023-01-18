67bca36a09815fc6a4760cd182ea8241ab1f12a6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-18 08:06:40 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (50.79%) - LeetHub

---
+++ 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public class pair{
+        int inc;
+        int dec;
+        pair(int inc,int dec) {
+            this.inc = inc;
+            this.dec = dec;
+        }
+    }
+    int maxLen = -1;
+    public pair getLCS(TreeNode node) {
+        if(node == null) return new pair(0,0);
+        
+        int  leftInc = 1 , leftDec = 1;
+        int rightInc = 1, rightDec = 1;
+        
+        if(node.left!=null) {
+            pair lAns = getLCS(node.left);
+            if(node.val == node.left.val +1) {
+                leftInc = lAns.inc + 1;
+            }
+            else if(node.val +1 == node.left.val) {
+                leftDec = lAns.dec +1;
+            }
+        }
+        
+        if(node.right!=null) {
+            pair rAns = getLCS(node.right);
+            if(node.val == node.right.val+1) {
+                rightInc = rAns.inc + 1;
+            }
+            else if(node.val + 1 == node.right.val) {
+                rightDec = rAns.dec + 1;
+            }
+        }
+        
+        int maxInc = Math.max(leftInc,rightInc);
+        int maxDec = Math.max(leftDec,rightDec);
+        
+        maxLen = Math.max(maxLen,Math.max(maxInc,maxDec));
+        maxLen = Math.max(maxLen,maxInc+maxDec -1);
+        
+        return new pair(maxInc,maxDec);
+    }
+    public int longestConsecutive(TreeNode root) {
+        getLCS(root);
+        return maxLen;
+    }
+}
