9fb4122aedb99ad456684a8d804517836019c285
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-18 15:10:41 +0530

    Time: 4 ms (53.53%), Space: 42.2 MB (89.53%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,22 +15,28 @@
  */
 class Solution {
     public boolean areEqual(TreeNode node1,TreeNode node2) {
-        if(node1==null && node2 == null) return true;
-        if(node1 == null || node2 == null) return false;
-        if(node1.val != node2.val) return false;
+        if(node1 == null && node2 == null) return true; // both are null hence equal.
+        if(node1 == null || node2 == null) return false; // only 1 node exists out of the 2.
+        if(node1.val != node2.val) return false; // both nodes are present but dont have equal values
         
-        return areEqual(node1.left,node2.left) && areEqual(node1.right,node2.right);
+        //left and right recursive call
+        boolean lEqual = areEqual(node1.left,node2.left);
+        boolean rEqual = areEqual(node1.right,node2.right);
+        
+        return (lEqual && rEqual);
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        //Base Case
         if(root == null) return false;
         
         if(root.val == subRoot.val) {
             if(areEqual(root,subRoot)) return true;
         }
         
+        //left and right recursive calls
         boolean lAns = isSubtree(root.left,subRoot);
         boolean rAns = isSubtree(root.right,subRoot);
         
-        return lAns || rAns;
+        return (lAns || rAns);
     }
 }
