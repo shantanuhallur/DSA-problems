c61c9a386fb925cf5aaf49d3bb1e817ffd1fe118
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-18 17:06:46 +0530

    Time: 4 ms (61.17%), Space: 11.1 MB (45.34%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -12,35 +12,29 @@
 class Solution {
 public:
     int par = -1;
-    
-    int getDepth(TreeNode* node, int depth, int num) {
-        if(node->val == num) {
-            return depth;
+    int getDepth(TreeNode* node,int depth,int target) {
+        if(node->val == target) return depth;
+        
+        if(node->left) {
+            if(node->left->val == target) par = node->val;
+            int lAns = getDepth(node->left,depth+1,target);
+            if(lAns != 0) return lAns;
         }
         
-        if(node->left != nullptr) {
-            if(node->left->val == num) par = node->val;
-            int leftAns = getDepth(node->left,depth+1,num);
-            if(leftAns != 0) return leftAns;
+        if(node->right) {
+            if(node->right->val == target) par = node->val;
+            int rAns = getDepth(node->right,depth+1,target);
+            if(rAns !=0) return rAns;
         }
-        
-        if(node->right != nullptr) {
-            if(node->right->val == num) par = node->val;
-            int rightAns = getDepth(node->right,depth+1,num);
-            if(rightAns != 0) return rightAns;
-        }
-        
         return 0;
     }
-    
     bool isCousins(TreeNode* root, int x, int y) {
         int depth1 = getDepth(root,0,x);
         int par1 = par;
         par = -1;
-        
         int depth2 = getDepth(root,0,y);
         int par2 = par;
         
-        return depth1 == depth2 && par1 != par2 ? true : false;
+        return depth1 == depth2 && par1 != par2 ? true : false; 
     }
 };
