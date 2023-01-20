f67408b79880da0a612a1e6a3a9ee2ecec8dec3d
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-20 14:40:25 +0530

    Time: 1 ms (99.03%), Space: 43.8 MB (89.21%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,18 +15,20 @@
  */
 class Solution {
     int ans = -1001;
-    public int getPathSum(TreeNode node) {
-        if(node ==  null) return -1001;
-        //PS passing through my left and right child
-        int l = getPathSum(node.left); 
+    int getPathSum(TreeNode node) {
+        if(node == null) return 0;
+        
+        int l = getPathSum(node.left);
         int r = getPathSum(node.right);
-        int myMaxsum = Math.max(Math.max(l+node.val,r+node.val),Math.max(node.val,l+r+node.val));
-        ans = Math.max(ans,myMaxsum);
-        //MaxSum Path passing through my node;
-        return Math.max(node.val,Math.max(l,r) + node.val);
+        
+        int maxTillFromChildsToMe = Math.max(Math.max(l,r) + node.val,node.val);
+        int potentialMaxThroughMe = Math.max(maxTillFromChildsToMe,l+r+node.val);
+        ans = Math.max(ans,potentialMaxThroughMe);
+        
+        return maxTillFromChildsToMe;
     }
     public int maxPathSum(TreeNode root) {
-        if(root == null) return 0;
+        if(root==null) return 0;
         getPathSum(root);
         return ans;
     }
