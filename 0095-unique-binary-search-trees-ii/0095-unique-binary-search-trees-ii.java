9dbdf2042f92f1a74004b36cfc68aacd408f7cb3
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-20 17:32:07 +0530

    Time: 1 ms (99.29%), Space: 43.2 MB (42.29%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -15,14 +15,15 @@
  */
 class Solution {
     public List<TreeNode> generateTree(int st,int end) {
-        if(st>end){
-            List<TreeNode> base = new ArrayList<>();
+        //Base Case
+        if(st>end) {
+            ArrayList<TreeNode> base = new ArrayList<>();
             base.add(null);
             return base;
         }
         List<TreeNode> ans = new ArrayList<>();
-        
-        for(int i=st;i<=end;i++) {
+        for(int i =st;i<=end;i++) {
+            //left and right recursive call
             List<TreeNode> left = generateTree(st,i-1);
             List<TreeNode> right = generateTree(i+1,end);
             for(TreeNode l : left) {
@@ -37,6 +38,6 @@ class Solution {
         return ans;
     }
     public List<TreeNode> generateTrees(int n) {
-     return generateTree(1,n);   
+        return generateTree(1,n);
     }
 }
