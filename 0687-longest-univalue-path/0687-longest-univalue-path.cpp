71775daf9e0c35902620b07d4d8fbc30e2ee6503
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-20 06:56:14 +0530

    Time: 147 ms (75.22%), Space: 71.9 MB (53.26%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.cpp
+++ 0687-longest-univalue-path/0687-longest-univalue-path.cpp
@@ -11,27 +11,25 @@
  */
 class Solution {
 public:
-    //gloabal  ans which hold LUP in whole tree
     int ans = 0;
-    int getLUP(TreeNode* node) {
-        //Base Case
-        if(node == NULL) return 0;
-        //left and right recursive call of faith -> get me LUP throught MY L&R CHILD
-        int l = getLUP(node->left);
-        int r = getLUP(node->right);
+    int getUVP(TreeNode* node) {
+        if(!node) return 0;
+        
+        int l = getUVP(node->left);
+        int r = getUVP(node->right);
         
         if(node->left && node->left->val == node->val) l+=1;
         else l=0;
         
         if(node->right && node->right->val == node->val) r+=1;
-        else r =0;
-        //Maximize ans's LUP
+        else r=0;
+        
         ans = max(ans,l+r);
-        //return value will be as per our faith the max UVP THROUGH MY ROOT NODE
+        
         return max(l,r);
     }
     int longestUnivaluePath(TreeNode* root) {
-        getLUP(root);
+        getUVP(root);
         return ans;
     }
 };
