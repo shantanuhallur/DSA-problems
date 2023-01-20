69034520a0bb3a26fa9cf903537b2466934a5590
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-20 06:52:51 +0530

    Time: 2 ms (99.87%), Space: 46.2 MB (40.00%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.java
+++ 0687-longest-univalue-path/0687-longest-univalue-path.java
@@ -14,28 +14,25 @@
  * }
  */
 class Solution {
-    //gloabal  ans which hold LUP in whole tree
-    int ans = 0;
-    
-    int getLUP(TreeNode node) {
-        //Base Case
+    int ans =0;
+    public int getUVP(TreeNode node) {
         if(node == null) return 0;
-        //left and right recursive call of faith -> get me LUP throught MY L&R CHILD
-        int l = getLUP(node.left);
-        int r = getLUP(node.right);
         
-        if(node.left != null && node.left.val == node.val) l += 1;
+        int l = getUVP(node.left);
+        int r = getUVP(node.right);
+        
+        if(node.left !=null && node.left.val == node.val) l +=1;
         else l = 0;
-        
-        if(node.right != null && node.right.val == node.val) r += 1;
+        if(node.right !=null && node.right.val == node.val) r += 1;
         else r = 0;
-        //Maximize ans's LUP
+        
         ans = Math.max(ans,l+r);
-        //return value will be as per our faith the max UVP THROUGH MY ROOT NODE
+        
         return Math.max(l,r);
     }
     public int longestUnivaluePath(TreeNode root) {
-        getLUP(root);
+        if(root==null) return 0;
+        getUVP(root);
         return ans;
     }
 }
