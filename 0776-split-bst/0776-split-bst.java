85967d90d15959500b2bd013cf6aae8e38cf5506
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-21 08:50:40 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (39.22%) - LeetHub

--- 0776-split-bst/0776-split-bst.java
+++ 0776-split-bst/0776-split-bst.java
@@ -14,26 +14,22 @@
  * }
  */
 class Solution {
-    public TreeNode[] split(TreeNode node,int target) {
-        TreeNode[] res = new TreeNode[2];
-        if(node == null) return res;
-        
-        if(node.val <= target) {
-            res[0] = node;
-            TreeNode[] rAns = split(node.right,target);
-            node.right = rAns[0];
-            res[1] = rAns[1];
-        }
-        else if(node.val > target) {
-            res[1] = node;
-            TreeNode[] lAns = split(node.left,target);
-            node.left = lAns[1];
-            res[0] = lAns[0];
-        }
-        return res;
-    }
     public TreeNode[] splitBST(TreeNode root, int target) {
-        TreeNode[] ans = split(root,target);
-        return ans;
+        TreeNode[] tree = new TreeNode[2];
+        if(root==null) return new TreeNode[2];
+        
+        if(root.val <= target) {
+            tree[0] = root;
+            TreeNode[] rAns = splitBST(root.right,target);
+            root.right = rAns[0];
+            tree[1] = rAns[1];
+        }
+        else if(root.val>target) {
+            tree[1] = root;
+            TreeNode[] lAns = splitBST(root.left,target);
+            root.left = lAns[1];
+            tree[0] = lAns[0];
+         }
+        return tree;
     }
 }
