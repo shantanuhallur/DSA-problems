5702b2905d8362f9d6624dd2b73e0feada49e9a4
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-22 17:39:38 +0530

    Time: 55 ms (41.56%), Space: 29.5 MB (24.53%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+        void serialize(TreeNode* node,string& sb) {
+        if(node==NULL){
+            sb+= "#,";
+            return;
+        }
+        sb+= to_string(node->val) + ",";
+        serialize(node->left,sb);
+        serialize(node->right,sb);
+    }
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string sb="" ;
+        serialize(root,sb);
+        cout<<sb;
+        return sb;
+    }
+    int s = 0;
+    TreeNode* buildTree(vector<string>& arr,int end){
+        if(s>end) return NULL;
+        if(arr[s]=="#") {
+            s++;
+            return NULL;
+        }
+        
+        TreeNode* root = new TreeNode(stoi(arr[s++])); 
+        root->left =  buildTree(arr,end);
+        root->right = buildTree(arr,end);
+        return root;
+    }
+    vector<string> arr1;
+    void adv_tokenizer(string s, char del)
+{
+    stringstream ss(s);
+    string word;
+    while (!ss.eof()) {
+        getline(ss, word, del);
+        // cout << word << endl;
+        arr1.push_back(word);
+    }
+}
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        
+        adv_tokenizer(data,',');
+        TreeNode* root = buildTree(arr1,data.size()-1);
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
