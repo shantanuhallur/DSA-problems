0c96f0f4267c4c031f13ebbbf9b45f4a69ab384a
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-23 15:03:11 +0530

    Time: 31 ms (93.08%), Space: 29.4 MB (24.93%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -10,52 +10,48 @@
 class Codec {
 public:
     
-    void serialize(TreeNode* node, string& sb) {
+    void serialize(TreeNode* node,string& s) {
         if(!node) {
-            sb+="#,";
+            s +="#,";
             return;
         }
         
-        sb+= to_string(node->val) + ",";
-        serialize(node->left,sb);
-        serialize(node->right,sb);
+        s+= to_string(node->val)+",";
+        serialize(node->left,s);
+        serialize(node->right,s);
     }
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        string sb = "";
+        string sb ="";
         serialize(root,sb);
-        return sb;
+        return sb ;
     }
-
-    int st = 0;
-    TreeNode* deserialize(vector<string>& arr,int end) {
-        if(st>end) {
-            return NULL;
+    
+    vector<string> arr;
+    void convert(string& data){
+        stringstream ss(data);
+        string word;
+        while(!ss.eof()){
+            getline(ss,word,',');
+            arr.push_back(word);
         }
-        if(arr[st]=="#") {
+    }
+    int st = 0;
+    TreeNode* deserialize(int end) {
+        if(st>end) return NULL;
+        if(arr[st] =="#") {
             st++;
             return NULL;
         }
-        
         TreeNode* root = new TreeNode(stoi(arr[st++]));
-        root->left = deserialize(arr,end);
-        root->right = deserialize(arr,end);
+        root->left = deserialize(end);
+        root->right = deserialize(end);
         return root;
-    }
-    vector<string> arr;
-    void convert(string& data,char delimiter) {
-        stringstream ss(data);
-        string word;
-        while(!ss.eof()) {
-            getline(ss,word,','); 
-            arr.push_back(word);
-            
-        }
     }
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        convert(data,',');
-        TreeNode* root = deserialize(arr,arr.size()-1);
+        convert(data);
+        TreeNode* root = deserialize(arr.size()-1);
         return root;
     }
 };
