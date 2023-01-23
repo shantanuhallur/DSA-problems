8c9bdfbf1029ad2ba32f8b90d0d5b9e4b60321cb
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-23 16:42:57 +0530

    Time: 22 ms (27.62%), Space: 49.2 MB (17.64%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
@@ -9,38 +9,45 @@
  */
 public class Codec {
 
-    public void serialize(TreeNode node,StringBuilder sb) {
-        if(node==null){
+    void serialize(TreeNode node, StringBuilder sb) {
+        //Bsae Case
+        if(node==null) {
             sb.append("#,");
             return;
         }
+        
         sb.append(node.val+",");
+        //preorder traversal
         serialize(node.left,sb);
         serialize(node.right,sb);
     }
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        //create a stringbuilder sb and traverse and create preorder string;
         StringBuilder sb = new StringBuilder();
         serialize(root,sb);
+        System.out.print(sb);
         return sb.toString();
     }
     int st = 0;
-    public TreeNode deserialize(String[] arr,int end) {
+    public TreeNode deserialize(String[] arr ,int end) {
+        //Base Case
         if(st>end) return null;
         if(arr[st].equals("#")) {
             st++;
             return null;
         }
-        TreeNode root =  new TreeNode(Integer.parseInt(arr[st++]));
+        TreeNode root = new TreeNode(Integer.parseInt(arr[st++]));
+        //recursively build tree in left and right
         root.left = deserialize(arr,end);
         root.right = deserialize(arr,end);
         return root;
     }
-
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        //create String array
         String[] arr = data.split(",");
-        TreeNode root =deserialize(arr,arr.length-1);
+        TreeNode root = deserialize(arr,arr.length-1);
         return root;
     }
 }
