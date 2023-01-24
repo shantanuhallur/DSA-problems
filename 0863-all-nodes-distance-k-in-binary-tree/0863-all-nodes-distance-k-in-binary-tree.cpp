78cd80459425f9f64cdc00e84991b1600ddbfe77
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-24 19:58:44 +0530

    Time: 0 ms (100.00%), Space: 12.8 MB (78.06%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,47 +9,53 @@
  */
 class Solution {
 public:
-    TreeNode* dfs(TreeNode* node,TreeNode* target,unordered_map<TreeNode*,TreeNode*>& map) {
+    TreeNode* dfs(TreeNode* node,TreeNode* target,unordered_map<int,TreeNode*>& bm) {
         if(!node) return NULL;
         if(node->val == target->val) return node;
-        TreeNode* left = dfs(node->left,target,map);
+        //LEFT AND RIGHT RECURSIVE CALL
+        TreeNode* left = dfs(node->left,target,bm);
         if(left) {
-            map[node->left] = node;
+            bm[node->left->val] = node;
             return left;
         }
-        TreeNode* right = dfs(node->right,target,map);
+        TreeNode* right = dfs(node->right,target,bm);
         if(right) {
-            map[node->right] = node;
+            bm[node->right->val] = node;
             return right;
         }
         return NULL;
     }
+    
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*,TreeNode*> map;
-        TreeNode* tar = dfs(root,target,map);
+        unordered_map<int,TreeNode*>bm;
+        TreeNode* start = dfs(root,target,bm);
         set<int> vis;
         queue<TreeNode*> que;
-        que.push(tar);
-        while(k-->0) {
-            int size = que.size();
-            while(size-->0) {
-                TreeNode* rn = que.front(); que.pop();
-                
-                if(rn->left && vis.find(rn->left->val)==vis.end()) {
-                    que.push(rn->left);
-                    vis.insert(rn->left->val);
-                }
-                if(rn->right && vis.find(rn->right->val)==vis.end()) {
-                    que.push(rn->right);
-                    vis.insert(rn->right->val);
-                }
-                if(map.find(rn)!=map.end() && vis.find(map[rn]->val)==vis.end()) {
-                    que.push(map[rn]);
-                    vis.insert(rn->val);
-                }
-            }
-        }
-        vector<int> ans;
+        que.push(start);
+       //BFS
+       while(k-->0) {
+           int size = que.size();
+           while(size-->0) {
+               TreeNode* rn = que.front(); que.pop();
+               if(rn->left && vis.find(rn->left->val)==vis.end()) {
+                   que.push(rn->left);
+                   vis.insert(rn->left->val);
+               }
+               
+               if(rn->right && vis.find(rn->right->val)==vis.end()) {
+                   que.push(rn->right);
+                   vis.insert(rn->right->val);
+               }
+               //check if any unvisited parent exists in our bm
+               if(bm.find(rn->val)!=bm.end() && vis.find(bm[rn->val]->val)==vis.end()) {
+                   que.push(bm[rn->val]);
+                   vis.insert(rn->val);
+               }
+               
+           }
+       } 
+       vector<int> ans;
+        
         while(que.size()>0) {
             ans.push_back(que.front()->val);
             que.pop();
