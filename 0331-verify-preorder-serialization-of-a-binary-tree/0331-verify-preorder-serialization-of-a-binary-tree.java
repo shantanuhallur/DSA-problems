8989a5622efcb09cccb86490bf88f49103a88f12
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-24 15:29:42 +0530

    Time: 9 ms (33.68%), Space: 43.6 MB (35.01%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
         String[] nodes = preorder.split(",");
-        int diff=1;
+        //difference of Edges( Outdegree - Indegree);
+        //root special case diff=1;
+        int diff = 1;
         for(String node : nodes) {
-            if(--diff<0) return false;
+            if(--diff<0)return false;
             if(!node.equals("#")) diff+=2;
         }
         return diff==0;
