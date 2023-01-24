193e4b58e5ca545acae70f5c7aa52206cbb9992e
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-24 16:12:04 +0530

    Time: 24 ms (12.68%), Space: 43.6 MB (11.55%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -7,55 +7,55 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {   
-    public TreeNode dfs(TreeNode node,TreeNode target,HashMap<TreeNode,TreeNode> backMap) {
+class Solution {
+    public TreeNode dfs(TreeNode node,TreeNode target, HashMap<TreeNode,TreeNode> backMap) {
         if(node==null) return null;
-        if(node == target) return node;
-        
-        TreeNode left =dfs(node.left,target,backMap);
-        if(left!=null){
+        if(node.val == target.val) {
+            return node;
+        }
+        TreeNode left = dfs(node.left,target,backMap);
+        if(left!=null) {
             backMap.put(node.left,node);
             return left;
         }
-        TreeNode right =dfs(node.right,target,backMap);
-        if(right!=null){
+        
+        TreeNode right = dfs(node.right,target,backMap);
+        if(right!=null) {
             backMap.put(node.right,node);
             return right;
         }
+        
         return null;
     }
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> ans = new ArrayList<>();
         HashMap<TreeNode,TreeNode> backMap = new HashMap<>();
         TreeNode tar = dfs(root,target,backMap);
+        HashSet<Integer> set = new HashSet<>();
         LinkedList<TreeNode> que = new LinkedList<>();
-        HashSet<TreeNode> vis = new HashSet<>();
-        que.addLast(tar);
-        vis.add(tar);
         
+        que.addLast(tar);
         while(k-->0) {
             int size = que.size();
-            while(size-->0){
-            TreeNode removeN = que.removeFirst();
-
-            if(removeN.left != null &&  vis.add(removeN.left)) {
-                    que.addLast(removeN.left);
-            }
-
-            if(removeN.right != null && vis.add(removeN.right)) {
-                que.addLast(removeN.right);
-            }
-
-            if(backMap.containsKey(removeN) && vis.add(backMap.get(removeN))) {
-                que.addLast(backMap.get(removeN));
-            }
+            while(size-->0) {
+                TreeNode rn = que.removeFirst();
+                
+                if(rn.left!=null && set.add(rn.left.val)) {
+                    que.addLast(rn.left);
+                }
+                
+                if(rn.right!=null && set.add(rn.right.val)) {
+                    que.addLast(rn.right);
+                }
+                
+                if(backMap.containsKey(rn) && set.add(rn.val)) {
+                    que.addLast(backMap.get(rn));
+                }
+             }
         }
-            
+        List<Integer> ans = new ArrayList<>();
+        for(TreeNode node : que) {
+            ans.add(node.val);
         }
-        for(TreeNode N : que) {
-            ans.add(N.val);
-        }
-
         return ans;
     }
 }
