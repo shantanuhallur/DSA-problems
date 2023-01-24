829f8a358e9fd64a3722be79a5cd29b5fd268524
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-24 14:47:06 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (59.74%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.cpp
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool isValidSerialization(string preorder) {
         stringstream ss(preorder);
-        string data;
-        int diff=1;
-       while(!ss.eof()) {
-           getline(ss,data,',');
-           if(--diff<0) return false;
-           if(data!="#") diff+=2;
-       }
+        string word;
+        int diff = 1;
+        while(getline(ss,word,',')) {
+            if(--diff<0) return false;
+            if(word!="#") diff+=2;
+        }
         return diff==0;
     }
 };
