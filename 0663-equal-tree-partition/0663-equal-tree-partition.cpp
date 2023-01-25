f411671b62e4a5e0c62cad42c3efd34ed2e2edbe
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-25 08:38:28 +0530

    Time: 29 ms (91.43%), Space: 32.9 MB (60.00%) - LeetHub

---
+++ 0663-equal-tree-partition/0663-equal-tree-partition.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> subtreeSum;
+    int getTotalSum(TreeNode* node) {
+        if(!node) return 0;
+        int totalSum = node->val + getTotalSum(node->left) + getTotalSum(node->right);
+        subtreeSum.push_back(totalSum);
+        return totalSum;
+    }
+    bool checkEqualTree(TreeNode* root) {
+        int totalSum = getTotalSum(root);
+        if(totalSum%2!=0)return false;
+        subtreeSum.pop_back();
+        auto it = std::find (subtreeSum.begin(), subtreeSum.end(), totalSum/2);
+        return it!=subtreeSum.end();
+    }
+};
