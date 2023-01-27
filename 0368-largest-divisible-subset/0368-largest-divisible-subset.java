32afda54a3283df0a7799533c25cee083a71af4c
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-27 08:48:29 +0530

    Time: 20 ms (96.97%), Space: 42.6 MB (60.55%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,32 +1,35 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-            int dp[] =   new int[nums.length];
-        int prev[] =  new int[nums.length];
+        int n = nums.length;
         Arrays.sort(nums);
-        int maxlen = 0,end = 0, n =  nums.length;
-        for(int i=0;i<n;i++){ //traverse over elements
-            int count  =  1;
-            int previdx = -1;
-            for(int j=0;j<i;j++){ //inclusion of an element
-                if(nums[i]%nums[j]==0 && dp[j]+1>count ){
-                    count =  dp[j]+1;
-                    previdx = j;                    
+        int[] dp = new int[n];
+        int[] prevIdx = new int[n];
+         for(int i =0;i<n;i++) {
+             dp[i]=1;
+             prevIdx[i] = -1; 
+         }
+        for(int i=0;i<n;i++) {
+            for(int j=i-1;j>=0;j--) {
+                if(nums[i]%nums[j]==0) {
+                    if(dp[j]+1>dp[i]) {
+                        dp[i] = dp[j]+1;
+                        prevIdx[i] = j;
+                    }
                 }
             }
-            dp[i] = count;
-            prev[i] = previdx;
-            
-            if(dp[i]>maxlen){
-                maxlen =  dp[i];
-                end =  i;
-            }                        
         }
-        ArrayList<Integer> ans =  new ArrayList<Integer>();
-        while(end!=-1){
-            ans.add(nums[end]);
-            end =  prev[end];
+        int max = 0; int index= 0;
+        for(int i=0;i<n;i++) {
+            if(dp[i] > max) {
+                 max = dp[i];
+                 index = i;
+            } 
         }
-        
+        List<Integer> ans = new ArrayList<>();
+        while(index != -1) {
+            ans.add(nums[index]);
+            index = prevIdx[index];
+        }
         return ans;
-        }
     }
+}
