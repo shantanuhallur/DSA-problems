f37cba7f238b720a4b8ea9a166d56e1e025b4d5b
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-28 14:52:14 +0530

    Time: 2341 ms (5.00%), Space: 292.4 MB (7.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+    int solveMemo(int n,vector<int>& nums,int curr,int prev,vector<vector<int>>& dp) {
+        if(curr==n) return 0; 
+        if(dp[curr][prev+1] != -1) return dp[curr][prev+1];
+        //Include
+        int take = 0;
+        if(prev == -1 || nums[curr]>nums[prev])
+        take = 1+ solveMemo(n,nums,curr+1,curr,dp);
+        
+        //exclude
+        int notTake = 0 + solveMemo(n,nums,curr+1,prev,dp);
+        
+        return dp[curr][prev+1] = max(take,notTake);
+    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>lis(nums.size(),1);
         int n = nums.size();
-        int omax = 0;
-            for(int i=0;i<n;i++) {
-                int max = 0;
-                for(int j=0;j<i;j++) {
-                    if(nums[i]>nums[j]) {
-                        if(max<lis[j]+1) {
-                            max = lis[j];
-                        }
-                    }
-                }
-                lis[i] = max+1;
-                if(omax<lis[i]) omax = lis[i];
-            }
-        return omax;
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        return solveMemo(n,nums,0,-1,dp);
     }
 };
