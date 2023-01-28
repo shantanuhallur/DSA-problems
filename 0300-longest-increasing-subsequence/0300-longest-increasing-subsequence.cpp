29d9bb35631c69c28d97fecbff57c4cea055d1df
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-28 15:39:05 +0530

    Time: 4 ms (98.61%), Space: 10.5 MB (76.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int solveMemo(int n,vector<int>& nums,int curr,int prev,vector<vector<int>>& dp) {
-        if(curr==n) return 0; 
-        if(dp[curr][prev+1] != -1) return dp[curr][prev+1];
-        //Include
-        int take = 0;
-        if(prev == -1 || nums[curr]>nums[prev])
-        take = 1+ solveMemo(n,nums,curr+1,curr,dp);
-        
-        //exclude
-        int notTake = 0 + solveMemo(n,nums,curr+1,prev,dp);
-        
-        return dp[curr][prev+1] = max(take,notTake);
+int solveOptimal(int n,vector<int>& nums) {
+    if(n==0) return 0;
+    vector<int> ans;
+    ans.push_back(nums[0]);
+    for(int i=0;i<n;i++) {
+        if(nums[i] > ans.back()) {
+            ans.push_back(nums[i]);
+        }
+        else {
+            int idx = lower_bound(ans.begin(),ans.end(),nums[i]) - ans.begin();
+            ans[idx] = nums[i];
+        }
     }
+    return ans.size();
+}
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solveMemo(n,nums,0,-1,dp);
+        int n =nums.size();
+        return solveOptimal(n,nums);
     }
 };
