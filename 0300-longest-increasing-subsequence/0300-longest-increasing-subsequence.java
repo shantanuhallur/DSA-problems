0613ad4fa785ad0fb82ecc0c66e00b3cd595cd1e
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-28 14:57:06 +0530

    Time: 94 ms (24.63%), Space: 44.3 MB (34.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,20 @@
-class Solution {
-    
-int solveMemo(int n,int[] nums,int curr,int prev,int[][] dp ) {
-        if(curr==n) return 0; 
-        if(dp[curr][prev+1] != -1) return dp[curr][prev+1];
-        //Include
-        int take = 0;
-        if(prev == -1 || nums[curr]>nums[prev])
-        take = 1+ solveMemo(n,nums,curr+1,curr,dp);
-        
-        //exclude
-        int notTake = 0 + solveMemo(n,nums,curr+1,prev,dp);
-        
-        return dp[curr][prev+1] = Math.max(take,notTake);
-    }  
+class Solution { 
     public int lengthOfLIS(int[] nums) {
+       int[] dp = new int[nums.length];
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
+        int maxAns=0;
         for(int i=0;i<n;i++) {
-            for(int j=-1;j<n;j++) {
-                dp[i][j+1] = -1;
+            int maxLen =0;
+            for(int j=0;j<i;j++) {
+                if(nums[i]>nums[j]) {
+                    if(dp[j]+1>maxLen) {
+                        maxLen = dp[j];
+                    }
+                }
             }
+            dp[i] = maxLen+1;
+            maxAns = Math.max(maxAns, dp[i]);
         }
-        return solveMemo(n,nums,0,-1,dp);
+        return maxAns;
     }
 }
