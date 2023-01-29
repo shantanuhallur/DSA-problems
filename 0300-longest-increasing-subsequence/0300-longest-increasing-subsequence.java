44bf06435e4db2cc769f1cc73632981f1319720d
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-29 06:36:29 +0530

    Time: 2 ms (99.52%), Space: 42.1 MB (50.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,19 @@
 class Solution {
-    /*
-    Return Type: index of the search key, if it is contained in the array; otherwise, (-(insertion point) – 1). The insertion point is defined as the point at which the key would be inserted into the array: the index of the first element greater than the key, or a.length if all elements in the array are less than the specified key. Note that this guarantees that the return value will be >= 0 if and only if the key is found.
-    */
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         int[] tails = new int[n];
         tails[0] = nums[0];
-        int len = 1;
-        
+        int len =1;
         for(int i=0;i<n;i++) {
-            if(nums[i] > tails[len-1]) {
+            if(nums[i]>tails[len-1]) {
                 tails[len] = nums[i];
                 len++;
             }
             else {
-                int idx = Arrays.binarySearch(tails,0,len-1,nums[i]);
-                if(idx<0) { 
-                    idx = -idx; //insertion point of element if not found.
-                    idx = idx-1; // overRite the element before insertion pt.
+               int idx = Arrays.binarySearch(tails,0,len-1,nums[i]);
+                if(idx<0) {
+                    idx = -idx;
+                    idx = idx-1;
                 }
                 tails[idx] = nums[i];
             }
