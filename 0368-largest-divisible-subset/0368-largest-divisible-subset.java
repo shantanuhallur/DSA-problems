d6ac5d77031ecd3968049ef64037ffed61a1f747
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-29 06:51:25 +0530

    Time: 22 ms (81.14%), Space: 42.4 MB (74.86%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -4,12 +4,13 @@ class Solution {
         Arrays.sort(nums);
         int[] dp = new int[n];
         int[] prev_idx = new int[n];
-        for(int i=0;i<n;i++) {
-            dp[i] = 1;
-            prev_idx[i] = -1;
-        }
+//         for(int i=0;i<n;i++) {
+//             dp[i] = 1;
+            
+//         }
         int OmaxLen = 0; int idx = -1;
         for(int i=0;i<n;i++) {
+            prev_idx[i] = -1;
             int maxLen =0;
             for(int j=0;j<i;j++) {
                 if(nums[i]%nums[j]==0 && dp[j]+1>maxLen) {
