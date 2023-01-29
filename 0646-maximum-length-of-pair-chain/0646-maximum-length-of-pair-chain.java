f15e706b8a489a325fffb05ced1e72057b978ed6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-29 19:46:55 +0530

    Time: 9 ms (99.36%), Space: 42.7 MB (50.45%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -3,12 +3,14 @@ class Solution {
         Arrays.sort(pairs,(e1,e2)-> {
                 return e1[1]-e2[1];
         });
-        int count = 0 , i=0,n = pairs.length;
-        while(i<n) {
-            count++;
-            int currEnd = pairs[i][1];
-            while(i<n && pairs[i][0] <= currEnd) i++;
+        int curr = -1001;
+        int ans = 0;
+        for(int i = 0;i<pairs.length;i++) {
+            if(pairs[i][0]> curr) {
+               curr = pairs[i][1];
+                ans++;
+            }
         }
-        return count;
+        return ans;
     }
 }
