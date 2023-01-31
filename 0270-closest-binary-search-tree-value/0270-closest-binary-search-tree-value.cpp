2ef09dc6c344f2466a366e9aabf3c0556bc3dc82
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-31 11:21:18 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (75.25%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
@@ -1,26 +1,29 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-    int closestValue(TreeNode* root, double target) {
-        int val,closestN  = root->val;
+    public int closestValue(TreeNode root, double target) {
+        int val,closestN  = root.val;
         
-        while(root != nullptr) {
-            val = root->val;
-            closestN = abs(val-target) < abs(closestN-target) ? val : closestN;
-            if(target < root->val) root = root->left;
-            else root = root->right;
+        while(root != null) {
+            val = root.val;
+            closestN = Math.abs(val-target) < Math.abs(closestN-target) ? val : closestN;
+            if(target < root.val) root = root.left;
+            else root = root.right;
         }
         
         return closestN;
     }
-};
+}
