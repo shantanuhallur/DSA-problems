640565f673a62eeb94cc1743f1923a3b15a823c6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-01-31 11:21:36 +0530

    Time: 4 ms (99.02%), Space: 22.2 MB (76.48%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,31 +11,23 @@
  */
 class Solution {
 public:
-    //Base Case
-    //Recursive left and right call of faith
-    //when my current node is roobed
-    //when my current node is not robbed
-    //PAIR {isRobbed , notRobbed}
+    //<isRob,notRob>
     pair<int,int> getMaxMoney(TreeNode* node) {
         //Base Case
         if(!node) return {0,0};
         
-        pair myAns =  {0,0};
-        //Recursive left and right call of faith
         pair leftAns = getMaxMoney(node->left);
         pair rightAns = getMaxMoney(node->right);
-        //when my current node is robbed
+        
+        pair myAns = {0,0};
         myAns.first = node->val + leftAns.second + rightAns.second;
-        //when my current node is not robbed
-        myAns.second = max(leftAns.first,leftAns.second) +
-                       max(rightAns.first,rightAns.second);
+        myAns.second = max(leftAns.first,leftAns.second) + max(rightAns.first,rightAns.second);
         
         return myAns;
     }
     
     int rob(TreeNode* root) {
-        pair rootP = getMaxMoney(root);
-            cout<<rootP.second;
-        return max(rootP.first,rootP.second);
+        pair rootAns = getMaxMoney(root);
+        return max(rootAns.first,rootAns.second);
     }
 };
