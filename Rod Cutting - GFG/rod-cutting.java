4c7e1dfd1bf6d33d4df0616c452bab4bbd7a4f3b
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-02 10:10:17 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -29,7 +29,7 @@ class Solution{
             len[i] = i+1;
         }
         
-        int[][] dp = new int[len.length+1][n+1];
+        int[][] dp = new int[n+1][n+1];
         
         for(int i=1;i<n+1;i++) {
             for(int j=1;j<n+1;j++) {
