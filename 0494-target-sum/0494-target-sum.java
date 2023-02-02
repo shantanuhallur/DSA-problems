0341d59e6ea214ba1d7f86845f800120f82a71f5
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-02 07:43:44 +0530

    Time: 4 ms (95.07%), Space: 41.7 MB (65.41%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -26,25 +26,27 @@ class Solution {
                 }
             }
         }
-        
+        // Each zero can be put in either of the partitions, so 2^z ways to do this
         return (int)Math.pow(2,noOfZeros)*dp[n][sum];
     }
     
     public int findTargetSumWays(int[] nums, int target) {
          if (nums.length == 1) {
-
+            // Edge case when only one element is present in array
             if (Math.abs(nums[0]) == Math.abs(target)) return 1;
-            else
-                return 0;
+            else return 0;
         }
-        
+        // We will solve problem considering there is no zeroes in the input array, and we will deal with it in the last. To count the number of zeroes
         int arrSum =0, noOfZeros = 0;
         for(int i=0;i<nums.length;i++) {
             arrSum += nums[i];
             if(nums[i] == 0) noOfZeros++;
         } 
+        // Because the sum of a subset can't be in decimals
         if((arrSum+target)%2==1) return 0;
+        // This is the given sum, of which we have to find the number of count of               subsets with sum equal to given sum
         int sumForCountOfSS = (target + arrSum)/2;
+        // Since we are dealing with only positive integers, so sum of a subset can't           be negative
         if(sumForCountOfSS<0) return 0;
         return countSS(nums,sumForCountOfSS,noOfZeros);
     }
