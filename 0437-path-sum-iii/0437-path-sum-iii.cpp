057f9141d371b85720d57fe1ee00e60540cb7022
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-08 11:09:19 +0530

    Time: 23 ms (76.77%), Space: 19.3 MB (37.06%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,32 +1,32 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public int getPref(TreeNode node,Long prefSum,int target,HashMap<Long,Integer> map) {
-        if(node==null) return 0;
-        prefSum+=node.val;
-        int res = map.getOrDefault(prefSum-target,0);
-        map.put(prefSum,map.getOrDefault(prefSum,0)+1);
-        res+= getPref(node.left,prefSum,target,map);
-        res+= getPref(node.right,prefSum,target,map);
-        map.put(prefSum,map.get(prefSum)-1);
+public:
+    int getPathSum(TreeNode* node,long prefSum,int target,unordered_map<long,int>& map) {
+        if(!node) return 0;
+        prefSum += node->val;
+        int res = 0;
+        if(map.find(prefSum-target) !=map.end()) {
+            res += map[prefSum-target];
+        }
+        map[prefSum]++;
+        res+= getPathSum(node->left,prefSum,target,map);
+        res+= getPathSum(node->right,prefSum,target,map);
+        map[prefSum]--;
         return res;
     }
-    public int pathSum(TreeNode root, int targetSum) {
-        HashMap<Long,Integer> map = new HashMap<>();
-        map.put(0l,1);
-        return getPref(root,(long)0,targetSum,map);
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<long,int> map;
+        map[0]++;
+        return getPathSum(root,0,targetSum,map);
     }
-}
+};
