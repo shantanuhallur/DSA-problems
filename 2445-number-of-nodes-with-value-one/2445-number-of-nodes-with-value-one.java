eeb5a841bb4dae8060e7da896637aad96021bfe4
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-11 09:02:00 +0530

    Time: 1377 ms (6.06%), Space: 62.3 MB (15.15%) - LeetHub

--- 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.java
+++ 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.java
@@ -1,17 +1,21 @@
 class Solution {
-public:
-    int numberOfNodes(int n, vector<int>& queries) {
-        vector<int> dp(n+1,0);
+    public int numberOfNodes(int n, int[] queries) {
+        int[] dp = new int[n+1];
         //save toggle of parent nodes
-        for(int query : queries) dp[query]^=1;
-        //get cumulative toggle of ith lable node
-        int result = 0;
-        for(int i=1;i<=n;i++){
-            
-           if(i!=1) dp[i] ^= dp[i/2];//as i/2th is the parent of this ith child
-          
-           if(dp[i]%2 ==1)result++; //get see if current node is toggled odd number of times if yes then add in result
+        for(int query : queries) {
+            dp[query]++;
         }
+        
+        int result = 0;
+        for(int i=1;i<=n;i++) {
+            //get cumulative toggle of ith lable node
+            if(i!=1) dp[i] += dp[i/2]; //as i/2th is the parent of this ith child
+            System.out.print(dp[i]+"->");
+            //get see if current node is toggled odd number of times if yes then add in result
+            if(dp[i]%2 ==1) result++;
+        }
+        
         return result;
     }
-};
+}
+
