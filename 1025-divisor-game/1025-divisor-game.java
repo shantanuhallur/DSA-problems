e51a2edb0d1f089b28b8a9d146dc783ad0adaa64
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-15 14:10:51 +0530

    Time: 3 ms (49.76%), Space: 5.8 MB (83.52%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,23 +1,6 @@
 class Solution {
-    int[] dp;
-    
-    public boolean getWin(int x) {
-        //base case
-        if(x<=0) return false;
-        //chache check
-        if(dp[x]!=-1) return true;
-        //compute
-        boolean ans = false;
-        for(int i=1;i<x;i++) {
-            if(x%i==0 && !getWin(x-i)) ans = true;
-        }
-        if(ans) dp[x] = 1;
-        return ans;
+public:
+    bool divisorGame(int n) {
+      return (n%2==0);  
     }
-    public boolean divisorGame(int n) {
-        // dp = new int[n+1];
-        // for(int i=0;i<n+1;i++) dp[i] = -1;
-        // return getWin(n);
-        return (n%2==0);
-    }
-}
+};
