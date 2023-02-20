e04aedc23ee392352695980182e24bdec9b4ad97
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-20 18:48:55 +0530

    Time: 11 ms (95.46%), Space: 8.6 MB (41.15%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n);
+        vector<int>dp(n);
         dp[0] = arr[0];
-       for(int i=1;i<n;i++) {
-           int maxEle = 0;
-           int maxOfAll=0;
-           for(int j=i; j>=max(0,i-k+1);j--) {
-               maxEle = max(maxEle,arr[j]);
-               if(j>0){
-                   maxOfAll = max(maxOfAll,(i-j+1)*maxEle+dp[j-1]);
-               }
-               else {
-                   maxOfAll = max(maxOfAll,(i-j+1)*maxEle);
-               }
-           }
-           dp[i] = maxOfAll;
-       }
+        for(int i=1;i<n;i++) {
+            int maxEle = 0;
+            int maxOfAll = 0;
+            for(int j=i;j>=max(0,i-k+1);j--) {
+                maxEle = max(maxEle,arr[j]);
+                if(j>0) {
+                    maxOfAll = max(maxOfAll,(i-j+1)*maxEle+dp[j-1]);
+                }
+                else {
+                    maxOfAll = max(maxOfAll,(i-j+1)*maxEle);
+                }
+            }
+            dp[i] = maxOfAll;
+        }
         return dp[n-1];
     }
 };
