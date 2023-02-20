f05baf7560ace4717e8e434802dfb70f5289fbbb
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-20 15:21:05 +0530

    Time: 0 ms (100.00%), Space: 42 MB (77.95%) - LeetHub

--- 0333-largest-bst-subtree/0333-largest-bst-subtree.cpp
+++ 0333-largest-bst-subtree/0333-largest-bst-subtree.cpp
@@ -1,27 +1,31 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-    vector<int> largestBST(TreeNode* node) {
-        if(!node) return{(int)1e6,(int)-1e6,0};
-        vector<int> l = largestBST(node->left);
-        vector<int> r = largestBST(node->right);
-        if(node->val > l[1] && node->val < r[0]) {
-            return {min(l[0],node->val),max(node->val , r[1]),l[2]+r[2]+1};
+    public int[] largestBST(TreeNode node) {
+        if(node==null) return new int[]{(int)1e6,-(int)1e6,0};
+        
+        int[] l = largestBST(node.left);
+        int[] r = largestBST(node.right);
+        if(node.val >l[1] && node.val < r[0]) {
+            return new int[] {Math.min(l[0],node.val),Math.max(node.val,r[1]),l[2]+r[2]+1};
         }
-        else return {(int)-1e6,(int)1e6,max(l[2],r[2])};
+        else return new int[]{-(int)1e6,(int)1e6,Math.max(l[2],r[2])};
     }
-    int largestBSTSubtree(TreeNode* root) {
-        vector<int> ret = largestBST(root);
+    public int largestBSTSubtree(TreeNode root) {
+        int[] ret = largestBST(root);
         return ret[2];
     }
-};
+}
