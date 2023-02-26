8f5086a104db72a5600c5fb73e6d11a14743267f
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-26 14:30:38 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (82.79%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,12 +1,11 @@
 class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        int ans =  -1e9;
+    public int maxProduct(int[] nums) {
+        int ans =  Integer.MIN_VALUE;
         int cprod=1;
         //subarray including elements from start
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.length;i++){
             cprod*= nums[i];
-            ans = max(ans,cprod);
+            ans = Math.max(ans,cprod);
             
             if(cprod==0){
                 cprod = 1;
@@ -15,9 +14,9 @@ public:
         
         //subarray including elements from end
         cprod = 1;
-        for(int i=nums.size()-1;i>=0;i--){
+        for(int i=nums.length-1;i>=0;i--){
             cprod*= nums[i];
-            ans = max(ans,cprod);
+            ans = Math.max(ans,cprod);
             
             if(cprod==0){
                 cprod = 1;
@@ -26,4 +25,4 @@ public:
         //subarray from start and subarray from end are not equal as either arrays can skip the element from start or end for example :- [2,3,-2,4] start can skip 4 and end can skip anu 3 or 2 for its subarray.
         return ans;
     }
-};
+}
