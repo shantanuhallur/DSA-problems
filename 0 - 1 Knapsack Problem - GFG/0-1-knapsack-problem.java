e00056cf4aa06d1f66efaa595e9cec996d89bee7
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-02-27 15:53:03 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -66,11 +66,24 @@ class Solution
     }
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
-        dp = new int[W+1][n+1];
-        for(int[] row : dp) {
-            Arrays.fill(row,-1);
+        dp = new int[n+1][W+1];
+        // for(int[] row : dp) {
+        //     Arrays.fill(row,-1);
+        //  }
+         
+         for(int i=1;i<n+1;i++) {
+             for(int j=1;j<W+1;j++) {
+                    
+                    if(wt[i-1]<=j) {
+                        dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]],
+                                            dp[i-1][j]);
+                    }
+                    else {
+                        dp[i][j] = dp[i-1][j];    
+                    }
+             }
          }
-         return getMax(wt,val,n,W);
+         return dp[n][W];
     } 
 }
 
