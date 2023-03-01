caef11ba22bda4ed0dbd09a403a6db3b42da5337
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-03-01 10:28:49 +0530

    Time: 387 ms (40.27%), Space: 13.2 MB (68.30%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,15 +6,17 @@ public:
             numSum += nums[i];
         }
         if(numSum%2!=0) return false;
-        vector<vector<bool>>dp(nums.size()+1,vector<bool>((numSum/2)+1,false));
+        int sum = numSum/2;
+        int n = nums.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
         
         int col =0;
         for(int row = 0 ; row<dp.size();row++) {
             dp[row][col] = true;
         }
         
-        for(int i=1;i<dp.size();i++) {
-            for(int j=1;j<dp[0].size();j++) {
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<sum+1;j++) {
                 if(nums[i-1]<=j) {
                     dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
                 }
@@ -23,6 +25,6 @@ public:
                 }
             }
         }
-        return dp[nums.size()][numSum/2];
+        return dp[n][sum];
     }
 };
