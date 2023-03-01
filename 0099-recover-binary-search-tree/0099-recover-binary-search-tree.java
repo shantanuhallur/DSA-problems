27eedaa5bc949a95c0e4bd4cd3f3f93062023b42
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-03-01 09:23:00 +0530

    Time: 45 ms (30.46%), Space: 57.8 MB (76.60%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -1,46 +1,42 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    TreeNode prev = null;
-    TreeNode a = null;
-    TreeNode b = null;
-    
-    public void recoverTree_01(TreeNode curr) {
-        //Base Case
-        if(curr == null) return;
+public:
+    TreeNode* prev = nullptr;
+    TreeNode* a = nullptr;
+    TreeNode* b = nullptr;
+    void recoverTree_01(TreeNode* curr) {
+        if(!curr) return;
         
         //Left Recursive Call
-        recoverTree_01(curr.left);
-        // INORDER ----- >>>>>>
-        //check if prev is first set or not.
-        if(prev!=null && prev.val > curr.val) {
+        recoverTree_01(curr->left);
+        //INORDER ------->>>>>>>
+        //condition
+        if(prev != nullptr && prev->val > curr->val) {
             b = curr;
-            if(a==null) a = prev;
+            if(a==nullptr) a = prev;
         }
-        
+        //update prev so that we can move our previous ahead.
         prev = curr;
-        // INORDER ----- >>>>>>
+        //INORDER ------->>>>>>>
         //Right Recursive Call
-        recoverTree_01(curr.right);
+        recoverTree_01(curr->right);
     }
     
-    public void recoverTree(TreeNode root) {
+    void recoverTree(TreeNode* root) {
         recoverTree_01(root);
-        int temp = a.val;
-        a.val = b.val;
-        b.val = temp;
+        //SWAPPING
+        int temp = a->val;
+        a->val = b->val;
+        b->val = temp;
     }
-}
+};
