534e3c37d784fff7ae8d56212b713ba62bdf3374
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-03-01 10:43:25 +0530

    Time: 27 ms (91.73%), Space: 58.2 MB (33.20%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,33 +1,82 @@
 class Solution {
-    boolean[][] dp;
-//     public boolean subsetSum(int[] nums,int n,int sum) {
-        
-//         if()
-//     }
+     boolean[][] val;
+     int[][] dp;
+    boolean getAns(int[] arr, int n, int sum) {
+        if(n==0 || sum==0) {
+            if(n==0 && sum==0){
+                dp[n][sum] = 1;
+                return val[n][sum] = true;
+            }
+            else if (n==0) {
+                dp[n][sum] = 1;
+                return val[n][sum] = false;
+            }
+            else {
+                dp[n][sum] = 1;
+                return val[n][sum] = true;
+            }
+             
+        }
+        if(dp[n][sum] != -1) return val[n][sum];
+            
+        if(arr[n-1]<=sum) {
+            val[n][sum] = getAns(arr,n-1,sum-arr[n-1]) || getAns(arr,n-1,sum);
+            dp[n][sum] = 1;
+        }
+        else {
+             val[n][sum] = getAns(arr,n-1,sum);
+             dp[n][sum] = 1;
+        }
+        return val[n][sum];
+    }
     public boolean canPartition(int[] nums) {
         int numSum =0;
         for(int i=0;i<nums.length;i++) {
             numSum += nums[i];
         }
         if(numSum%2!=0) return false;
-        dp = new boolean[nums.length+1][(numSum/2)+1];
         
-        int col =0;
-        for(int row = 0 ; row<dp.length;row++) {
-            dp[row][col] = true;
-        }
-        
-        for(int i=1;i<dp.length;i++) {
-            for(int j=1;j<dp[0].length;j++) {
-                if(nums[i-1]<=j) {
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-                else {
-                    dp[i][j] = dp[i-1][j];
-                }
+        int n = nums.length;
+        int sum = numSum/2;
+        // int col =0;
+        // for(int row = 0 ; row<dp.length;row++) {
+        //     dp[row][col] = true;
+        // }
+        val = new boolean[n+1][sum+1];
+        dp = new int[n+1][sum+1];
+        for(int i=0;i<n+1;i++) {
+            for(int j=0;j<sum+1;j++) {
+                // if(i==0) {
+                //     dp[i][j] = 1;
+                //     val[i][j] = false;
+                // }
+                // else{
+                //     dp[i][j] = -1;
+                // }
+                
+                // if(j==0) {
+                //     dp[i][j] = 1;
+                //     val[i][j] = true;
+                // }
+                // else {
+                //     dp[i][j] = -1;
+                // }
+                dp[i][j] = -1;
             }
         }
-        return dp[nums.length][numSum/2];
+        return getAns(nums,n,sum);
+        
+        // for(int i=1;i<dp.length;i++) {
+        //     for(int j=1;j<dp[0].length;j++) {
+        //         if(nums[i-1]<=j) {
+        //             dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+        //         }
+        //         else {
+        //             dp[i][j] = dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return dp[nums.length][numSum/2];
         // return subsetSum(nums,nums.length,numSum/2);
     }
 }
