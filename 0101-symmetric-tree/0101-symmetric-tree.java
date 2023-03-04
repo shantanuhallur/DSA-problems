2f4eec5ff9552b13259e635ea0cdcbefbeb0e382
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-03-04 14:28:50 +0530

    Time: 7 ms (55.42%), Space: 16.5 MB (58.33%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,35 +1,32 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public boolean getSymmetric(TreeNode fNode,TreeNode sNode) {
+public:
+    bool getSymmetric(TreeNode* fNode,TreeNode* sNode) {
         //if both nodes dont exist in this pos we return true;
-        if(fNode == null && sNode == null) return true;
+        if(!fNode && !sNode) return true;
         //if even one of them is null return false
-        if(fNode == null || sNode ==null) return false;
+        if(!fNode || !sNode) return false;
         //if both nodes exist but with different values we return false
-        if(fNode.val != sNode.val) return false;
+        if(fNode->val != sNode->val) return false;
         //check bottom nodes if symmetric or not with right position
-        if(!getSymmetric(fNode.left,sNode.right)) return false;
-        if(!getSymmetric(fNode.right,sNode.left)) return false;
+        if(!getSymmetric(fNode->left,sNode->right)) return false;
+        if(!getSymmetric(fNode->right,sNode->left)) return false;
         //else the treee is symmetric
         return true;
     }
-    public boolean isSymmetric(TreeNode root) {
-        //Base Case 
-        if(root == null) return true;
-        return getSymmetric(root.left,root.right);
+    bool isSymmetric(TreeNode* root) {
+        //Base Case
+        if(!root) return true;
+        return getSymmetric(root->left,root->right);
     }
-}
+};
