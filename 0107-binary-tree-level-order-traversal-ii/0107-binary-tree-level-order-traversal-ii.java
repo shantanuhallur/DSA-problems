ad617839de7c42c15f1c5b62443d67d7a1bed819
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-04-01 18:08:37 +0530

    Time: 1 ms (92.80%), Space: 42.7 MB (45.60%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -14,39 +14,30 @@
  * }
  */
 class Solution {
-    public static void levelOrder_M3(TreeNode root, Stack<List<Integer>> st) {
-        LinkedList<TreeNode> que = new LinkedList<>();
-        que.addLast(root);
-
-        int level = 0;
-        while (que.size() != 0) {
-            //System.out.println("Level :" + level + " : ");
-            List<Integer> smallAns = new ArrayList<>();
-            int size = que.size();
-            while (size-- > 0) {
-                TreeNode rn = que.removeFirst(); // remove Node
-                //System.out.print(rn.val + " ");
-                smallAns.add(rn.val);
-
-                if (rn.left != null)
-                    que.addLast(rn.left);
-                if (rn.right != null)
-                    que.addLast(rn.right);
-
-            }
-            List<Integer> base = new ArrayList<>(smallAns);
-            st.push(base);
-            smallAns.clear();
-        }
-    }
-    
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        if(root==null) return new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        Stack<List<Integer>> st = new Stack<>();
-        levelOrder_M3(root,st);
-        while(st.size()>0) {
-            ans.add(st.pop());
+        if(root==null) return new LinkedList<>();
+        LinkedList<TreeNode> que = new LinkedList<>();
+        
+        List<List<Integer>> ans = new LinkedList<>();
+        que.addLast(root);
+        
+        while(que.size()>0) {
+            int size = que.size();
+            LinkedList<Integer> levelAns = new LinkedList<>();
+            while(size-->0) {
+                TreeNode rn = que.removeFirst();
+                
+                if(rn.left!=null) {
+                    que.addLast(rn.left);
+                }
+                
+                if(rn.right!=null) {
+                    que.addLast(rn.right);
+                }
+                
+                levelAns.addLast(rn.val);
+            }
+            ans.add(0,levelAns);
         }
         return ans;
     }
