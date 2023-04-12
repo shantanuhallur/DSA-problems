235b5901948ee140d3d75b25c0663d2e716336ba
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-04-12 14:18:20 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (45.42%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,39 +1,35 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public void inorderTraversal_01(TreeNode node,ArrayList<Integer> ans) {
+public:
+    void inorderTraversal_01(TreeNode* node,vector<int>& ans) {
         //Base Case
-        if(node == null) return;
+        if(node == nullptr) return;
         
-        //recursive left call
-        inorderTraversal_01(node.left,ans);
-        // INORDER ----------->
-       
-        ans.add(node.val);
+        //Left Recursive Call
+        inorderTraversal_01(node->left,ans);
+        // INORDER ------>>>>
         
-        // INORDER ----------->
-        //recursive right call
-        inorderTraversal_01(node.right,ans);
+        ans.push_back(node->val);
         
+        
+        // INORDER ------>>>>
+        //Right Recursive Call
+        inorderTraversal_01(node->right,ans);
     }
     
-    public List<Integer> inorderTraversal(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
         inorderTraversal_01(root,ans);
         return ans;
     }
-}
+};
