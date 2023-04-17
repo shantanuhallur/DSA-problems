39b0b82c87e07b0b1cd23f144396e07a8812f9cc
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-04-17 06:08:18 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (27.92%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -1,17 +1,20 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-     //-1 -> im not covered
+    //-1 -> im not covered
     //0 -> im covered
     //1 -> im a camera and im also covered. 
      //null nodes are alredy in covered state.
@@ -20,26 +23,27 @@ public:
     // both left and right nodes are covered-> (0) but im not covered(my Node) return -1;
     //check if root is covered or not
     int cameras = 0;
-    int getInfo(TreeNode* node) {
-         //null nodes are alredy in covered state.
-        if(!node) return 0;
+    public int getInfo(TreeNode node) {
+        //null nodes are alredy in covered state.
+        if(node == null) return 0;
         
-        int leftInfo = getInfo(node->left);
-        int rightInfo = getInfo(node->right);
+        int leftInfo = getInfo(node.left);
+        int rightInfo = getInfo(node.right);
         //one of my child node is not covered i will compulsory have to attach a camera to my node
         if(leftInfo == -1 || rightInfo == -1) {
             cameras++;
             return 1;
         }
-         //check if my child is a camera
+        
+        //check if my child is a camera
         if(leftInfo == 1 || rightInfo == 1) return 0;
         // both left and right nodes are covered-> (0) but im not covered(my Node) return -1;
         return -1;
-    } 
-    int minCameraCover(TreeNode* root) {
+    }
+    public int minCameraCover(TreeNode root) {
         //check if root is covered or not
         int rootInfo = getInfo(root);
         if(rootInfo == -1) cameras++;
         return cameras;
     }
-};
+}
