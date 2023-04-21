b7ac3e6113b8ec1dfd2cffae9b83abdda53f04ec
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-04-21 12:28:52 +0530

    Time: 3 ms (44.50%), Space: 42.5 MB (46.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,35 +1,39 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
     
-        TreeNode* buildTree(vector<int>& preorder,int psi,int pei, vector<int>& inorder,int isi,int iei) {
+    public TreeNode buildTree(int[] preorder,int psi,int pei, int[] inorder,int isi,int iei) {
         if(psi>pei){ //for 1 node (0,0) indexes will get passed as psi+1 , psi + isi = (1,0)
-            return NULL;
+            return null;
         }
         
-        TreeNode* node = new TreeNode(preorder[psi]); //root node is starting node of preorder.
+        TreeNode node = new TreeNode(preorder[psi]); //root node is starting node of preorder.
         int idx = isi;
         //find this root node in inorder array.
         while(preorder[psi] != inorder[idx]) idx++;
         //find how many elements are behind this found root in inorder.
         int tel = idx - isi; //total no of elements = idx - inorder starting index.
         
-        node->left = buildTree(preorder,psi+1,psi+tel,inorder,isi,idx-1);
-        node->right = buildTree(preorder,psi+tel+1,pei,inorder,idx+1,iei);
+        node.left = buildTree(preorder,psi+1,psi+tel,inorder,isi,idx-1);
+        node.right = buildTree(preorder,psi+tel+1,pei,inorder,idx+1,iei);
         
         return node;
     }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return buildTree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1);
+    
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
-};
+}
