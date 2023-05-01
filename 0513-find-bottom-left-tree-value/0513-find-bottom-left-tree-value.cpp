6d1bdb44b8ed51af0ff8853eede61971213ef7f6
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-05-01 16:52:09 +0530

    Time: 13 ms (56.24%), Space: 21.7 MB (89.42%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -1,33 +1,30 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        LinkedList<TreeNode> que = new LinkedList<>();
-        que.addLast(root);
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> que;
+        que.push(root);
         
         while(que.size()!=0) {
-            //remove the node from the que
-            root = que.removeFirst();
-            // add first right child then left child.
-            if(root.right != null) que.addLast(root.right);
+            //remove the node in front of que
+            root = que.front(); que.pop();
             
-            if(root.left != null) que.addLast(root.left); 
-        
+            //FIRST add RIGHT child then LEFT child.
+            if(root->right) que.push(root->right);
+            
+            if(root->left) que.push(root->left);
         }
-        //Now we have our leftmost node saved in root.
-        return root.val;
+        
+        return root->val;
     }
-}
+};
