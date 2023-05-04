69ea84438702ee88aa2f8161b33258842a2eb434
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-05-04 08:54:25 +0530

    Time: 12 ms (66.22%), Space: 22.3 MB (43.10%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,41 +1,36 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public List<Integer> largestValues(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        
-        LinkedList<TreeNode> que = new LinkedList<>();
-        que.addLast(root);
-        //LVL ORDER TRAVERSAL
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root) return {};
+        vector<int> ans;
+        queue<TreeNode*> que;
+        que.push(root);
+        //LVL ORFER TRAVERSAL
         while(que.size()!=0) {
             int size = que.size();
-            int largest = que.peek().val;
+            int largest = que.front()->val;
             while(size-->0) {
-                //Remove Node
-                TreeNode removeN = que.removeFirst();
+                TreeNode* removeN = que.front(); que.pop();
                 
-                if(removeN.left != null) que.addLast(removeN.left);
+                if(removeN->left) que.push(removeN->left);
                 
-                if(removeN.right != null) que.addLast(removeN.right);
-                
-                if(removeN.val >= largest) largest = removeN.val;
+                if(removeN->right) que.push(removeN->right);
+                //UPDATE LARGEST
+                if(removeN->val >= largest) largest = removeN->val;
             }
-           ans.add(largest); 
+            ans.push_back(largest);
         }
         return ans;
     }
-}
+};
