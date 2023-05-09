ae2c7c4f828b796c4684c7f5fea2851e0ea2fafc
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-05-09 18:28:35 +0530

    Time: 1 ms (94.20%), Space: 42.4 MB (46.71%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        if(numRows==0)return triangle;
+        
+        List<Integer>first_row = new ArrayList<>();
+        first_row.add(1);
+        triangle.add(first_row);
+        
+        for(int i=1;i<numRows;i++){
+            List<Integer> prev_row = triangle.get(i-1);
+            List<Integer>curr_row = new ArrayList<>();
+            
+            curr_row.add(1);
+            
+            for(int j=1;j<i;j++){ //row indexing starts from 1 and we need to add row-1 characters excluding two ones in the list as the final answer.
+                curr_row.add(prev_row.get(j-1)+prev_row.get(j));
+            }
+            
+            curr_row.add(1);
+            triangle.add(curr_row);
+        }
+        return triangle;
+    }
+}
