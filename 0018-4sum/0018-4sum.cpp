d0c31791fe9d183cf675077a64cc4913cb43e101
shantanuhallur <60377862+shantanuhallur@users.noreply.github.com>
2023-06-03 15:45:55 +0530

    Time: 79 ms (65.43%), Space: 13 MB (99.88%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> st;
+        vector<vector<int>> quadruplets;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                set<long long> hashset;
-                for(int k=j+1;k<n;k++){
-                    long long sum = nums[i]+nums[j];
-                    sum+=nums[k];
-                    long long fourth = target-sum;
-                    if(hashset.find(fourth)!=hashset.end()){
-                        vector<int> temp = {nums[i],nums[j],nums[k],(int)fourth};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
+        // Sorting the array
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 3; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates
+                if (j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    long long sum = static_cast<long long>(nums[i]) + nums[j] + nums[left] + nums[right];
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        quadruplets.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        // Skip duplicates
+                        while (left < right && nums[left] == nums[left + 1]){
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]){
+                            right--;
+                        }
+                        left++;
+                        right--;
                     }
-                    hashset.insert(nums[k]);
                 }
             }
         }
-        vector<vector<int>> quadruplets(st.begin(),st.end());
         return quadruplets;
-
     }
 };
